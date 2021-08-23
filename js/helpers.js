c7e8840c15a9409f1b50864134377c8b81dff353
Tijl Leenders <tijl.leenders@gmail.com>
2021-08-23 06:32:11 +0200

    add contact form handler and change wording

--- js/helpers.js
+++ js/helpers.js
@@ -1834,3 +1834,35 @@ function formattimesOfDaysPreftring(timesOfDaysPref) {
             break;
     }
 }
+
+function submitToAPI(e) {
+    e.preventDefault();
+    var URL = "https://" + _config.apiContact + ".execute-api.eu-west-1.amazonaws.com/prod/contact";
+
+    var desc = $("#description-input").val();
+    var email = $("#email-input").val();
+    var data = {
+        desc: desc,
+        email: email
+    };
+
+    $.ajax({
+        type: "POST",
+        url: "https://" + _config.apiContact + ".execute-api.eu-west-1.amazonaws.com/prod/contact",
+        dataType: "json",
+        crossDomain: "true",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(data),
+
+        success: function() {
+            // clear form and show a success message
+            alert("Thank you so much for your feedback! We will improve.");
+            document.getElementById("contact-form").reset();
+            location.reload();
+        },
+        error: function() {
+            // show an error message
+            alert("Aww... So sorry. Please try emailing. We'd love to hear from you!");
+        }
+    });
+}
