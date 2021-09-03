34a9cb7caac1c7f6f81527b124da10f09ecccd94
Tijl Leenders <tijl.leenders@gmail.com>
2021-09-03 07:33:05 +0200

    remove circle on slot

--- js/helpers.js
+++ js/helpers.js
@@ -1146,13 +1146,7 @@ function generateSlotHTML(element) {
         <div class="col nopadding text-truncate icons d-flex flex-row align-items-center" id="slot-col-' +
         slotId +
         '">\
-          <div class="m-1 todo-circle" id="todo-circle-' +
-        slotId +
-        '">\
-            ' + slotSvg + '\
-          </div>\
-          ' +
-        '<div class="row nopadding"><div class="col nopadding d-flex flex-column" id="col-begin-end-' + slotId + '" >' +
+        <div class="row nopadding"><div class="col nopadding d-flex flex-column" id="col-begin-end-' + slotId + '" >' +
         '<div class="mx-2 begin-time" id="begin-' + slotId + '" >' + begin.format('HH:mm') + '</div>' +
         '<div class="mx-2 end-time" id="end-' + slotId + '" >' + end.format('HH:mm') + '</div>' +
         '</div></div>' +
