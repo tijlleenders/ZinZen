e3fd401f5b14e0b3213f2772e2945972147d6c18
Tijl Leenders <tijl.leenders@gmail.com>
2021-09-13 18:38:50 +0200

    ask for suggestions on each keystroke

--- js/clickhandlers/modalClickHandlerAdd.js
+++ js/clickhandlers/modalClickHandlerAdd.js
@@ -1,6 +1,15 @@
 'use strict'
 
-$("#inputCommand").on("keyup", function(e) {
+$("#myModal").on("keyup", "#inputCommand", function(e) {
+    console.log("key pressed:", $("#inputCommand").val())
+    let goalId = $("#myModal").data("idx")
+    let getSuggestions = {
+        action: "read",
+        command: "getSuggestions",
+        input: $("#inputCommand").val(),
+        goalId: goalId
+    }
+    send(JSON.stringify(getSuggestions))
     if (e.which === 13) {
         if ($(this).val().length == 0) return;
 
