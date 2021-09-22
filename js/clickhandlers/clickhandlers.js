2a4be099db016f2d2d8a895e9e382ac83505cd31
Tijl Leenders <tijl.leenders@gmail.com>
2021-09-22 10:00:04 +0200

    document what's missing from hardcoded properties

--- js/clickhandlers/clickhandlers.js
+++ js/clickhandlers/clickhandlers.js
@@ -128,19 +128,6 @@ $("#top-settings").click(function() {
         handleIncomingProperties(propertiesMap)
     });
 
-    //     properties.set('subCountMaybe', sub.get('subCountMaybe'))
-    //     properties.set('subCountPromised', sub.get('subCountPromised'))
-    //     properties.set('subCountDone', sub.get('subCountDone'))
-    //     properties.set('subCountNever', sub.get('subCountNever'))
-
-    // item.get("allSubs").forEach(sub => {
-    //     let properties = sub.get("properties")
-    //     properties.set('directParents', sub.get('directParents'))
-    //     properties.set('ultimateParents', sub.get('ultimateParents'))
-    //     properties.set('scheduledBeginISO', sub.get('scheduledBeginISO'))
-    //     properties.set('scheduledEndISO', sub.get('scheduledEndISO'))
-    //     handleIncomingProperties(properties)
-    // })
 })
 
 $("#top-lists").click(function() {
