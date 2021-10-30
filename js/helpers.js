5d2a7d5b1f288fc8cb21061da1ab643e85fbd884
Tijl Leenders <tijl.leenders@gmail.com>
2021-10-30 07:58:50 +0200

    fix rendering error

--- js/helpers.js
+++ js/helpers.js
@@ -416,7 +416,7 @@ function generateGoalHTML(properties) {
     let goalSvg = getGoalSvg(status, goalId)
 
     //Todo: add directParents from relationships
-    // let parentRowAndColHTML = ''
+    let parentRowAndColHTML = ''
     // if (directParents.length != 0) {
     //     // console.log("directParents for " + goalId + ":" + directParents)
     //     parentRowAndColHTML += '<div class="row" id="goal-parents-row-' +
@@ -438,17 +438,6 @@ function generateGoalHTML(properties) {
     //     }
     //     parentRowAndColHTML += '</div></div>'
     // }
-
-    let tagHTML = ''
-    //only set tag icon if top level goal
-    if (directParents.length == 0) {
-        tagHTML = '\
-    <div class="tags mx-1" id="tags-' +
-            goalId +
-            '">\
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M2.5 7.775V2.75a.25.25 0 01.25-.25h5.025a.25.25 0 01.177.073l6.25 6.25a.25.25 0 010 .354l-5.025 5.025a.25.25 0 01-.354 0l-6.25-6.25a.25.25 0 01-.073-.177zm-1.5 0V2.75C1 1.784 1.784 1 2.75 1h5.025c.464 0 .91.184 1.238.513l6.25 6.25a1.75 1.75 0 010 2.474l-5.026 5.026a1.75 1.75 0 01-2.474 0l-6.25-6.25A1.75 1.75 0 011 7.775zM6 5a1 1 0 100 2 1 1 0 000-2z"/></svg>\
-          </div>'
-    }
 
     let returnHTML = `
           <div class="col-2 circle-col d-flex align-items-center align-text-center" id="circle-col-` + goalId + `">
