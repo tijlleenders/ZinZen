cf52c0fb71a0c27b5ea656597f3dbd1848e517e0
Tijl Leenders <tijl.leenders@gmail.com>
2021-11-17 15:24:30 +0100

    committing to test on different browser

--- js/clickhandlers/modalClickHandlerFeelings.js
+++ js/clickhandlers/modalClickHandlerFeelings.js
@@ -6,7 +6,15 @@ $("#myModal").on("click", "#go-to-mind-button", function () {
 })
 
 $("#myModal").on("click", ".feeling", function (e) {
+    e.preventDefault()
     console.log("feeling pressed:", e.currentTarget.innerText)
-    $("#emotion-" + e.currentTarget.innerText).toggleClass('active')
+    if ($("#emotion-" + e.currentTarget.innerText).hasClass('active')) {
+        $("#emotion-" + e.currentTarget.innerText).removeClass('active')
+        $("#emotion-" + e.currentTarget.innerText).addClass('no-hover')
+    } else {
+        $("#emotion-" + e.currentTarget.innerText).addClass('active')
+        $("#emotion-" + e.currentTarget.innerText).removeClass('no-hover')
+    }
+
     $("#emo-title-row").focus()
 })
