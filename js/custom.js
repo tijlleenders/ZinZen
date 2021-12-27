bddec04d848a3f601161555e5e42969878bc30e5
Tijl Leenders <tijl.leenders@gmail.com>
2021-12-27 13:06:07 +0100

    test installPrompt

--- js/custom.js
+++ js/custom.js
@@ -175,7 +175,8 @@ window.addEventListener('beforeinstallprompt', (e) => {
     // Stash the event so it can be triggered later.
     deferredPrompt = e;
     // Update UI notify the user they can install the PWA
-    showInstallPromotion();
+    // showInstallPromotion(); //To implement
+    deferredPrompt.prompt();
     // Optionally, send analytics event that PWA install promo was shown.
     console.log(`'beforeinstallprompt' event was fired.`);
 });
