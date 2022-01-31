8f20e7e84d0efd2868035559d495ba0170eb9de0
Mallika Rai <mallika.rai05@gmail.com>
2022-01-31 12:51:43 +0530

    theme buttons added

--- zinzen-frontend/src/components/Themes.js
+++ zinzen-frontend/src/components/Themes.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import ThemeLight from '../images/DashboardThemeLight.svg'
+import ThemeDark from '../images/DashboardThemeDark.svg'
 
 const Themes = () => {
   return (
     <div>
-      <h1>THEMES</h1>
+      <button className="theme-btn1"><img src={ThemeLight} alt="Light Theme" className="themechoice"/></button>
+      <br/>
+      <br/>
+      <button className="theme-btn2"><img src={ThemeDark} alt="Dark Theme" className="themechoice"/></button>
     </div>
   )
 }
