b992d1a700edb2443a3fc220ba3b96154459ebf7
Mallika Rai <mallika.rai05@gmail.com>
2022-02-11 00:47:25 +0530

    Language saved in settings

--- src/components/LangItem.jsx
+++ src/components/LangItem.jsx
@@ -7,19 +7,19 @@ export const LangItem = ({ lang }) => {
     const [isLanguageChosen, setIsLanguageChosen] = useRecoilState(languageSelectionState);
     const handleClick = (langId) => {
         setIsLanguageChosen(true);
-        changeLanguage(langId);
+        //changeLanguage(langId);
         localStorage.setItem("language", JSON.stringify(langId));
     }
        
     return (
         <div className="containerLang">
-            {lang.sno % 4 === 1 ? <Button variant="peach" size="lg" className="lang-btn1" onClick={() => { setIsLanguageChosen(true);}}>
+            {lang.sno % 4 === 1 ? <Button variant="peach" size="lg" className="lang-btn1" onClick={() => { handleClick(lang.langId);}}>
                 {lang.title}
-            </Button> : lang.sno % 4 === 2 ? <Button variant="dark-pink" size="lg" className="lang-btn1" onClick={() => { setIsLanguageChosen(true) }}>
+            </Button> : lang.sno % 4 === 2 ? <Button variant="dark-pink" size="lg" className="lang-btn1" onClick={() => { handleClick(lang.langId) }}>
                 {lang.title}
-            </Button> : lang.sno % 4 === 3 ? <Button variant="grey-base" size="lg" className="lang-btn2" onClick={() => { setIsLanguageChosen(true) }}>
+            </Button> : lang.sno % 4 === 3 ? <Button variant="grey-base" size="lg" className="lang-btn2" onClick={() => { handleClick(lang.langId)}}>
                 {lang.title}
-            </Button> : <Button variant="pale-blue" size="lg" className="lang-btn2" onClick={() => { setIsLanguageChosen(true) }}>
+            </Button> : <Button variant="pale-blue" size="lg" className="lang-btn2" onClick={() => { handleClick(lang.langId) }}>
                 {lang.title}
             </Button>}
         </div>
