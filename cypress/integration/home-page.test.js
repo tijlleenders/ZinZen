6c10dfd641fb4e46a4e50b9f2a56e524456eddd3
Mallika Rai <mallika.rai05@gmail.com>
2022-02-16 00:06:55 +0530

    Change cypress file

--- cypress/integration/home-page.test.js
+++ cypress/integration/home-page.test.js
@@ -21,5 +21,8 @@ describe('Basic Tests Home Page', () => {
     it("should display the book icon", () => {
         cy.get('[alt="Book Icon"]').should('be.visible');
     });
-
+    it("user choice panel should work", () => {
+        cy.contains("Choose your preferred Language")
+        cy.contains("Language").click()
+    });
 })
--- cypress/integration/language-choice-panel.test.js
+++
@@ -1,7 +0,0 @@
-/// <reference types="cypress" />
-describe('Basic Tests Language Choice', () => {
-    it("user choice panel should work", () => {
-        cy.contains("Choose your preferred Language")
-        cy.contains("Language").click()
-    });
-});
