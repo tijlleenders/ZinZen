033e7923dacc1603f92ef7c5ede29e7e1c5d93e4
Mallika Rai <mallika.rai05@gmail.com>
2022-02-22 15:34:16 +0530

    Changes made in navbar

--- src/components/HeaderDashboard.js
+++ src/components/HeaderDashboard.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import {Nav, Navbar} from 'react-bootstrap';
 import DarkModeToggle from "react-dark-mode-toggle";
 import { useTranslation } from "react-i18next";
 import "../translations/i18n"
@@ -8,37 +9,26 @@ const HeaderDashboard = () => {
   const { t } = useTranslation();
   return (
     <div>
-      <div className="navbar-custom">
-        <nav className="navbar-custom navbar navbar-expand-sm navbar-light ">
-    
-          <div className="mx-auto d-sm-flex d-block flex-sm-nowrap">
+      <Navbar collapseOnSelect expand="lg">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="navbar-custom">
+            <Nav.Link href="#features" className="nav-link">{t("home")}</Nav.Link>
+            <Nav.Link href="#pricing" className="nav-link">{t("discover")}</Nav.Link>
+            <Nav.Link href="#deets" className="nav-link">{t("donate")}</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <DarkModeToggle
+        onChange={setIsDarkMode}
+        checked={isDarkMode}
+        size={60}
+      />
+      </Navbar>
 
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample11" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse text-center" id="navbarsExample11">
-              <ul className="navbar-nav">
-                <li></li>
-                <li className="nav-item active">
-                  <a className="nav-link" href="#">{t("home")}</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">{t("discover")}</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">{t("donate")}</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <DarkModeToggle
-            onChange={setIsDarkMode}
-            checked={isDarkMode}
-            size={60}
-          />
-        </nav>
-      </div>
+     
     </div>
+
+
   )
 }
 
