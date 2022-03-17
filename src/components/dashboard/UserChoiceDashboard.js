0f4696149186272deff7f910411631acb87e26c4
Mallika Rai <mallika.rai05@gmail.com>
2022-03-17 14:28:23 +0530

    Add quick add button

--- src/components/dashboard/UserChoiceDashboard.js
+++ src/components/dashboard/UserChoiceDashboard.js
@@ -4,6 +4,8 @@ import { useRecoilState } from 'recoil'
 import { darkModeState } from '../../store/DarkModeState'
 import { Container, Row } from 'react-bootstrap'
 import { useTranslation } from "react-i18next";
+import AddIconLight from '../../images/AddIconLight.png'
+import AddIconDark from '../../images/AddIconDark.png'
 import "../../translations/i18n"
 
 const UserChoiceDashboard = () => {
@@ -16,18 +18,33 @@ const UserChoiceDashboard = () => {
       <Container fluid>
         <Row >
           <Button variant={darkModeStatus ? "brown" : "peach"} size="lg" className={darkModeStatus ? "dashboard-choice-dark1" : "dashboard-choice-light1"}>{t("mygoals")}</Button>
+          <Button variant={darkModeStatus ? "brown" : "peach"} className={darkModeStatus ? "dashboard-add-btn-dark1" : "dashboard-add-btn-light1"}>
+            {darkModeStatus ? <img src={AddIconDark} alt="Add Icon" className="add-icon" /> : <img src={AddIconLight} alt="Add Icon" className="add-icon" />}
+          </Button>
         </Row>
         <Row >
           <Button variant={darkModeStatus ? "dark-pink" : "pink"} size="lg" className={darkModeStatus ? "dashboard-choice-dark" : "dashboard-choice-light"}>{t("myfeelings")}</Button>
+          <Button variant={darkModeStatus ? "dark-pink" : "pink"} className={darkModeStatus ? "dashboard-add-btn-dark" : "dashboard-add-btn-light"}>
+            {darkModeStatus ? <img src={AddIconDark} alt="Add Icon" className="add-icon" /> : <img src={AddIconLight} alt="Add Icon" className="add-icon" />}
+          </Button>
         </Row>
         <Row >
-          <Button variant={darkModeStatus ? "dark-grey" : "grey-base"}  size="lg" className={darkModeStatus ? "dashboard-choice-dark" : "dashboard-choice-light"}>{t("mytime")}</Button>
+          <Button variant={darkModeStatus ? "dark-grey" : "grey-base"} size="lg" className={darkModeStatus ? "dashboard-choice-dark" : "dashboard-choice-light"}>{t("mytime")}</Button>
+          <Button variant={darkModeStatus ? "dark-grey" : "grey-base"} className={darkModeStatus ? "dashboard-add-btn-dark" : "dashboard-add-btn-light"}>
+            {darkModeStatus ? <img src={AddIconDark} alt="Add Icon" className="add-icon" /> : <img src={AddIconLight} alt="Add Icon" className="add-icon" />}
+          </Button>
         </Row>
         <Row >
           <Button variant={darkModeStatus ? "dark-blue" : "pale-blue"} size="lg" className={darkModeStatus ? "dashboard-choice-dark" : "dashboard-choice-light"}>{t("explore")}</Button>
+          <Button variant={darkModeStatus ? "dark-blue" : "pale-blue"} className={darkModeStatus ? "dashboard-add-btn-dark" : "dashboard-add-btn-light"}>
+            {darkModeStatus ? <img src={AddIconDark} alt="Add Icon" className="add-icon" /> : <img src={AddIconLight} alt="Add Icon" className="add-icon" />}
+          </Button>
         </Row>
         <Row >
-          <Button variant={darkModeStatus ? "dark-purple" : "purple"}  size="lg" className={darkModeStatus ? "dashboard-choice-dark" : "dashboard-choice-light"}>{t("zinzen")}</Button>
+          <Button variant={darkModeStatus ? "dark-purple" : "purple"} size="lg" className={darkModeStatus ? "dashboard-choice-dark" : "dashboard-choice-light"}>{t("zinzen")}</Button>
+          <Button variant={darkModeStatus ? "dark-purple" : "purple"} className={darkModeStatus ? "dashboard-add-btn-dark" : "dashboard-add-btn-light"}>
+            {darkModeStatus ? <img src={AddIconDark} alt="Add Icon" className="add-icon" /> : <img src={AddIconLight} alt="Add Icon" className="add-icon" />}
+          </Button>
         </Row>
       </Container>
     </div>
