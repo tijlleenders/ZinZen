35f48aa5ba9c80129b7aca2c3f4f0f2ed513ae1c
hans <hanson4e@gmail.com>
2022-04-30 10:39:07 +0300

    Making the Add goals title be avilable in the listed languages

--- src/components/todoList/TodoList.jsx
+++ src/components/todoList/TodoList.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
-import { HeaderDashboard } from "../dashboard/HeaderDashboard";
+import  {HeaderDashboard}  from "../dashboard/HeaderDashboard";
 import { useTranslation } from "react-i18next";
 import { useRecoilValue } from 'recoil';
 import { darkModeState } from '../../store/DarkModeState'
 import "../../translations/i18n";
+import "./TodoList.scss"
 
 export const TodoList=()=>{
   const darkModeStatus = useRecoilValue(darkModeState);
   const { t } = useTranslation();
   return(
     <div>
+      <row>
     <HeaderDashboard/>
-     <h1>Add a Goal</h1> 
-     </div>);
+     </row>
+     <row>
+     <h3 className={darkModeStatus ? "myGoals-font-dark" : "myGoals-font-light"}>{t("myGoalsMessage")}</h3>
+     </row>
+     </div>
+
+     );
 }
