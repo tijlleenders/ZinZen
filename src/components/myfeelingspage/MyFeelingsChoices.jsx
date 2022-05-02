44975ff2c21822c78415be59ed6ba93e2f950ebc
pulkitvyas08 <e19cse284@bennett.edu.in>
2022-05-02 17:16:55 +0530

    Modularize the feelings pages' structure

--- src/components/myfeelingspage/MyFeelingsChoices.jsx
+++ src/components/myfeelingspage/MyFeelingsChoices.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
 
-import { FeelingHappy } from './FeelingHappy';
-import { FeelingExcited } from './FeelingExcited';
-import { FeelingGratitude } from './FeelingGratitude';
-import { FeelingSad } from './FeelingSad';
-import { FeelingAfraid } from './FeelingAfraid';
-import { FeelingAngry } from './FeelingAngry';
+import { feelingsList, feelingsCategories } from '@consts/FeelingsList';
+import { FeelingTemplate } from './FeelingTemplate';
 
 export function MyFeelingsChoices() {
   return (
     <div>
-      <FeelingHappy />
-      <FeelingExcited />
-      <FeelingGratitude />
-      <FeelingSad />
-      <FeelingAfraid />
-      <FeelingAngry />
+      {feelingsCategories.map((feelingCategory) => (
+        <FeelingTemplate
+          key={feelingCategory}
+          feelingCategory={feelingCategory}
+          feelingsList={feelingsList[feelingCategory]}
+        />
+      ))}
     </div>
   );
 }
