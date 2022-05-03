68286d0d72446b94bb4d785294bd2a59fcd755dd
hans <hanson4e@gmail.com>
2022-05-03 17:53:51 +0300

    Adiing some scss to the form

--- src/components/todoList/TodoList.jsx
+++ src/components/todoList/TodoList.jsx
@@ -1,30 +1,29 @@
 import React from 'react'
-import { Container, Row} from "react-bootstrap";
-import  {HeaderDashboard}  from "../dashboard/HeaderDashboard";
+import { Container, Row } from "react-bootstrap";
+import { HeaderDashboard } from "../dashboard/HeaderDashboard";
 import { useTranslation } from "react-i18next";
 import { useRecoilValue } from 'recoil';
 import { darkModeState } from '../../store/DarkModeState'
 import "../../translations/i18n";
 import "./TodoList.scss"
-import {TodoForm} from './TodoListForm'
+import { TodoForm } from './TodoListForm'
 
-export const TodoList=()=>{
+export const TodoList = () => {
   const darkModeStatus = useRecoilValue(darkModeState);
   const { t } = useTranslation();
-  return(
+  return (
     <div>
       <Container fluid>
-      <Row>
-    <HeaderDashboard/>
-     </Row>
-     <Row>
-     <h2 className={darkModeStatus ? "myGoals-font-dark" : "myGoals-font-light"}>{t("myGoalsMessage")}</h2>
-     </Row>
-     <Row>
-       <TodoForm/>
-     </Row>
-     </Container>
-     </div>
-
-     );
+        <Row>
+          <HeaderDashboard />
+        </Row>
+        <Row>
+          <h2 className={darkModeStatus ? "myGoals-font-dark" : "myGoals-font-light"}>{t("myGoalsMessage")}</h2>
+        </Row>
+        <Row>
+          <TodoForm />
+        </Row>
+      </Container>
+    </div>
+  );
 }
--- src/components/todoList/TodoListForm.jsx
+++ src/components/todoList/TodoListForm.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { Button } from 'react-bootstrap';
 import { useRecoilValue } from 'recoil';
 import { darkModeState } from '../../store/DarkModeState'
@@ -7,97 +7,79 @@ import AddIconLight from "../../images/AddIconLight.png";
 import AddIconDark from "../../images/AddIconDark.png";
 
 
-export const TodoForm=()=> {
+export const TodoForm = () => {
     const darkModeStatus = useRecoilValue(darkModeState);
-    const [tableData,setTableData]=useState([] )
+    const [tableData, setTableData] = useState([])
     const [formInputData, setFormInputData] = useState(
         {
-        inputGoal:'',
-        inputTime:''
-       }
+            inputGoal: '',
+            inputTime: ''
+        }
     );
 
-    const handleChange=(e)=>{
+    const handleChange = (e) => {
         const value = e.target.value;
-       setFormInputData({ ...formInputData,
-        [e.target.name]:value
+        setFormInputData({
+            ...formInputData,
+            [e.target.name]: value
         })
-       }
+    }
 
-    const handleSubmit=(e)=>{
-        e.preventDefault();
-        if(formInputData.inputGoal!==''){
-            const newData = (data)=>([...data, formInputData])
+    const handleSubmit = (e) => {
+        if (formInputData.inputGoal) {
+            const newData = (data) => ([...data, formInputData])
             setTableData(newData);
         }
         setFormInputData({
-          inputGoal:'',
-          inputTime:''
-          })
+            inputGoal: '',
+            inputTime: ''
+        })
 
+        e.preventDefault();
     }
 
 
-  return (
-      <form className='todo-form' onSubmit={handleSubmit}>
-       <div>
-        <input  className={darkModeStatus ? "addTaskDark" : "addTaskLight"}
-        type='text'
-        name='inputGoal'
-         placeholder='Add task'
-        value={formInputData.inputGoal}
-        onChange={handleChange}
-        />
-       </div>
-       <div>
-          <input
-           className={darkModeStatus ? "addTaskDark-time" : "addTaskLight-time"}
-           type='date'
-           placeholder='Add time'
-           name='inputTime'
-            value={formInputData.inputTime}
-            onChange={handleChange}
-            />
-        </div>
+    return (
+        <form className='todo-form' onSubmit={handleSubmit}>
+            <div>
+                <input className={darkModeStatus ? "addTaskDark" : "addTaskLight"}
+                    type='text'
+                    name='inputGoal'
+                    placeholder='What do you want to achieve today?'
+                    value={formInputData.inputGoal}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <input
+                    className={darkModeStatus ? "addTaskDark-time" : "addTaskLight-time"}
+                    type='date'
+                    placeholder='Add time'
+                    name='inputTime'
+                    value={formInputData.inputTime}
+                    onChange={handleChange}
+                />
+            </div>
 
-        <div>
+            <div className='inputs'>
+                {
+                    tableData.map((data) => {
+                        return (
+                            <div className={darkModeStatus ? "addTaskDark" : "addTaskLight"}>
+                                <div className='inputTime'>{data.inputTime}</div>
+                                <div className='inputGoal'>{data.inputGoal}</div>
+                            </div>
+                        )
+                    })
+                }
 
-            {
-                tableData.map((data)=>{
-                    return(
-                        <div className={darkModeStatus ? "addTaskDark" : "addTaskLight"}>
-                            <div className='inputGoal'>{data.inputGoal}</div>
-                            <div>{data.inputTime}</div>
-                        </div>
-                    )
-                })
-            }
+            </div>
 
-        </div>
-
-        <div  className={darkModeStatus ? "myGoalsButton-Dark" : "myGoalsButton-Light"}>
-       <Button variant={darkModeStatus ? "dark-pink" : "pink"}
-         onClick={handleSubmit}
-       >
-
-       {darkModeStatus ? (
-                           <img
-                                src={AddIconDark}
-                                alt="Add Icon"
-                                className="add-icon"
-                            />
-                        ) : (
-                            <img
-                                src={AddIconLight}
-                                alt="Add Icon"
-                                className="add-icon"
-
-
-                                />)}
-       </Button>
-      </div>
-      </form>
-
-
-  )
+            <div className={darkModeStatus ? "myGoalsButton-Dark" : "myGoalsButton-Light"}>
+                <Button variant={darkModeStatus ? "dark-pink" : "pink"} onClick={handleSubmit}>
+                    <img src={darkModeStatus ? AddIconDark : AddIconLight} alt="Add Icon" className="add-icon" />
+                </Button>
+            </div>
+        </form>
+    )
 }
