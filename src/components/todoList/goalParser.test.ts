213d3ad559ca1503fe4d76e38e9ba5a4ed71da1c
GithinjiHans <hanson4e@gmail.com>
2022-05-14 18:20:39 +0300

    Made a failing test for the goal parser

---
+++ src/components/todoList/goalParser.test.ts
@@ -0,0 +1,17 @@
+
+const goalObject="jkhsjkdfhjkdh daily"
+
+
+function getGoalObject(){
+
+}
+
+describe('getGoalObject function',()=>{
+      it('should return daily',()=>{
+       // const fun=goalObject.indexOf('daily')!==-1
+       // }
+        const expected="daily";
+
+        expect(getGoalObject()).toBe(expected);
+      })
+    })
