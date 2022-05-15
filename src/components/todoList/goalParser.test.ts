f67ac6f6f0e7f39fee15e2659881304a2ebdd8f4
GithinjiHans <hanson4e@gmail.com>
2022-05-15 07:49:29 +0300

    Created a dummy object to work with

--- src/components/todoList/goalParser.test.ts
+++ src/components/todoList/goalParser.test.ts
@@ -1,9 +1,32 @@
+import { useState } from "react";
+
+const goalObject=({
+   title: 'Walk 1h daily',
+    lang: 'en',
+   color:  '#fff'})
+
+const [outputObject,setOutputObject]=useState([])
+
 function getGoalObject(){}
 
 describe('getGoalObject function',()=>{
       it('should return repetition suggestion : daily',()=>{
 
-        const expected="daily";
-        expect(getGoalObject()).toBe(expected);
+        function fun(){
+          if(goalObject.title.indexOf('daily')!==-1){
+            const newData =(data)=>[...data,goalObject]
+            setOutputObject({
+           newData,
+           suggestion: {repetition: "daily"},
+            });}
+             else{
+              setOutputObject({
+                newData
+              })
+            }
+            return outputObject;
+          }
+
+        expect(getGoalObject()).toBe(fun);
       })
     })
