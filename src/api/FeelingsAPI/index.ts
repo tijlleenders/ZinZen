f19d4050c3ca85db2a853d90e99a67481d71b936
pulkitvyas08 <e19cse284@bennett.edu.in>
2022-05-17 01:28:56 +0530

    Fix rendering issue for the feelings page

--- src/api/FeelingsAPI/index.ts
+++ src/api/FeelingsAPI/index.ts
@@ -1,20 +1,9 @@
 import { db } from '@models';
 import { getJustDate } from '@src/utils';
 
-export const resetDatabase = () => db.transaction('rw', db.feelingItems, async () => {
+export const resetDatabase = () => db.transaction('rw', db.feelingsCollection, async () => {
   await Promise.all(db.tables.map((table) => table.clear()));
 });
-
-export const addFeeling = (feelingName : string, feelingCategory : string) => {
-  const currentDate = getJustDate(new Date());
-  db.transaction('rw', db.feelingsCollection, async () => {
-    await db
-      .feelingsCollection
-      .add({ content: feelingName, date: currentDate, category: feelingCategory });
-  }).catch((e) => {
-    console.log(e.stack || e);
-  });
-};
 
 export const removeFeeling = (feelingId) => {
   db.transaction('rw', db.feelingsCollection, async () => {
@@ -26,22 +15,37 @@ export const removeFeeling = (feelingId) => {
 
 export const getAllFeelings = async () => {
   const allFeelings = await db.feelingsCollection.toArray();
-  console.log(allFeelings);
   return allFeelings;
 };
 
 export const getFeelingsOnDate = async (date) => {
-  db.transaction('rw', db.feelingsCollection, async () => {
-    const feelingsList = await db.feelingsCollection.where('date').equals(date);
-    return feelingsList;
+  let feelingsList = [];
+  await db.transaction('rw', db.feelingsCollection, async () => {
+    feelingsList = await db.feelingsCollection.where('date').equals(date).toArray();
   }).catch((e) => {
     console.log(e.stack || e);
   });
+  return feelingsList;
 };
 
 export const getFeelingsBetweenDates = async (startDate, endDate) => {
   db.transaction('rw', db.feelingsCollection, async () => {
     const feelingsList = await db.feelingsCollection.where('date').between(startDate, endDate);
     return feelingsList;
+  });
+};
+
+export const addFeeling = async (feelingName : string, feelingCategory : string) => {
+  const currentDate = getJustDate(new Date());
+  const currentDateFeelings = await getFeelingsOnDate(currentDate);
+  const checkFeelings = (feeling) => feeling.content === feelingName;
+  if (currentDateFeelings.some(checkFeelings)) { return; }
+  console.log(getFeelingsOnDate(currentDate));
+  db.transaction('rw', db.feelingsCollection, async () => {
+    await db
+      .feelingsCollection
+      .add({ content: feelingName, date: currentDate, category: feelingCategory });
+  }).catch((e) => {
+    console.log(e.stack || e);
   });
 };
