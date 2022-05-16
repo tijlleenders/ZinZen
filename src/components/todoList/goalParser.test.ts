10438c4fa5367d45628bef98c928c86c3b97002b
GithinjiHans <hanson4e@gmail.com>
2022-05-16 17:34:47 +0300

    Made the unit test for goal parser pass

--- src/components/todoList/goalParser.test.ts
+++ src/components/todoList/goalParser.test.ts
@@ -4,29 +4,38 @@ const goalObject=({
    title: 'Walk 1h daily',
     lang: 'en',
    color:  '#fff'})
+   const newData =(data)=>[...data,goalObject];
 
-const [outputObject,setOutputObject]=useState([])
+//const [outputObject,setOutputObject]=useState([])
 
-function getGoalObject(){}
+function getGoalObject(){
+  if(goalObject.title.indexOf('daily')!==-1){
+    return({
+   newData,
+   suggestion: {repetition: "daily"},
+    });}
+     else{
+      return({
+         newData
+      })
+    }
+}
 
 describe('getGoalObject function',()=>{
       it('should return repetition suggestion : daily',()=>{
-
         function fun(){
           if(goalObject.title.indexOf('daily')!==-1){
-            const newData =(data)=>[...data,goalObject]
-            setOutputObject({
+            return({
            newData,
            suggestion: {repetition: "daily"},
             });}
              else{
-              setOutputObject({
+              return({
                 newData
               })
             }
-            return outputObject;
           }
-
-        expect(getGoalObject()).toBe(fun);
+        const e=fun();
+        expect(getGoalObject()).toEqual(e);
       })
     })
