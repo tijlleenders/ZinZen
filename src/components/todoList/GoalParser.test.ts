5663318a00b09f958668015f099c10626cca35d0
GithinjiHans <hanson4e@gmail.com>
2022-05-21 22:37:35 +0300

    Created a failing unit test for the duration suggestion

--- src/components/todoList/GoalParser.test.ts
+++ src/components/todoList/GoalParser.test.ts
@@ -1,9 +1,9 @@
 const goalObject = ({
-  title: 'Walk 1h daily',
+  title: 'Walk 22h daily',
   lang: 'en',
   color: '#fff',
 });
-   const newData = (data) =>[...data,goalObject];
+   const newData = (data) =>[...data, goalObject];
 
 function getGoalObject() {
   if (goalObject.title.indexOf('daily') !== -1) {
@@ -16,6 +16,8 @@ function getGoalObject() {
     newData,
   });
 }
+
+function timeSuuggestion() {}
 
 describe('getGoalObject function', () => {
   it('should return repetition suggestion : daily', () => {
@@ -33,5 +35,28 @@ describe('getGoalObject function', () => {
     }
     const e = fun();
     expect(getGoalObject()).toEqual(e);
+  });
+});
+
+describe('getTime function', () => {
+  it('should return duration suggestion: num', () => {
+    function time() {
+      const tracker = /(1[0-9]|2[0-4]|[1-9])+(h)/;
+      const label = /1[0-9]|2[0-4]|[0-9]/;
+      const matchGoal = goalObject.title.match(label);
+
+      if (goalObject.title.search(tracker) !== -1) {
+        return ({
+          newData,
+          suggestion: { duration: `${matchGoal} hours` },
+        });
+      }
+
+      return ({
+        newData,
+      });
+    }
+    const a = time();
+    expect(timeSuuggestion()).toEqual(a);
   });
 });
