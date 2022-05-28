158ea0842dce36b97ff9acc975954738ee3bce45
GithinjiHans <hanson4e@gmail.com>
2022-05-28 14:54:28 +0300

    Positioned the zinzen text center

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -3,19 +3,25 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 import { DashboardImagePanel } from '@components/DasboardImagePanel/DashboardImagePanel';
 import { DashboardUserChoicePanel } from '@components/DashboardUserChoicePanel/DashboardUserChoicePanel';
+import { MainHeaderDashboard } from '@components/HeaderDashboard/MainHeaderDashboard';
 
 export const Dashboard = () => (
-  <Container fluid>
+  <div>
     <Row>
-      <Col sm={2} />
-      <Col sm={3}>
-        <DashboardImagePanel />
-      </Col>
-      <Col sm={1} />
-      <Col sm>
-        <DashboardUserChoicePanel />
-      </Col>
-      <Col sm={2} />
+      <MainHeaderDashboard />
     </Row>
-  </Container>
+    <Container fluid>
+      <Row>
+        <Col sm={2} />
+        <Col sm={3}>
+          <DashboardImagePanel />
+        </Col>
+        <Col sm={1} />
+        <Col sm>
+          <DashboardUserChoicePanel />
+        </Col>
+        <Col sm={2} />
+      </Row>
+    </Container>
+  </div>
 );
