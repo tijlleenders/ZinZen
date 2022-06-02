1af898225817e1837c27e4f073283bce5d597202
Tushar Inani <tusharinani93@gmail.com>
2022-06-02 11:37:09 +0530

    Moving serviceWorker into src

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,6 +2,7 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js',
 );
 
+// This will work!
 workbox.routing.registerRoute(
   ({ request }) => request.destination === 'image',
   new workbox.strategies.CacheFirst(),
