f94b25ddc36726bc266074bdde6edcdf3956c11f
Tushar Inani <tusharinani93@gmail.com>
2022-06-02 09:56:10 +0530

    Update: Registering serviceWorker inside index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -14,3 +14,14 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('../service-worker.js')
+    .then((registration) => {
+      console.log('Registration successful, scope is:', registration.scope);
+    })
+    .catch((err) => {
+      console.log('Service worker registration failed, error:', err);
+    });
+}
