fdde0912ba23a705c9c9b79ace9129bd35b62aa3
GithinjiHans <hanson4e@gmail.com>
2022-06-04 00:28:57 +0300

    improved the sw

--- src-sw.js
+++ src-sw.js
@@ -1,4 +1,5 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
 
-console.log('my Adjustments');
-workbox.precaching.precacheAndRoute([]);
+console.log('service worker customized');
+
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
--- workbox-config.js
+++ workbox-config.js
@@ -4,6 +4,5 @@ module.exports = {
 		'**/*.{png,ico,svg,json,js,html,ts,scss,tsx,jpg,jpeg,lock}'
 	],
 	swDest: 'sw.js',
-	swSrc: 'src-sw.js',
-	"injectionPoint": "injectionPoint"
+	"swSrc": "src-sw.js"
 };
