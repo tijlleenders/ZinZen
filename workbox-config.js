75d56e51fe3933ed710e0dbbf62e1f3c798a8fef
GithinjiHans <hanson4e@gmail.com>
2022-06-03 12:14:01 +0300

    Added config for the service worker

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: 'src/',
+	globPatterns: [
+		'**/*.{ts,scss,tsx,png,svg,jpg,jpeg,json}'
+	],
+	swDest: 'src/sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
+};
