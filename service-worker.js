d0de1e9f958eb3546496f29f6119112d6daf66e5
GithinjiHans <hanson4e@gmail.com>
2022-06-03 10:24:49 +0300

    Service worker.

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CacheName = 'v2';
+
+self.addEventListener('install', (e) => {
+  console.log('Service Worker : installed');
+});
+
+self.addEventListener('activate', e => {
+  console.log('service worker: Activated');
+  e.waitUntil(
+    caches.keys().then(CacheNames => {
+      return Promise.all(
+        CacheNames.map(cache => {
+          if (cache !== CacheName) {
+            console.log('serviceW : clearing old cache');
+            return caches.delete(cache);
+          }
+
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  console.log('service worker: fetching');
+  e.respondWith(
+    fetch(e.request)
+    .then(res => {
+      const resClone = res.clone();
+      caches
+      .open(CacheName)
+      .then(cache => {
+        cache.put(e.request, resClone);
+      });
+      return res;
+    }).catch(err => caches.match(e.request).then(res => res))
+  );
+});
