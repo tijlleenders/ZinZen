49ee8fe507e6f17607f56b19fc6d15875915f355
GithinjiHans <hanson4e@gmail.com>
2022-06-04 00:41:48 +0300

    changed the file reference

--- src-sw.js
+++ src-sw.js
@@ -2,4 +2,8 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox
 
 console.log('service worker customized');
 
+workbox.routing.registerRoute(
+    new RegExp('https://zinzen-qqu97eop7-tijlleenders.vercel.app/Home/MyGoals),
+    workbox.strategies.cacheFirst()
+  );
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
