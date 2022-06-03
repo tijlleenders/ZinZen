ac5b38a32e880f835d21aa41bb7dd06df5aa712d
GithinjiHans <hanson4e@gmail.com>
2022-06-03 12:26:35 +0300

    Chenged the service worker

--- main.js
+++
@@ -1,8 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('service-worker.js')
-      .then(reg => console.log('Service Worker registered.'))
-      .catch((error) => console.error(error));
-  });
-}
