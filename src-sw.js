90ff20ef61cb67ec6319c59fcb9841b0096f3154
GithinjiHans <hanson4e@gmail.com>
2022-06-03 22:28:37 +0300

    Added sw dependencies

--- src-sw.js
+++ src-sw.js
@@ -1,4 +1,4 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
-//console.log('my Adjustments');
+console.log('my Adjustments');
 workbox.precaching.precacheAndRoute([]);
