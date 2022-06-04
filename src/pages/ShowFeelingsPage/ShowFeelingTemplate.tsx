0030d9b4129179b45871b7590fac37f46445ef9f
pulkitvyas08 <e19cse284@bennett.edu.in>
2022-06-04 18:19:36 +0530

    Update ShowFeelings design, add notes modals, can update dates now

--- src/pages/ShowFeelingsPage/ShowFeelingTemplate.tsx
+++ src/pages/ShowFeelingsPage/ShowFeelingTemplate.tsx
@@ -1,15 +1,18 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useReducer, useState } from 'react';
 import {
   Button, Nav, Container,
 } from 'react-bootstrap';
 import { useRecoilValue } from 'recoil';
 import { useTranslation } from 'react-i18next';
+import { ChevronDown, ChevronRight, TrashFill, Journal } from 'react-bootstrap-icons';
+import { Modal } from 'react-bootstrap';
 
 import { darkModeState } from '@store';
 import { IFeelingItem } from '@models';
-import { removeFeeling } from '@api/FeelingsAPI';
+import { removeFeeling, addFeelingNote, removeFeelingNote } from '@api/FeelingsAPI';
 import { feelingListType } from '@src/global';
+import { feelingsEmojis } from '@consts/FeelingsList';
 
 import '@translations/i18n';
 import './ShowFeelingsPage.scss';
@@ -28,6 +31,19 @@ interface IProps {
 export const ShowFeelingTemplate: React.FC<IProps> = ({ feelingsListObject, setFeelingsListObject, currentFeelingsList }) => {
   const { t } = useTranslation();
   const darkModeStatus = useRecoilValue(darkModeState);
+  const [showInputModal, setShowInputModal] = useState(false);
+  const [showNotesModal, setShowNotesModal] = useState(false);
+  const [showOptions, setShowOptions] = useState(false);
+  const [selectedFeeling, setSelectedFeeling] = useState<number>();
+  const [selectedFeelingNote, setSelectedFeelingNote] = useState("");
+  const [noteValue, setNoteValue] = useState("");
+  const [, forceUpdate] = useReducer(x => x + 1, 0);
+
+  const handleInputClose = () => setShowInputModal(false);
+  const handleInputShow = () => setShowInputModal(true);
+  const handleNotesClose = () => setShowNotesModal(false);
+  const handleNotesShow = () => setShowNotesModal(true);
+
   return (
     <div>
       <Container fluid>
@@ -35,28 +51,55 @@ export const ShowFeelingTemplate: React.FC<IProps> = ({ feelingsListObject, setF
           {feelingsListObject && Object.keys(feelingsListObject)
             .map(((feelingId: string) => (
               <Button
-                key={feelingsListObject[Number(feelingId)].content}
-                className={
-                darkModeStatus ? 'btn-my-feelings-dark btn-feelings-dark' : 'btn-my-feelings-light btn-feelings-light'
-              }
-                size="lg"
-                onClick={() => {
-                  const numFeelingId = feelingsListObject[Number(feelingId)].id;
-                  if (numFeelingId !== undefined)
-                    removeFeeling(numFeelingId);
-                  else
-                    console.log("Attempting to remove feeling not in the database");
-
-                  let newFeelingsList = currentFeelingsList;
-                  const feelingDate = feelingsListObject[Number(feelingId)].date;
-                  newFeelingsList[feelingDate] = currentFeelingsList[feelingDate].filter(
-                    (feelingOnDate: IFeelingItem) => feelingOnDate.id !== numFeelingId
-                  );
-                  setFeelingsListObject.setFeelingsList({...newFeelingsList});
-                }}
-              >
-                {t(feelingsListObject[Number(feelingId)].content)}
-              </Button>
+                  key={feelingsListObject[Number(feelingId)].content
+                    + feelingsListObject[Number(feelingId)].date}
+                  className={
+                    darkModeStatus ? 'btn-my-feelings-dark btn-feelings-dark' : 'show-btn-my-feelings-light'
+                  }
+                  onClick={() => { 
+                    setShowOptions(!showOptions)
+                    setSelectedFeeling(feelingsListObject[Number(feelingId)].id);
+                  }}
+                  size="lg"
+                >
+                  {feelingsEmojis[feelingsListObject[Number(feelingId)].category]}
+                  <span className="btn-my-feelings__text">{t(feelingsListObject[Number(feelingId)].content)}</span>
+                  <i>
+                    { (showOptions && (selectedFeeling === feelingsListObject[Number(feelingId)].id)) 
+                    ? <ChevronDown />
+                    : <ChevronRight />}
+                  </i>
+                  <br />
+                  {feelingsListObject[Number(feelingId)]?.note && (<span className="btn-my-feelings__note">...</span>)}
+                  {showOptions && (selectedFeeling === feelingsListObject[Number(feelingId)].id) && (
+                    <div className="show-feelings__options">
+                      <TrashFill
+                        onClick={() => {
+                          const numFeelingId = feelingsListObject[Number(feelingId)].id;
+                          if(numFeelingId !== undefined)
+                            removeFeeling(numFeelingId);
+                          else
+                            console.log("Attempting to remove feeling not in the database");
+                          let newFeelingsList = currentFeelingsList;
+                          const feelingDate = feelingsListObject[Number(feelingId)].date;
+                          newFeelingsList[feelingDate] = currentFeelingsList[feelingDate].filter(
+                            (feelingOnDate: IFeelingItem) => feelingOnDate.id !== numFeelingId
+                          );
+                          setFeelingsListObject.setFeelingsList({...newFeelingsList});
+                        }}
+                        size={20}
+                      />
+                      <Journal
+                        onClick={() => {
+                          if (feelingsListObject[Number(feelingId)]?.note)
+                            setSelectedFeelingNote(feelingsListObject[Number(feelingId)]?.note!);
+                          feelingsListObject[Number(feelingId)]?.note ? handleNotesShow() : handleInputShow();
+                        }}
+                        size={20}
+                      />
+                    </div>
+                  )}
+                </Button>
             )))}
         </div>
         <div className="feelings-menu-mobile">
@@ -65,29 +108,109 @@ export const ShowFeelingTemplate: React.FC<IProps> = ({ feelingsListObject, setF
               ((feelingId: string) => (
                 <Button
                   key={feelingsListObject[Number(feelingId)].content
-                  + feelingsListObject[Number(feelingId)].date}
+                    + feelingsListObject[Number(feelingId)].date}
                   className={
-                darkModeStatus ? 'btn-my-feelings-dark btn-feelings-dark' : 'btn-my-feelings-light btn-feelings-light'
-              }
-                  size="lg"
-                  onClick={() => {
-                    const numFeelingId = feelingsListObject[Number(feelingId)].id;
-                    if(numFeelingId !== undefined)
-                      removeFeeling(numFeelingId);
-                    else
-                      console.log("Attempting to remove feeling not in the database");
-                    let newFeelingsList = currentFeelingsList;
-                    const feelingDate = feelingsListObject[Number(feelingId)].date;
-                    newFeelingsList[feelingDate] = currentFeelingsList[feelingDate].filter(
-                      (feelingOnDate: IFeelingItem) => feelingOnDate.id !== numFeelingId
-                    );
-                    setFeelingsListObject.setFeelingsList({...newFeelingsList});
+                    darkModeStatus ? 'btn-my-feelings-dark btn-feelings-dark' : 'show-btn-my-feelings-light'
+                  }
+                  onClick={() => { 
+                    setShowOptions(!showOptions)
+                    setSelectedFeeling(feelingsListObject[Number(feelingId)].id);
                   }}
+                  size="lg"
                 >
-                  {t(feelingsListObject[Number(feelingId)].content)}
+                  {feelingsEmojis[feelingsListObject[Number(feelingId)].category]}
+                  <span className="btn-my-feelings__text">{t(feelingsListObject[Number(feelingId)].content)}</span>
+                  <i>
+                    { (showOptions && (selectedFeeling === feelingsListObject[Number(feelingId)].id)) 
+                    ? <ChevronDown />
+                    : <ChevronRight />}
+                  </i>
+                  <br />
+                  {feelingsListObject[Number(feelingId)]?.note && (<span className="btn-my-feelings__note">...</span>)}
+                  {showOptions && (selectedFeeling === feelingsListObject[Number(feelingId)].id) && (
+                    <div className="show-feelings__options">
+                      <TrashFill
+                        onClick={() => {
+                          const numFeelingId = feelingsListObject[Number(feelingId)].id;
+                          if(numFeelingId !== undefined)
+                            removeFeeling(numFeelingId);
+                          else
+                            console.log("Attempting to remove feeling not in the database");
+                          let newFeelingsList = currentFeelingsList;
+                          const feelingDate = feelingsListObject[Number(feelingId)].date;
+                          newFeelingsList[feelingDate] = currentFeelingsList[feelingDate].filter(
+                            (feelingOnDate: IFeelingItem) => feelingOnDate.id !== numFeelingId
+                          );
+                          setFeelingsListObject.setFeelingsList({...newFeelingsList});
+                        }}
+                        size={20}
+                      />
+                      <Journal
+                        onClick={() => {
+                          if (feelingsListObject[Number(feelingId)]?.note)
+                            setSelectedFeelingNote(feelingsListObject[Number(feelingId)]?.note!);
+                          feelingsListObject[Number(feelingId)]?.note ? handleNotesShow() : handleInputShow();
+                        }}
+                        size={20}
+                      />
+                    </div>
+                  )}
                 </Button>
               )),
             )}
+            <Modal show={showInputModal} onHide={handleInputClose} centered>
+                <Modal.Header closeButton>
+                  <Modal.Title><i>Want to tell more about it?</i></Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <input
+                    type="text"
+                    placeholder="Add a reason"
+                    className="show-feelings__note-input"
+                    value={noteValue}
+                    onChange={(e) => {setNoteValue(e.target.value)}}
+                  />
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button
+                    variant="primary"
+                    onClick={() => {
+                      addFeelingNote(selectedFeeling!, noteValue);
+                      setFeelingsListObject.setFeelingsList(currentFeelingsList);
+                      setNoteValue("");
+                      handleInputClose();
+                    }}
+                    className="show-feelings__modal-button"
+                  >
+                    Done
+                  </Button>
+                </Modal.Footer>
+            </Modal>
+            <Modal show={showNotesModal} onHide={handleNotesClose} centered>
+                <Modal.Body>
+                  <textarea readOnly className="show-feeling__note-textarea" rows={5} cols={32} value={selectedFeelingNote} />
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button 
+                    variant="primary"
+                    onClick={() => {
+                      removeFeelingNote(selectedFeeling!);
+                      setFeelingsListObject.setFeelingsList({...currentFeelingsList});
+                      handleNotesClose();
+                    }}
+                    className="show-feelings__modal-button"
+                  >
+                    Delete
+                  </Button>
+                  <Button
+                    variant="primary"
+                    onClick={handleNotesClose}
+                    className="show-feelings__modal-button"
+                  >
+                    Done
+                  </Button>
+                </Modal.Footer>
+            </Modal>
           </Nav>
         </div>
       </Container>
--- src/pages/ShowFeelingsPage/ShowFeelingsPage.tsx
+++ src/pages/ShowFeelingsPage/ShowFeelingsPage.tsx
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { useRecoilValue } from 'recoil';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router';
 
 import { getAllFeelings } from '@api/FeelingsAPI';
 import { IFeelingItem } from '@models';
 import { darkModeState } from '@store';
 import { ShowFeelingTemplate } from './ShowFeelingTemplate';
 import { feelingListType } from '@src/global';
+import { getDates } from '@utils';
+import addIcon from '@assets/images/GoalsAddIcon.svg';
 
 import './ShowFeelingsPage.scss';
 import './ShowFeelings.scss';
@@ -15,6 +18,7 @@ import './ShowFeelings.scss';
 export const ShowFeelingsPage = () => {
   const darkModeStatus = useRecoilValue(darkModeState);
   const { t } = useTranslation();
+  const navigate = useNavigate();
   const [feelingsList, setFeelingsList] = useState<feelingListType[]>([]);
   useEffect(() => {
     const getData = async () => {
@@ -35,33 +39,56 @@ export const ShowFeelingsPage = () => {
     };
     getData();
   }, []);
+  const dateArr = Object.keys(feelingsList).map((date) => {return date});
+  const dateRangeArr = getDates(new Date(dateArr[0]), new Date());
 
   return (
-    <Container fluid className="slide">
+    <Container fluid className="slide show-feelings__container">
       <Row>
         <Col>
           <h3 className={darkModeStatus ? 'my-feelings-font-dark' : 'my-feelings-font-light'}>{t('showfeelingsmessage')}</h3>
-          {feelingsList !== null && Object.keys(feelingsList).map((date) => (
+          {feelingsList !== null && dateRangeArr.map((date) => (
             <div
-              key={feelingsList[date]}
+              key={date}
               className="show-feelings__list-category"
             >
               <h3 className={darkModeStatus
                 ? 'my-feelings-font-dark'
                 : 'my-feelings-font-light'}
               >
-                {new Date(date).toDateString()}
+                <span
+                  onClick={() => {
+                    navigate('/Home/AddFeelings', {
+                      state: {feelingDate: new Date(date)}
+                    });
+                  }}
+                  style={{ cursor: 'pointer' }}
+                >
+                  {new Date(date).toDateString() === new Date().toDateString() ? 'Today' : new Date(date).toDateString()}
+                </span>
               </h3>
-              <ShowFeelingTemplate
-                key={feelingsList[date]}
-                feelingsListObject={feelingsList[date]}
-                setFeelingsListObject={{ feelingsList, setFeelingsList }}
-                currentFeelingsList={feelingsList}
-              />
+              {feelingsList[date]
+                ? <ShowFeelingTemplate
+                  key={date}
+                  feelingsListObject={feelingsList[date]}
+                  setFeelingsListObject={{ feelingsList, setFeelingsList }}
+                  currentFeelingsList={feelingsList}
+                />
+                : <img
+                    key={date}
+                    src={addIcon}
+                    alt="add-goal"
+                    style={{margin: '5px 0 0 30px', height: '30px', width: '30px'}}
+                    onClick={() => {
+                      navigate('/Home/AddFeelings', {
+                        state: {feelingDate: new Date(date)}
+                      });
+                    }}
+                  />
+              }
             </div>
           ))}
         </Col>
-        <Col sm={1} />
       </Row>
     </Container>
   );
