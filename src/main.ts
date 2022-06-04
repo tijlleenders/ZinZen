308c2a771c0780711ad58ccc8be9f80cd0e90831
GithinjiHans <hanson4e@gmail.com>
2022-06-04 10:34:34 +0300

    Removed the constructor object

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 export function register() {
-    constructor() {
       if ("serviceWorker" in navigator && process.env.NODE_ENV === 'production') {
         navigator.serviceWorker
           .register('sw.js')
           .then(function () {
             console.log("Service Worker Registered");
           });
-      }
     }
   }
