a3b3ca9bb02409bb40c7367c38c7b2100092f29a
GithinjiHans <hanson4e@gmail.com>
2022-06-04 11:15:06 +0300

    Added scope to the sw

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ export default class Reg {
     constructor() {
       if ("serviceWorker" in navigator && process.env.NODE_ENV === 'production') {
         navigator.serviceWorker
-          .register('./Home')
+          .register('sw.js', { scope: '/' })
           .then(function () {
             console.log("Service Worker Registered");
           });
