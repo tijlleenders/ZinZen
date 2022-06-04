430bdcbb2cab5cb4c7e5bc5c045a86b5bd1dee55
GithinjiHans <hanson4e@gmail.com>
2022-06-04 10:04:27 +0300

    used public url

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-//export default class Reg {
-  //  constructor() {
+export default class Reg {
+    constructor() {
       if ("serviceWorker" in navigator && process.env.NODE_ENV === 'production') {
         navigator.serviceWorker
-          .register("sw.js")
+          .register("%PUBLIC_URL%/sw.js")
           .then(function () {
             console.log("Service Worker Registered");
           });
       }
-   // }
- // }
+    }
+  }
 
- // new Reg();
+  new Reg();
