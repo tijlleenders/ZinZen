ad285b62cbe9851576569514e33287693a15c1df
GithinjiHans <hanson4e@gmail.com>
2022-06-04 10:42:20 +0300

    Returned class

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-export function register() {
+export default class Reg {
+    constructor() {
       if ("serviceWorker" in navigator && process.env.NODE_ENV === 'production') {
         navigator.serviceWorker
           .register('sw.js')
           .then(function () {
             console.log("Service Worker Registered");
           });
+      }
     }
   }
+
+  new Reg();
