cb22756160898849c5aa5cdb7fa0c8b5185cd10f
pulkitvyas08 <e19cse284@bennett.edu.in>
2022-06-04 18:17:37 +0530

    Add daterange utility function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -12,3 +12,19 @@ export const truncateContent = (content: string, maxLength = 20) => {
   }
   return content;
 };
+
+Date.prototype.addDays = function(days: number) {
+  var date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
+}
+
+export const getDates = (startDate: Date, stopDate: Date) => {
+  var dateArray = new Array();
+  var currentDate = startDate;
+  while (currentDate <= stopDate) {
+      dateArray.push(new Date (currentDate));
+      currentDate = currentDate.addDays(1);
+  }
+  return dateArray;
+};
