33602ec456e4ad69b002d7150a7e6660731da735
GithinjiHans <hanson4e@gmail.com>
2022-06-04 08:03:00 +0300

    used ts instead

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+export default class Reg {
+    constructor() {
+      if ("serviceWorker" in navigator && process.env.NODE_ENV === 'production') {
+        navigator.serviceWorker
+          .register("sw.js", { scope: "./" })
+          .then(function () {
+            console.log("Service Worker Registered");
+          });
+      }
+    }
+  }
+
+  new Reg();
