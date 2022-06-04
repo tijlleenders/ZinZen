4217c9f7a73be10e413d2f1bdca7ce89ff54eede
GithinjiHans <hanson4e@gmail.com>
2022-06-04 08:32:52 +0300

    removed the scope attribute

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ export default class Reg {
     constructor() {
       if ("serviceWorker" in navigator && process.env.NODE_ENV === 'production') {
         navigator.serviceWorker
-          .register("/sw.js", { scope: "/" })
+          .register("./sw.js")
           .then(function () {
             console.log("Service Worker Registered");
           });
