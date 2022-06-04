98e3e265309d787bfbe8a0a3ca380bd02990f9bf
GithinjiHans <hanson4e@gmail.com>
2022-06-04 10:51:17 +0300

    changed path adress

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ export default class Reg {
     constructor() {
       if ("serviceWorker" in navigator && process.env.NODE_ENV === 'production') {
         navigator.serviceWorker
-          .register('src/index.tsx')
+          .register('./index.tsx')
           .then(function () {
             console.log("Service Worker Registered");
           });
