9cc6adff56c9b53ca8cba3b9f27672799fad36f5
GithinjiHans <hanson4e@gmail.com>
2022-06-04 08:25:45 +0300

    used the whole link

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ export default class Reg {
     constructor() {
       if ("serviceWorker" in navigator && process.env.NODE_ENV === 'production') {
         navigator.serviceWorker
-          .register("./sw.js", { scope: "./" })
+          .register("https://zinzen-fxi1r4z2d-tijlleenders.vercel.app/sw.js", { scope: "./" })
           .then(function () {
             console.log("Service Worker Registered");
           });
