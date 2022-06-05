4742ea1ac705a0be642008657d421527a2e136b3
GithinjiHans <hanson4e@gmail.com>
2022-06-05 22:16:47 +0300

    commited to move to another branch

--- vite.config.ts
+++ vite.config.ts
@@ -25,45 +25,46 @@ export default defineConfig({
   },
   plugins: [
     VitePWA({
+      registerType: 'autoUpdate',
       manifest: {
-        short_name: "ZinZen",
-        name: "ZinZen: Deliver purpose",
+        short_name: 'ZinZen',
+        name: 'ZinZen: Deliver purpose',
         icons: [
-            {
-                src: "favicon.ico",
-                sizes: "64x64 32x32 24x24 16x16",
-                type: "image/x-icon"
-            },
-            {
-                src: "./assets/icons/zinzenlogo-192x192.png",
-                type: "image/png",
-                sizes: "192x192"
-            },
-            {
-                src: "./assets/icons/zinzenlogo-512X512.png",
-                type: "image/png",
-                sizes: "512x512"
-            }
+          {
+            src: 'favicon.ico',
+            sizes: '64x64 32x32 24x24 16x16',
+            type: 'image/x-icon',
+          },
+          {
+            src: './assets/icons/zinzenlogo-192x192.png',
+            type: 'image/png',
+            sizes: '192x192',
+          },
+          {
+            src: './assets/icons/zinzenlogo-512X512.png',
+            type: 'image/png',
+            sizes: '512x512',
+          },
         ],
-        start_url: "./",
-        background_color: "#3367D6",
-        display: "standalone",
-        scope: "./",
+        start_url: './',
+        background_color: '#3367D6',
+        display: 'standalone',
+        scope: './',
         related_applications: [
-            {
-                platform: "webapp",
-                url: "https://zinzen.me/manifest.webmanifest"
-            }
+          {
+            platform: 'webapp',
+            url: 'https://zinzen.me/manifest.webmanifest',
+          },
         ],
-        theme_color: "#3367D6",
-        description: "ZinZen for purpose"
-    },
-    workbox: {
-      globDirectory: './',
-      globPatterns: [
-      '**/*.{png,ico,svg,md,json,js,html,txt,ts,scss,tsx,jpg,jpeg,lock}'
-    ],
-    swDest: 'sw.js',
+        theme_color: '#3367D6',
+        description: 'ZinZen for purpose',
+      },
+      workbox: {
+        globDirectory: './',
+        globPatterns: [
+          '**/*.{png,ico,svg,md,json,js,html,txt,ts,scss,tsx,jpg,jpeg,lock}',
+        ],
+        swDest: 'sw.js',
       },
     }),
   ],
