e20820e29a906dc3617d3a7103b9c9ace3661251
GithinjiHans <hanson4e@gmail.com>
2022-06-06 14:18:25 +0300

    This may solve the icons caching issue

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,16 @@
 import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching';
 
-cleanupOutdatedCaches();
+const cacheName = 'v1';
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request)
+    .then(res => {
+      const resClone = res.clone();
+      caches
+        .open(cacheName)
+        .then(cache => {
+          cache.put(e.request, resClone);
+        });
+      return res;
+    }).catch(err => caches.match(e.request).then(res => res)));
+});
 precacheAndRoute(self.__WB_MANIFEST);
