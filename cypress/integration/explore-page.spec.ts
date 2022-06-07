d2abe74f9175aef9467ac66da08a94fa0ae03100
Tushar Inani <tusharinani93@gmail.com>
2022-06-07 23:52:05 +0530

    CSS checks added

--- cypress/integration/explore-page.spec.ts
+++ cypress/integration/explore-page.spec.ts
@@ -1,11 +1,26 @@
 describe('DashboardUserChoice Test', () => {
-  it('Should have Health and Fitness Goals Card', () => {
+  it('Explore page should have Backgorund color rgb(240, 230, 225)', () => {
     cy.visit('/');
     cy.get('.lang-btn1').contains('English').click();
     cy.get('.theme-choice-btn').first().click();
     cy.contains('I already know ZinZen').click();
     cy.contains('Explore').click();
+    cy.get('#explore-container-light').invoke('css', 'background-color').should('equal', 'rgb(240, 230, 225)');
+  });
 
+  it('Goal Card Should have Backgorund color rgb(237, 199, 183)', () => {
+    cy.get('.goal-row').invoke('css', 'background-color').should('equal', 'rgb(237, 199, 183)');
+  });
+
+  it('Font color of Goal Title should be black', () => {
+    cy.get('.goal-title').invoke('css', 'color').should('equal', 'rgb(0, 0, 0)');
+  });
+
+  it('Should have Goal title in center of card', () => {
+    cy.get('.goal-title').invoke('css', 'text-align').should('equal', 'center');
+  });
+
+  it('Should have Health and Fitness Goals Card', () => {
     cy.contains('Health and Fitness Goals');
   });
 
