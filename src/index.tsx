343e36bd603d8f326644b95969c939a0ab91073c
Tijl Leenders <tijl.leenders@gmail.com>
2022-06-12 07:42:29 +0200

    move src files to /src/service-worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { RecoilRoot } from 'recoil';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorkerRegistration from './service-worker/serviceWorkerRegistration';
 
 import { App } from './App';
 
