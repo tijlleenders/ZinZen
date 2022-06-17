4d4c64c9158b2417ae1c4e85725a4a5fc390ba89
pulkitvyas08 <e19cse284@bennett.edu.in>
2022-06-17 12:38:00 +0530

    Add built directory to ignore list, delete the folder

--- built/src/jest.setup.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("@testing-library/jest-dom");
-require("@testing-library/jest-dom/extend-expect");
-require("regenerator-runtime/runtime");
