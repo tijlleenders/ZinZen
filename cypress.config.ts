53508e093a2a7145ce3b93d53cc75b80a406e647
Tushar Inani <tusharinani93@gmail.com>
2022-06-18 16:57:50 +0530

    cypress update

---
+++ cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:3000", // "https://zinzen.vercel.app/",
+    chromeWebSecurity: false,
+    specPattern: "**/*.spec.ts",
+
+  }
+  ,
+});
