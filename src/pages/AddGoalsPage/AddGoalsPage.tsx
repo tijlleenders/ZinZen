3267317a046b8a956f6a1ad0cffa2b23a65109b3
pulkitvyas08 <e19cse284@bennett.edu.in>
2022-06-23 23:14:37 +0530

    Extend AddGoals page to allow creation of sublist goals

--- src/pages/AddGoalsPage/AddGoalsPage.tsx
+++ src/pages/AddGoalsPage/AddGoalsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import { useRecoilValue } from "recoil";
@@ -10,21 +10,37 @@ import paintBrush from "@assets/images/paintBrush.svg";
 
 import "@translations/i18n";
 import "@components/GoalsComponents/GoalsComponents.scss";
+import { getGoal } from "@src/api/GoalsAPI";
+import { useLocation } from "react-router";
 
-export const AddGoalsPage = () => {
+interface locationProps {
+  goalId: number;
+}
+
+export const AddGoalsPage: React.FC = () => {
   const darkModeStatus = useRecoilValue(darkModeState);
   const { t } = useTranslation();
+  const locationState = useLocation().state as locationProps;
 
   const [selectedColorIndex, setColorIndex] = useState(0);
-
+  const [parentGoalId, setParentGoalId] = useState<number | -1>();
+  const [parentGoalTitle, setParentGoalTitle] = useState("");
   const darkrooms = ["#443027", "#9C4663", "#646464", "#2B517B", " #612854"];
   const lightcolors = [" #EDC7B7", "#AC3B61", " #BAB2BC", " #3B6899", " #8E3379"];
+
+  useEffect(() => {
+    if (locationState) {
+      setParentGoalId(locationState.goalId);
+    } else setParentGoalId(-1);
+    if (parentGoalId !== -1) getGoal(parentGoalId!).then((parentGoal) => setParentGoalTitle(parentGoal.title));
+  }, [parentGoalId]);
 
   const changeColor = () => {
     const newColorIndex = selectedColorIndex + 1;
     if (darkrooms[newColorIndex]) setColorIndex(newColorIndex);
     else setColorIndex(0);
   };
+
   return (
     <div>
       <Container fluid>
@@ -37,7 +53,15 @@ export const AddGoalsPage = () => {
           <div>
             <h2 className={darkModeStatus ? "mygoals-font-dark" : "mygoals-font-light"}>{t("myGoalsMessage")}</h2>
             <div className={darkModeStatus ? "goalsubtext-font-dark" : "goalsubtext-font-light"}>
-              <p>
+              <p style={{ fontStyle: "normal" }}>
+                {parentGoalId !== -1 ? (
+                  <>
+                    for sublist <span style={{ color: "#BA0880" }}>{parentGoalTitle}</span>
+                  </>
+                ) : (
+                  ""
+                )}
+                <br />
                 {t("goalsubtext")}
                 <br /> {t("format")}
               </p>
@@ -57,7 +81,7 @@ export const AddGoalsPage = () => {
           </button>
         </Row>
         <Row>
-          <GoalsForm selectedColorIndex={selectedColorIndex} />
+          <GoalsForm selectedColorIndex={selectedColorIndex} parentGoalId={parentGoalId} />
         </Row>
       </Container>
     </div>
