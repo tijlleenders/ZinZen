46cfe814a05215d20eba3fc58a6675b8450d1f82
pulkitvyas08 <e19cse284@bennett.edu.in>
2022-06-23 23:13:03 +0530

    Update sublist-goal relation to symbolic, add and modify API functions for sublist goal interaction

--- src/models/GoalItem.ts
+++ src/models/GoalItem.ts
@@ -2,11 +2,11 @@ export interface GoalItem {
   id?: number;
   title: string;
   duration: number;
-  sublist?: GoalItem[];
+  sublist?: number[];
   repeat: "Once" | "Daily";
   start: Date | null;
   finish: Date | null;
   createdAt?: Date;
   status: 0 | 1; // 0 = active, 1 = archived
-  parentGoalId?: number;
+  parentGoalId: number | -1;
 }
