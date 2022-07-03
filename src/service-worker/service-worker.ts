1d7af87f3204e6d261b38edcfb312933819739e2
GithinjiHans <hanson4e@gmail.com>
2022-07-03 11:29:49 +0300

    Added nature-environment-goals.jpeg to the cache

--- src/service-worker/service-worker.ts
+++ src/service-worker/service-worker.ts
@@ -60,7 +60,7 @@ registerRoute(
 // precache, in this case same-origin .png requests like those from in public/
 registerRoute(
   // Add in any other file extensions or routing criteria as needed.
-  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith(".jpg"),
+  ({ url }) => url.origin === self.location.origin && (url.pathname.endsWith(".jpg") || url.pathname.endsWith(".jpeg")),
   // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new CacheFirst({
     cacheName: "images",
