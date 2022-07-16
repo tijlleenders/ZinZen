4ad9eb9c31fc86f046740d7844b6039c290fc5f9
Tushar Inani <tusharinani93@gmail.com>
2022-07-16 10:58:53 +0530

    Added: Cypress Test for mobile mode

--- cypress/integration/Feelings.spec.ts
+++ cypress/integration/Feelings.spec.ts
@@ -54,3 +54,38 @@ describe("Desktop Mode Tests", () => {
     cy.contains("Joyful").should("not.exist");
   });
 });
+
+describe("Mobile Mode Tests", () => {
+  it("Should show expand icon adjacent to all 6 categories", () => {
+    cy.visit("/");
+    cy.get(".lang-btn1").contains("English").click();
+    cy.get(".theme-choice-btn").first().click();
+    cy.contains("Skip Intro").click();
+    cy.get('[alt="Zinzen Logo"]').click();
+    cy.get(".add-icon").eq(1).click();
+    cy.get(".feelings-expand-btw-light").should("have.length", 6);
+  });
+  it("Should display popup modal when clicked on expand icon of a feeling", () => {
+    cy.viewport("iphone-6");
+    cy.get(".feelings-expand-btw-light").eq(0).click();
+  });
+  it("Should have input field for custom feeling", () => {
+    cy.get("#myfeelings-custom-feeling-input").should("exist");
+  });
+  it("Should have feelings to choose from", () => {
+    cy.get(".feelingOption-name").should("have.length.at.least", 1);
+  });
+  it("Should have add note input field", () => {
+    cy.get("#myfeelings-note-input").should("exist");
+  });
+  it("should be able to add feeling (Loved) with note", () => {
+    cy.get(".feelingOption-name").contains("Loved").click({ force: true });
+    cy.get("#myfeelings-note-input").type("This is mobile test note");
+    cy.contains("Done").click();
+  });
+  it("should show the added feeling and ellipsis on MyFeelings Page", () => {
+    cy.viewport("iphone-6");
+    cy.contains("Loved");
+    cy.contains("...");
+  });
+});
