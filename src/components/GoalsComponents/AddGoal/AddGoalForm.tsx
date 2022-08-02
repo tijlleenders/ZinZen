fe90ca21d7432d02ba098cbff41c0e734c2ab901
GithinjiHans <hanson4e@gmail.com>
2022-08-02 23:18:52 +0300

    created the www.x.tld handler

--- src/components/GoalsComponents/AddGoal/AddGoalForm.tsx
+++ src/components/GoalsComponents/AddGoal/AddGoalForm.tsx
@@ -65,7 +65,7 @@ export const AddGoalForm: React.FC<AddGoalFormProps> = ({ goalId, setShowAddGoal
     setGoalRepeats(null);
   }
   function handleGoalLink() {
-    const detector = /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\\/~+#-]*[\w@?^=%&\\/~+#-])/;
+    const detector = /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\\/~+#-]*[\w@?^=%&\\/~+#-])|([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\\/~+#-]*[\w@?^=%&\\/~+#-])/;
     const linkIndex = formInputData.inputGoal.search(detector);
     if (linkIndex !== -1) {
       const link = formInputData.inputGoal.slice(linkIndex).split(" ")[0];
