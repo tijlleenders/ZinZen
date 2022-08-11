c43bca7af8b6752705bd671c9858b55379996fb7
GithinjiHans <hanson4e@gmail.com>
2022-08-12 01:08:08 +0300

    removed useless Button import

--- src/components/GoalsComponents/UpdateGoal/UpdateGoal.tsx
+++ src/components/GoalsComponents/UpdateGoal/UpdateGoal.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Container, Row, Button } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import { useRecoilValue } from "recoil";
 
