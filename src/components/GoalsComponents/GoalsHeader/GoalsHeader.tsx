ddc8225f0ce0e6103f27a56b5b89847d8b481dc8
Tushar Inani <tusharinani93@gmail.com>
2022-08-17 22:32:02 +0530

    Integrated: Suggestion Pop up modal

--- src/components/GoalsComponents/GoalsHeader/GoalsHeader.tsx
+++ src/components/GoalsComponents/GoalsHeader/GoalsHeader.tsx
@@ -1,6 +1,6 @@
 // @ts-nocheck
-import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Navbar, Nav, Modal, Tabs, Tab } from "react-bootstrap";
 import { useRecoilValue } from "recoil";
 import { useNavigate } from "react-router-dom";
 
@@ -11,9 +11,11 @@ import ZinZenTextLight from "@assets/images/LogoTextLight.svg";
 import ZinZenTextDark from "@assets/images/LogoTextDark.svg";
 import ArrowIcon from "@assets/images/ArrowIcon.svg";
 import LogoGradient from "@assets/images/LogoGradient.png";
+import { getAllArchivedGoals } from "@src/api/GoalsAPI";
 
 import "@translations/i18n";
 import "@components/HeaderDashboard/HeaderDashboard.scss";
+import { GoalItem } from "@src/models/GoalItem";
 
 interface GoalsHeaderProps {
   popFromHistory: () => void
@@ -21,6 +23,35 @@ interface GoalsHeaderProps {
 export const GoalsHeader:React.FC<GoalsHeaderProps> = ({ popFromHistory }) => {
   const navigate = useNavigate();
   const darkModeStatus = useRecoilValue(darkModeState);
+
+  const [showSuggestionsModal, setShowSuggestionsModal] = useState(false);
+  const [archiveGoals, setArchiveGoals] = useState<GoalItem[]>([]);
+
+  const getArchiveGoals = (isArchiveTab: boolean) => (archiveGoals.length > 0 ?
+    archiveGoals.map((goal) => (
+      <div key={`my-archive-${goal.id}`} className="suggestions-goal-name">
+        <p style={{ marginBottom: 0, padding: "2%" }}>{goal.title}</p>
+      </div>
+    ))
+    : (
+      <div style={{ textAlign: "center" }} className="suggestions-goal-name">
+        <p style={{ marginBottom: 0, padding: "2%" }}>
+          {
+            isArchiveTab ? "Sorry, No Archived Goals" : "Sorry, No Public Goals"
+          }
+        </p>
+      </div>
+    ));
+
+  useEffect(() => {
+    if (window.location.href.includes("AddGoals")) {
+      (async () => {
+        const goals: GoalItem[] = await getAllArchivedGoals();
+        setArchiveGoals(goals);
+      })();
+    }
+  }, []);
+
   return (
     <div className={darkModeStatus ? "positioning-dark" : "positioning-light"}>
       <Navbar collapseOnSelect expand="lg">
@@ -58,10 +89,10 @@ export const GoalsHeader:React.FC<GoalsHeaderProps> = ({ popFromHistory }) => {
           <Nav className="navbar-custom" />
         </Navbar.Collapse>
         { window.location.href.includes("AddGoals") ? (
-          <div id="goal-suggestion-btn">
-            <img src={LogoGradient} />
+          <button type="button" id="goal-suggestion-btn" onClick={() => { setShowSuggestionsModal(true); }}>
+            <img alt="create-goals-suggestion" src={LogoGradient} />
             <div>?</div>
-          </div>
+          </button>
         )
           : (
             <img
@@ -75,6 +106,34 @@ export const GoalsHeader:React.FC<GoalsHeaderProps> = ({ popFromHistory }) => {
             />
           )}
       </Navbar>
+      <Modal
+        id="suggestions-modal"
+        show={showSuggestionsModal}
+        onHide={() => setShowSuggestionsModal(false)}
+        centered
+        autoFocus={false}
+      >
+
+        <Modal.Body id="suggestions-modal-body">
+          <button type="button" id="suggestions-modal-icon" onClick={() => { setShowSuggestionsModal(true); }}>
+            <img alt="create-goals-suggestion" src={LogoGradient} />
+            <div>?</div>
+          </button>
+          <Tabs
+            defaultActiveKey="My Archive"
+            id="suggestions"
+            className="mb-3"
+            justify
+          >
+            <Tab eventKey="My Archive" title="My Archive">
+              {getArchiveGoals(true)}
+            </Tab>
+            <Tab eventKey="Public Goals" title="Public Goals">
+              {getArchiveGoals(false)}
+            </Tab>
+          </Tabs>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 };
