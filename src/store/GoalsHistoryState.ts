5668f636198520625d2793eb85984a136dbdd761
WFWA TECH <tusharinani93@gmail.com>
2022-09-14 22:23:12 +0530

    Created: Selectors to manage states of goal components

--- src/store/GoalsHistoryState.ts
+++ src/store/GoalsHistoryState.ts
@@ -23,3 +23,48 @@ export const goalsHistory = atom({
   key: "goalsHistory",
   default: [] as ISubGoalHistory[],
 });
+
+export const addInGoalsHistory = selector({
+  key: "addGoalInGoalsHistory",
+  get: ({ get }) => get(goalsHistory),
+  set: ({ get, set }, goal) => {
+    set(goalsHistory, [...get(goalsHistory), ({
+      goalID: goal.id || -1,
+      goalColor: goal.goalColor || "#ffffff",
+      goalTitle: goal.title || "",
+    })]);
+    set(displayGoalId, goal.id || -1);
+  }
+});
+
+export const resetGoalsHistory = selector({
+  key: "resetGoalsHistory",
+  get: ({ get }) => get(goalsHistory),
+  set: ({ set }) => {
+    set(goalsHistory, []);
+    set(displayGoalId, -1);
+  }
+});
+
+export const popFromGoalsHistory = selector({
+  key: "popFromGoalsHistory",
+  get: ({ get }) => get(goalsHistory),
+  set: ({ get, set }, index) => {
+    if (get(displayAddGoal)?.open) {
+      set(displayAddGoal, null);
+    } else if (get(displayUpdateGoal)?.open) {
+      set(displayUpdateGoal, null);
+    } else {
+      const currentState = get(goalsHistory).slice(0, index === -1 ? -1 : index + 1);
+      if (currentState.length === 0) {
+        set(displayGoalId, -1);
+        set(displayAddGoal, null);
+        set(displayUpdateGoal, null);
+      } else {
+        const item = currentState.slice(-1)[0];
+        set(displayGoalId, item.goalID);
+      }
+      set(goalsHistory, [...currentState]);
+    }
+  }
+});
