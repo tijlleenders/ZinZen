620aa7153642eed766a2bab0eeb33d7718330cb9
WFWA TECH <tusharinani93@gmail.com>
2022-09-28 21:07:09 +0530

    Fix: ESLint warnings

--- src/components/GoalsComponents/InputGoal.tsx
+++ src/components/GoalsComponents/InputGoal.tsx
@@ -1,75 +1,74 @@
-import React, { useEffect, useState } from 'react'
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import React, { useEffect, useState } from "react";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 
-import { darkModeState } from '@src/store';
-import ITagExtractor, { ITagIndices } from '@src/Interfaces/ITagExtractor';
-import { useTranslation } from 'react-i18next';
-import { TagsExtractor } from '@src/helpers/TagsExtractor';
-import { extractedTitle, inputGoalTags } from '@src/store/GoalsHistoryState';
+import { darkModeState } from "@src/store";
+import ITagExtractor, { ITagIndices } from "@src/Interfaces/ITagExtractor";
+import { useTranslation } from "react-i18next";
+import { TagsExtractor } from "@src/helpers/TagsExtractor";
+import { extractedTitle, inputGoalTags } from "@src/store/GoalsHistoryState";
 
 interface IGoalTagsProps {
   goalInput: string,
   selectedColor: string,
   goalLang: string
 }
-const InputGoal: React.FC<IGoalTagsProps> = ({ 
+const InputGoal: React.FC<IGoalTagsProps> = ({
   goalInput,
   selectedColor,
-  goalLang, 
+  goalLang,
 }) => {
-    const { t } = useTranslation();
- 
-    const darkModeStatus = useRecoilValue(darkModeState);
-    const [formInputData, setFormInputData] = useState(goalInput);
-    const [magicIndices, setMagicIndices] = useState<ITagIndices[]>([]);
-    const setGoalTitle = useSetRecoilState(extractedTitle);
-    const [goalTags, setGoalTags] = useRecoilState(inputGoalTags);
-  
+  const { t } = useTranslation();
 
-    const handleTagClick = (tagType: string) => {
-      if(goalTags) {
-        const tmpTags = {...goalTags};
-        let tmpString = formInputData;
-        const index = magicIndices.findIndex((ele) => ele.word === tagType);
-        
-        let nextIndex = index + 1;
-        while (nextIndex < magicIndices.length && magicIndices[nextIndex].index === magicIndices[index].index) { nextIndex += 1; }
-        
-        if (index + 1 === magicIndices.length) {
-          tmpString = tmpString.slice(0, magicIndices[index]?.index);
-        } else {
-          tmpString = `${tmpString.slice(0, magicIndices[index]?.index).trim()} ${tmpString.slice(magicIndices[nextIndex].index).trim()}`;
-        }
-        if (tagType === "duration" && goalTags.duration)  {
-          tmpTags.duration = null;
-        } else if (tagType === "repeats" && goalTags.repeats) {
-          tmpTags.repeats = null;
-        } else if (tagType === "link" && goalTags.link) {
-          tmpTags.link = null;
-        } else if (tagType === "start" && goalTags.start) {
-          tmpTags.start = null;
-        } else if (tagType === "due" && goalTags.due) {
-          tmpTags.due = null;
-        } else if (tagType === "startTime" && goalTags.startTime) {
-          tmpTags.startTime = null;
-        } else if (tagType === "endTime" && goalTags.endTime) {
-          tmpTags.endTime = null;
-        }
-        setGoalTags({...tmpTags})
-        setFormInputData(tmpString.trim());
+  const darkModeStatus = useRecoilValue(darkModeState);
+  const [formInputData, setFormInputData] = useState(goalInput);
+  const [magicIndices, setMagicIndices] = useState<ITagIndices[]>([]);
+  const setGoalTitle = useSetRecoilState(extractedTitle);
+  const [goalTags, setGoalTags] = useRecoilState(inputGoalTags);
+
+  const handleTagClick = (tagType: string) => {
+    if (goalTags) {
+      const tmpTags = { ...goalTags };
+      let tmpString = formInputData;
+      const index = magicIndices.findIndex((ele) => ele.word === tagType);
+
+      let nextIndex = index + 1;
+      while (nextIndex < magicIndices.length && magicIndices[nextIndex].index === magicIndices[index].index) { nextIndex += 1; }
+
+      if (index + 1 === magicIndices.length) {
+        tmpString = tmpString.slice(0, magicIndices[index]?.index);
+      } else {
+        tmpString = `${tmpString.slice(0, magicIndices[index]?.index).trim()} ${tmpString.slice(magicIndices[nextIndex].index).trim()}`;
       }
-    };
-    
-    useEffect(() => {
-      const output: ITagExtractor = TagsExtractor(formInputData);
-      if (output.occurences.length > 0) setGoalTitle(formInputData.slice(0, output.occurences[0].index));
-      else setGoalTitle(formInputData.trim());
-      setGoalTags(output.tags);
-      setMagicIndices(output.occurences);
-    }, [formInputData]);
+      if (tagType === "duration" && goalTags.duration) {
+        tmpTags.duration = null;
+      } else if (tagType === "repeats" && goalTags.repeats) {
+        tmpTags.repeats = null;
+      } else if (tagType === "link" && goalTags.link) {
+        tmpTags.link = null;
+      } else if (tagType === "start" && goalTags.start) {
+        tmpTags.start = null;
+      } else if (tagType === "due" && goalTags.due) {
+        tmpTags.due = null;
+      } else if (tagType === "startTime" && goalTags.startTime) {
+        tmpTags.startTime = null;
+      } else if (tagType === "endTime" && goalTags.endTime) {
+        tmpTags.endTime = null;
+      }
+      setGoalTags({ ...tmpTags });
+      setFormInputData(tmpString.trim());
+    }
+  };
+
+  useEffect(() => {
+    const output: ITagExtractor = TagsExtractor(formInputData);
+    if (output.occurences.length > 0) setGoalTitle(formInputData.slice(0, output.occurences[0].index));
+    else setGoalTitle(formInputData.trim());
+    setGoalTags(output.tags);
+    setMagicIndices(output.occurences);
+  }, [formInputData]);
   return (
     <>
-     <div>
+      <div>
         <input
           autoComplete="off"
           className={darkModeStatus ? "addtask-dark" : "addtask-light"}
@@ -81,114 +80,114 @@ const InputGoal: React.FC<IGoalTagsProps> = ({
           // eslint-disable-next-line jsx-a11y/no-autofocus
           autoFocus
           onChange={(e) => setFormInputData(e.target.value)}
-          />
-     </div>
-     <div className="tags">
-      <button
-      type="button"
-      style={
+        />
+      </div>
+      <div className="tags">
+        <button
+          type="button"
+          style={
         darkModeStatus
-        ? { backgroundColor: selectedColor }
-        : { backgroundColor: selectedColor }
-      }
-      className="language"
-      >
-      {goalLang}
-      </button>
-
-      <button
-      type="button"
-      style={
-          darkModeStatus
           ? { backgroundColor: selectedColor }
           : { backgroundColor: selectedColor }
       }
-      className={goalTags?.start?.value ? "form-tag" : "blank"}
-      onClick={() => { handleTagClick("start"); }}
-      >
-      {`Start ${goalTags?.start?.value.toLocaleDateString()}${goalTags?.startTime?.value ? "" : `, ${goalTags?.start?.value?.toTimeString().slice(0, 5)}`}`}
-      </button>
-
-      <button
-      type="button"
-      style={
-          darkModeStatus
-          ? { backgroundColor: selectedColor }
-          : { backgroundColor: selectedColor }
-        }
-        className={goalTags?.startTime?.value ? "form-tag" : "blank"}
-        onClick={() => { handleTagClick("startTime"); }}
+          className="language"
         >
-      {`After ${goalTags?.startTime?.value}:00`}
-      </button>
+          {goalLang}
+        </button>
 
-      <button
-      type="button"
-      style={
+        <button
+          type="button"
+          style={
           darkModeStatus
-          ? { backgroundColor: selectedColor }
-          : { backgroundColor: selectedColor }
-        }
-        className={goalTags?.due?.value ? "form-tag" : "blank"}
-        onClick={() => { handleTagClick("due"); }}
+            ? { backgroundColor: selectedColor }
+            : { backgroundColor: selectedColor }
+      }
+          className={goalTags?.start?.value ? "form-tag" : "blank"}
+          onClick={() => { handleTagClick("start"); }}
         >
-      {`Due ${goalTags?.due?.value.toLocaleDateString()}${goalTags?.endTime?.value ? "" : `, ${goalTags?.due?.value?.toTimeString().slice(0, 5)}`}`}
-      </button>
+          {`Start ${goalTags?.start?.value.toLocaleDateString()}${goalTags?.startTime?.value ? "" : `, ${goalTags?.start?.value?.toTimeString().slice(0, 5)}`}`}
+        </button>
 
-      <button
-      type="button"
-      style={
+        <button
+          type="button"
+          style={
           darkModeStatus
-          ? { backgroundColor: selectedColor }
-          : { backgroundColor: selectedColor }
+            ? { backgroundColor: selectedColor }
+            : { backgroundColor: selectedColor }
         }
-        className={goalTags?.endTime?.value ? "form-tag" : "blank"}
-        onClick={() => { handleTagClick("endTime"); }}
+          className={goalTags?.startTime?.value ? "form-tag" : "blank"}
+          onClick={() => { handleTagClick("startTime"); }}
         >
-      {`Before ${goalTags?.endTime?.value}:00`}
-      </button>
+          {`After ${goalTags?.startTime?.value}:00`}
+        </button>
 
-      <button
-      type="button"
-      style={
+        <button
+          type="button"
+          style={
+          darkModeStatus
+            ? { backgroundColor: selectedColor }
+            : { backgroundColor: selectedColor }
+        }
+          className={goalTags?.due?.value ? "form-tag" : "blank"}
+          onClick={() => { handleTagClick("due"); }}
+        >
+          {`Due ${goalTags?.due?.value.toLocaleDateString()}${goalTags?.endTime?.value ? "" : `, ${goalTags?.due?.value?.toTimeString().slice(0, 5)}`}`}
+        </button>
+
+        <button
+          type="button"
+          style={
+          darkModeStatus
+            ? { backgroundColor: selectedColor }
+            : { backgroundColor: selectedColor }
+        }
+          className={goalTags?.endTime?.value ? "form-tag" : "blank"}
+          onClick={() => { handleTagClick("endTime"); }}
+        >
+          {`Before ${goalTags?.endTime?.value}:00`}
+        </button>
+
+        <button
+          type="button"
+          style={
         darkModeStatus
-        ? { backgroundColor: selectedColor }
-        : { backgroundColor: selectedColor }
+          ? { backgroundColor: selectedColor }
+          : { backgroundColor: selectedColor }
       }
-      className={goalTags?.duration?.value ? "form-tag" : "blank"}
-      onClick={() => { handleTagClick("duration"); }}
-      >
-      {`${goalTags?.duration?.value} hours`}
-      </button>
-
-      <button
-      type="button"
-      style={
-          darkModeStatus
-          ? { backgroundColor: selectedColor }
-          : { backgroundColor: selectedColor }
-        }
-        className={goalTags?.repeats?.value ? "form-tag" : "blank"}
-        onClick={() => { handleTagClick("repeats"); }}
+          className={goalTags?.duration?.value ? "form-tag" : "blank"}
+          onClick={() => { handleTagClick("duration"); }}
         >
-      {goalTags?.repeats?.value}
-      </button>
+          {`${goalTags?.duration?.value} hours`}
+        </button>
 
-      <button
-      type="button"
-      style={
+        <button
+          type="button"
+          style={
           darkModeStatus
-          ? { backgroundColor: selectedColor }
-          : { backgroundColor: selectedColor }
+            ? { backgroundColor: selectedColor }
+            : { backgroundColor: selectedColor }
         }
-        className={goalTags?.link?.value ? "form-tag" : "blank"}
-      onClick={() => { handleTagClick("link"); }}
-      >
-      URL
-      </button>
-     </div>
+          className={goalTags?.repeats?.value ? "form-tag" : "blank"}
+          onClick={() => { handleTagClick("repeats"); }}
+        >
+          {goalTags?.repeats?.value}
+        </button>
+
+        <button
+          type="button"
+          style={
+          darkModeStatus
+            ? { backgroundColor: selectedColor }
+            : { backgroundColor: selectedColor }
+        }
+          className={goalTags?.link?.value ? "form-tag" : "blank"}
+          onClick={() => { handleTagClick("link"); }}
+        >
+          URL
+        </button>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default InputGoal;
