dacf121769ba69e385717627481d68ed8b6b5eca
Eric Githinji <ericgithinji@gmail.com>
2022-10-05 13:39:38 +0300

    Update artifacts.

--- pkg/scheduler_bg.wasm.d.ts
+++ pkg/scheduler_bg.wasm.d.ts
@@ -4,7 +4,4 @@ export const memory: WebAssembly.Memory;
 export function schedule(a: number, b: number): void;
 export function __wbindgen_malloc(a: number): number;
 export function __wbindgen_realloc(a: number, b: number, c: number): number;
-export const __wbindgen_export_2: WebAssembly.Table;
 export function __wbindgen_add_to_stack_pointer(a: number): number;
-export function __externref_table_dealloc(a: number): void;
-export function __wbindgen_start(): void;
