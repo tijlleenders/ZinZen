7b09a7968dc55862ab362e830e1d6f9b097194d8
WFWA TECH <tusharinani93@gmail.com>
2022-10-19 18:17:41 +0530

    Update: handling duration and repeats value crash

--- src/helpers/TagsExtractor.ts
+++ src/helpers/TagsExtractor.ts
@@ -4,58 +4,68 @@ import { goalLinkHandler } from "./GoalLinkHandler";
 import { goalRepeatHandler } from "./GoalRepeatHandler";
 import { goalTimingHandler } from "./GoalTimingHandler";
 
-export function TagsExtractor (inputGoal: string) {
+export function TagsExtractor(inputGoal: string) {
+  const magicIndices : ITagIndices[] = [];
 
-    function handleTiming() {
-        const handlerOutput = goalTimingHandler(inputGoal);
+  const tempData : ITags = {
+    start: null,
+    due: null,
+    startTime: null,
+    endTime: null,
+    link: null,
+    duration: null,
+    repeats: null
+  };
 
-        tempData.start = handlerOutput.start;
-        tempData.due = handlerOutput.end;
-        tempData.startTime = handlerOutput.startTime;
-        tempData.endTime = handlerOutput.endTime;
+  function handleTiming() {
+    const handlerOutput = goalTimingHandler(inputGoal);
 
-        if (handlerOutput.start) { magicIndices.push({ word: "start", index: handlerOutput.start.index }); }
-        if (handlerOutput.end) { magicIndices.push({ word: "due", index: handlerOutput.end.index }); }
-        if (handlerOutput.startTime) { magicIndices.push({ word: "startTime", index: handlerOutput.startTime.index }); }
-        if (handlerOutput.endTime) { magicIndices.push({ word: "endTime", index: handlerOutput.endTime.index }); }
+    tempData.start = handlerOutput.start;
+    tempData.due = handlerOutput.end;
+    tempData.startTime = handlerOutput.startTime;
+    tempData.endTime = handlerOutput.endTime;
 
-        return magicIndices;
+    if (handlerOutput.start) { magicIndices.push({ word: "start", index: handlerOutput.start.index }); }
+    if (handlerOutput.end) { magicIndices.push({ word: "due", index: handlerOutput.end.index }); }
+    if (handlerOutput.startTime) { magicIndices.push({ word: "startTime", index: handlerOutput.startTime.index }); }
+    if (handlerOutput.endTime) { magicIndices.push({ word: "endTime", index: handlerOutput.endTime.index }); }
+
+    return magicIndices;
+  }
+  function handleGoalLink() {
+    const handlerOutput = goalLinkHandler(inputGoal);
+    tempData.link = handlerOutput.value;
+    if (handlerOutput.value) { magicIndices.push({ word: "link", index: handlerOutput.value.index }); }
+    return handlerOutput.value ? [{ word: "link", index: handlerOutput.value.index }] : [];
+  }
+  function handleGoalDuration() {
+    let handlerOutput = goalDurationHandler(inputGoal);
+    let begin = -1;
+    if (handlerOutput.value) {
+      begin = handlerOutput.value.index;
     }
-    function handleGoalLink() {
-        const handlerOutput = goalLinkHandler(inputGoal);
-        tempData.link = handlerOutput.value;
-        if(handlerOutput.value) {magicIndices.push({word: "link", index:handlerOutput.value.index})};
-        return handlerOutput.value ? [{ word: "link", index: handlerOutput.value.index }] : [];
+    if (tempData.repeats) {
+      if (begin > tempData.repeats.index && begin <= tempData.repeats.endIndex) {
+        handlerOutput = { status: false, value: null };
+      }
     }
-    function handleGoalDuration() {
-        const handlerOutput = goalDurationHandler(inputGoal);
-        tempData.duration = handlerOutput.value;
-        if(handlerOutput.value) {magicIndices.push({word: "duration", index:handlerOutput.value.index})};
-        return handlerOutput.value ? [{ word: "duration", index: handlerOutput.value.index }] : [];
-    }
-    function handleGoalRepeat() {
-        const handlerOutput = goalRepeatHandler(inputGoal);
-        tempData.repeats = handlerOutput.value;
-        if(handlerOutput.value) {magicIndices.push({word: "repeats", index:handlerOutput.value.index})};
-        return handlerOutput.value ? [{ word: "repeats", index: handlerOutput.value.index }] : [];
-    }  
+    tempData.duration = handlerOutput.value;
+    if (handlerOutput.value) { magicIndices.push({ word: "duration", index: handlerOutput.value.index }); }
+    return handlerOutput.value ? [{ word: "duration", index: handlerOutput.value.index }] : [];
+  }
+  function handleGoalRepeat() {
+    const handlerOutput = goalRepeatHandler(inputGoal);
+    tempData.repeats = handlerOutput.value;
+    if (handlerOutput.value) { magicIndices.push({ word: "repeats", index: handlerOutput.value.index }); }
+    return handlerOutput.value ? [{ word: "repeats", index: handlerOutput.value.index }] : [];
+  }
 
-    let tempData : ITags = { 
-        start : null,
-        due : null,
-        startTime : null,
-        endTime : null,
-        link : null,
-        duration : null,
-        repeats : null
-    };
-    let magicIndices : ITagIndices[] = [];
-    handleTiming();
-    handleGoalLink();
-    handleGoalDuration();
-    handleGoalRepeat();
-    
-    magicIndices.sort((a, b) => a.index - b.index);
+  handleTiming();
+  handleGoalLink();
+  handleGoalRepeat();
+  handleGoalDuration();
 
-    return { tags: tempData, occurences: magicIndices}
+  magicIndices.sort((a, b) => a.index - b.index);
+
+  return { tags: tempData, occurences: magicIndices };
 }
