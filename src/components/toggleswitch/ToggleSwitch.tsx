c58d33d9b84db624757d0bba79fdd424f38c59a0
WFWA TECH <tusharinani93@gmail.com>
2022-11-05 20:00:27 +0530

    Fix: ESlint issues

--- src/components/toggleswitch/ToggleSwitch.tsx
+++ src/components/toggleswitch/ToggleSwitch.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import React from "react";
 
 import "./ToggleSwitch.scss";
 
--- src/pages/FAQPage/FAQPage.tsx
+++ src/pages/FAQPage/FAQPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Container, div } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { useRecoilValue } from "recoil";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
