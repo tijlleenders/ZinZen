9c896cdb5ca684744527fba892799521f5267ebd
WFWA TECH <tusharinani93@gmail.com>
2022-11-05 17:42:44 +0530

    Removed: Old code of Dashboard

--- src/components/DasboardImagePanel/DashboardImagePanel.tsx
+++
@@ -1,7 +0,0 @@
-// @ts-nocheck
-import React from "react";
-
-import BookIcon from "@assets/images/bookicon.svg";
-import "./DashboardImagePanel.scss";
-
-export const DashboardImagePanel = () => <img src={BookIcon} alt="Book Icon" className="book-icon-dashboard" />;
