0dd32bc74a457530a5db8df7e1fe7d1fd17ee3b2
WFWA TECH <tusharinani93@gmail.com>
2022-12-02 20:14:59 +0530

    update: darktheme colors for querypage

--- src/pages/QueryPage/QueryPage.tsx
+++ src/pages/QueryPage/QueryPage.tsx
@@ -4,21 +4,23 @@ import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router";
 import { queryStyle } from "@src/constants/booleanScreen";
 import { LandingHeader } from "@components/HeaderDashboard/LandingHeader";
+import { useRecoilValue } from "recoil";
+import { darkModeState } from "@src/store";
 
 export const QueryPage = () => {
   const navigate = useNavigate();
   const { t } = useTranslation();
-
+  const darkModeStatus = useRecoilValue(darkModeState);
   return (
     <div id="query-container">
       <LandingHeader avatar={null} />
       <div style={{ ...queryStyle.main }}>
-        <p className="landing-about" style={{ paddingTop: "100px", margin: 0 }}>
+        <p className={`landing-about${darkModeStatus ? "-dark" : ""}`} style={{ paddingTop: "100px", margin: 0 }}>
           <span> a platform for </span>
-          <span style={{ color: "#C6441D" }}>self-actualization </span>
+          <span style={{ color: darkModeStatus ? "#AB9ED8" : "#C6441D" }}>self-actualization </span>
           <br style={{ marginTop: "5px" }} />
           <span>and </span>
-          <span style={{ color: "#C6441D" }}>collaboration</span>
+          <span style={{ color: darkModeStatus ? "#AB9ED8" : "#C6441D" }}>collaboration</span>
         </p>
         <button
           type="button"
