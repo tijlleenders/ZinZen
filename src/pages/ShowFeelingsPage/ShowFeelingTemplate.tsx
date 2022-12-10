149a96379f6ad20ee7deb95aa61ab1e8f3f59b1b
WFWA TECH <tusharinani93@gmail.com>
2022-12-10 14:09:02 +0530

    silenced: eslint warning

--- src/pages/ShowFeelingsPage/ShowFeelingTemplate.tsx
+++ src/pages/ShowFeelingsPage/ShowFeelingTemplate.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 // @ts-nocheck
 /* eslint-disable react/prop-types */
 import React, { useState } from "react";
