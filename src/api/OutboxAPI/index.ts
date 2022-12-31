e3882d8d04e1e7cef5ec3da330820221845d4ed1
Tushar <tusharinani93@gmail.com>
2022-12-31 22:31:49 +0530

    created: api for deleting outbox

--- src/api/OutboxAPI/index.ts
+++ src/api/OutboxAPI/index.ts
@@ -1,7 +1,7 @@
 import { db } from "@models";
 import { GoalItem } from "@src/models/GoalItem";
 import { OutboxItem } from "@src/models/OutboxItem";
-import { changeNewUpdatesStatus } from "../GoalsAPI";
+import { changeNewUpdatesStatus, removeGoal } from "../GoalsAPI";
 
 export const getDump = async (relId = "", goalId = "") => {
   const dump: OutboxItem[] = await db.outboxCollection.where("goalId").equals(goalId).and((obj) => obj.relId === relId).toArray();
@@ -16,7 +16,7 @@ export const createDumpForContact = async (dump: OutboxItem) => {
       console.log(e.stack || e);
     });
 };
-export const addGoalChanges = async (type: string, event: OutboxItem) => {
+export const addGoalChanges = async (event: OutboxItem) => {
   const dump = await getDump(event.relId, event.goalId);
   const { goalId } = event;
   db.transaction("rw", db.goalsCollection, async () => {
@@ -48,4 +48,34 @@ export const cleanChangesOf = async (goalId: string, k: string) => {
   }).catch((e) => {
     console.log(e.stack || e);
   });
+};
+
+export const deleteChanges = async (root: boolean, goalId: string) => {
+  await removeGoal(goalId);
+  if (root) {
+    await db.outboxCollection.where("goalId").equals(goalId).delete();
+  }
+};
+
+export const addDeleteChanges = async (event: OutboxItem) => {
+  const dump = await getDump(event.relId, event.goalId);
+  const { goalId } = event;
+  db.transaction("rw", db.goalsCollection, async () => {
+    await db.goalsCollection.where("id").equals(goalId)
+      .modify((obj: GoalItem) => {
+        obj.collaboration = { ...obj.collaboration, newUpdates: true };
+      });
+  }).catch((e) => {
+    console.log(e.stack || e);
+  });
+  if (!dump) { await createDumpForContact(event); } else {
+    db.transaction("rw", db.outboxCollection, async () => {
+      await db.outboxCollection.where("goalId").equals(goalId)
+        .modify((obj: OutboxItem) => {
+          obj.deletedGoals = [...obj.deletedGoals, ...event.deletedGoals];
+        });
+    }).catch((e) => {
+      console.log(e.stack || e);
+    });
+  }
 };
