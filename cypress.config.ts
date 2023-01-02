a9fc3b97c0a4bc84efda9b923051d4e86ee0a477
Dmitry <fendermany@gmail.com>
2023-01-02 18:44:38 +0200

    o

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000", // "https://zinzen.vercel.app/",
+    baseUrl: "https://zinzen.vercel.app/",
     chromeWebSecurity: false,
     specPattern: "**/*.spec.ts",
   },
