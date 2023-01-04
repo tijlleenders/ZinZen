97df2a08358c1b657241b93931c77cf927e342e7
Tushar <tusharinani93@gmail.com>
2023-01-05 00:21:11 +0530

    updated: schema of shared goal

--- src/Interfaces/ISharedGoal.ts
+++ src/Interfaces/ISharedGoal.ts
@@ -1,14 +1,13 @@
 export interface ISharedGoal {
     id: string;
     title: string,
-    duration?: number | null,
-    repeat?: string | null;
+    duration: number | null,
+    repeat: string | null;
     start: Date | null;
     due: Date | null;
     afterTime: number | null;
     beforeTime: number | null;
-    createdAt?: string,
-    goalColor?: string,
+    goalColor: string,
     language: string,
     link: string | null,
     parentTitle: string
