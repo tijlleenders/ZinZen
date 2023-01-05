c4ed4db81b33d0316930be5a00fd1de72219c3a9
Eric Githinji <ericgithinji@gmail.com>
2023-01-05 14:41:13 +0300

    Add helper module that filters tasks according to start day.

---
+++ src/helpers/TaskFilter/TaskFilter.ts
@@ -0,0 +1,13 @@
+import { TaskItem } from "@src/models/TaskItem";
+
+// Returns tasks whose start day is a specified number of days after today
+// For tasks starting today, pass 'daysAfterToday' = 0
+export const TaskFilter = (tasks: TaskItem[], daysAfterToday: number): TaskItem[] => {
+  const today = new Date();
+  const targetDate = new Date();
+  targetDate.setDate(today.getDate() + daysAfterToday);
+  return tasks.filter((task) => {
+    const taskDate = new Date(`${task.start}Z`); // 'Z' needed to specify UTC
+    return taskDate.toDateString() === targetDate.toDateString();
+  });
+};
