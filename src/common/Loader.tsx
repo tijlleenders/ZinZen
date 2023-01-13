de6132eb8bdff01d4bea5671fe9c22df97521de6
Tushar <tusharinani93@gmail.com>
2023-01-13 15:49:20 +0530

    Teleported: inline CSS of loader in global scss

--- src/common/Loader.tsx
+++ src/common/Loader.tsx
@@ -1,33 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Spinner from "react-bootstrap/Spinner";
 
-const Loader = () => {
-  const [mobile, setMobile] = useState(false);
+const Loader = () => (
+  <Spinner
+    id="loader"
+    animation="border"
+    role="status"
+  >
+    <span className="visually-hidden">Loading...</span>
+  </Spinner>
 
-  // window.matchMedia("(max-width: 600px)").onchange = () => setMobile(window.matchMedia("(max-width: 600px)").matches);
-
-  // useEffect(() => {
-  //   setMobile(window.matchMedia("(max-width: 600px)").matches);
-  // }, []);
-
-  return (
-    <Spinner
-      style={{
-        position: "absolute",
-        top: "18px",
-        left: mobile ? "18px" : 0,
-        width: "50px",
-        height: "50px",
-        color: "#CD6E51"
-      }}
-      animation="border"
-      role="status"
-    >
-
-      <span className="visually-hidden">Loading...</span>
-    </Spinner>
-
-  );
-};
+);
 
 export default Loader;
