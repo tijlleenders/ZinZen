72bf659f7a5e8cecbb6a468553211a026c5ba71b
Elyes Arifa <earifa@resqsoft.com>
2023-01-19 14:16:30 +0100

    added other features and fixed requested changes

--- src/api/PublicGroupsAPI/index.ts
+++ src/api/PublicGroupsAPI/index.ts
@@ -1,48 +1,47 @@
-/* eslint-disable no-param-reassign */
-/* eslint-disable arrow-body-style */
-import { PublicGroupGoalItem } from "@src/models/PublicGroupGoalItem";
-
-const getInstallId = () => { return localStorage.getItem("installId"); };
-
-const createRequest = async (url: string, body : object | null = null, method = "POST") => {
-  try {
-    const res = await fetch(url, {
-      method,
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body || {})
-    });
-    return { success: res.ok, response: await res.json() };
-  } catch (err) {
-    return {
-      success: false,
-      message: "Aww... So sorry something went wrong. Try again later",
-    };
-  }
-};
+import { UpdatePublicGroupGoalItem } from "@src/models/UpdatePublicGroupGoalItem";
+import { RetrievePublicGroupGoalItem } from "@src/models/RetrievePublicGroupGoalItem";
+import { getInstallId, createContactRequest } from "@src/utils";
 
 export const getPublicGroupGoals = async (publicGroupParentGoalId: string) => {
   const url = "https://xcmyippimqii2x76diluvd4ywu0oojhu.lambda-url.eu-west-1.on.aws/";
-  const res = await createRequest(url, { method: "getPublicGroupGoals", installId: getInstallId(), publicGroupParentGoalId });
+  const res = await createContactRequest(url, { method: "getPublicGroupGoals", installId: getInstallId(), publicGroupParentGoalId });
   return res;
 };
 
 export const joinPublicGroup = async (title: string) => {
   const url = "https://xcmyippimqii2x76diluvd4ywu0oojhu.lambda-url.eu-west-1.on.aws/";
-  const res = await createRequest(url, { method: "joinPublicGroup", installId: getInstallId(), title });
+  const res = await createContactRequest(url, { method: "joinPublicGroup", installId: getInstallId(), title });
   return res;
 };
 
 export const quitPublicGroup = async (groupId: string) => {
   const url = "https://xcmyippimqii2x76diluvd4ywu0oojhu.lambda-url.eu-west-1.on.aws/";
-  const res = await createRequest(url, { method: "quitPublicGroup", installId: getInstallId(), groupId });
+  const res = await createContactRequest(url, { method: "quitPublicGroup", installId: getInstallId(), groupId });
   return res;
 };
 
-export const updatePublicGroupGoal = async (publicGroupGoal: PublicGroupGoalItem) => {
+export const updatePublicGroupGoal = async (publicGroupGoal: UpdatePublicGroupGoalItem) => {
   const url = "https://xcmyippimqii2x76diluvd4ywu0oojhu.lambda-url.eu-west-1.on.aws/";
-  const res = await createRequest(url, { method: "updatePublicGroupGoal", installId: getInstallId(), publicGroupGoal });
+  const res = await createContactRequest(url, { method: "updatePublicGroupGoal", installId: getInstallId(), publicGroupGoal });
   return res;
+};
+
+export const createRootGroup = async () => {
+  const res = await joinPublicGroup("Root Group"); // creates a new root group
+  return res.response.groups[res.response.groups.length - 1]; // returns the id of the created root group
+};
+
+export const addGroupToRootGroup = async (publicGroupGoal: UpdatePublicGroupGoalItem) => {
+  // eslint-disable-next-line no-param-reassign
+  publicGroupGoal.parentId = await createRootGroup(); // gets the id of the created root group (without recreating another group)
+  const res = await updatePublicGroupGoal(publicGroupGoal); // add a new group to the created root group (for explore groups screen)
+  const rootGroupAddedGroup : RetrievePublicGroupGoalItem = res.response.publicGroupGoal;
+  return rootGroupAddedGroup;
+};
+
+export const getRootGroupGroups = async () => {
+  const publicGroupParentGoalId = await createRootGroup();
+  const res = await getPublicGroupGoals(publicGroupParentGoalId); // gets the sub groups of the root group (for explore groups screen)
+  const rootGroupPublicGroups : RetrievePublicGroupGoalItem[] = res.response;
+  return rootGroupPublicGroups;
 };
