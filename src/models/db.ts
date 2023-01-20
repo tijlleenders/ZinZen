c674e73ed4e6f6284a34c05e1741520c6a600e62
Tushar <tusharinani93@gmail.com>
2023-01-20 15:43:06 +0530

    shared with me collection

--- src/models/db.ts
+++ src/models/db.ts
@@ -7,9 +7,11 @@ import { OutboxItem } from "./OutboxItem";
 export class FeelingsDB extends Dexie {
   feelingsCollection!: Table<IFeelingItem, number>;
 
-  goalsCollection!: Table<GoalItem, number>;
+  goalsCollection!: Table<GoalItem, string>;
 
   contactsCollection!: Table<ContactItem, number>;
+
+  sharedWMCollection!: Table<GoalItem, string>;
 
   outboxCollection!: Table<OutboxItem, number>;
 
@@ -17,8 +19,8 @@ export class FeelingsDB extends Dexie {
     super("ZinZenDB");
     this.version(1).stores({
       feelingsCollection: "++id, content, category, date, note",
-      goalsCollection:
-      "id, title, duration, sublist, repeat, start, due, afterTime, beforeTime, createdAt, parentGoalId, archived, goalColor, language, link, collaboration, shared",
+      goalsCollection: "id, title, duration, sublist, repeat, start, due, afterTime, beforeTime, createdAt, parentGoalId, archived, goalColor, language, link, collaboration, shared",
+      sharedWMCollection: "id, title, duration, sublist, repeat, start, due, afterTime, beforeTime, createdAt, parentGoalId, archived, goalColor, language, link, collaboration, shared",
       contactsCollection: "id, name, collaborativeGoals, sharedGoals, relId, accepted, createdAt",
       outboxCollection: "++id, relId, goalId, subgoals, updates, deleted, completed, anyUpdates"
     });
