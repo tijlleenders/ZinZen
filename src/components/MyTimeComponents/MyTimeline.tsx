2d3891de907dcd178e2a2f9ab4fb11926fb6c6dc
Tushar <tusharinani93@gmail.com>
2023-01-28 12:36:49 +0530

    created: skeleton & integration of showing the impossible tasks

--- src/components/MyTimeComponents/MyTimeline.tsx
+++ src/components/MyTimeComponents/MyTimeline.tsx
@@ -8,51 +8,61 @@ import { ITask } from "@src/Interfaces/Task";
 
 import "./MyTimeline.scss";
 
-export const MyTimeline = ({ myTasks }: {myTasks: ITask[]}) => {
+export const MyTimeline = ({ myTasks, impossible }: {myTasks: ITask[], impossible: ITask[]}) => {
   const navigate = useNavigate();
   const darkModeStatus = useRecoilValue(darkModeState);
   const [displayOptionsIndex, setDisplayOptionsIndex] = useState("root");
+  const [showScheduled, setShowScheduled] = useState(true);
 
+  const handleView = () => { setShowScheduled(!showScheduled); };
   return (
-    <div className={`MTL-display-${darkModeStatus ? "dark" : "light"}`}>
-      { myTasks.map((task) => (
-        <div>
-          <div style={{
-            display: "flex" }}
-          >
-            <button
-              type="button"
-              className="MTL-circle"
-              style={{ backgroundColor: `${task.goalColor}` }}
-            >.
-            </button>
-            <div style={{ marginLeft: "11px", color: `${task.goalColor}` }}>
+    <>
+      {impossible.length > 0 && (
+        <div className={`timeline-view${darkModeStatus ? "-dark" : ""}`}>
+          <button type="button" className={`${showScheduled && "activeView"}`} onClick={handleView}>Scheduled</button>
+          <button type="button" className={`${!showScheduled && "activeView"}`} onClick={handleView}>Impossible</button>
+        </div>
+      )}
+      <div className={`MTL-display-${darkModeStatus ? "dark" : "light"}`}>
+        { (showScheduled ? myTasks : impossible).map((task) => (
+          <div>
+            <div style={{
+              display: "flex" }}
+            >
               <button
                 type="button"
-                className="MTL-taskTitle"
-                onClick={() => {
-                  setDisplayOptionsIndex(task.goalid);
-                  if (displayOptionsIndex === task.goalid) {
-                    navigate("/MyGoals", { state: { isRootGoal: task.parentGoalId === "root", openGoalOfId: task.goalid } });
-                  }
-                }}
-              >
-                {task.title}
+                className="MTL-circle"
+                style={{ backgroundColor: `${task.goalColor}` }}
+              >.
               </button>
-              <p className="MTL-goalTiming">
-                { task.start ? `${task.start.split("T")[1].slice(0, 2)}:00` : "" } - { task.deadline ? `${task.deadline.split("T")[1].slice(0, 2)}:00` : "" }
-              </p>
+              <div style={{ marginLeft: "11px", color: `${task.goalColor}` }}>
+                <button
+                  type="button"
+                  className="MTL-taskTitle"
+                  onClick={() => {
+                    setDisplayOptionsIndex(task.goalid);
+                    if (displayOptionsIndex === task.goalid) {
+                      navigate("/MyGoals", { state: { isRootGoal: task.parentGoalId === "root", openGoalOfId: task.goalid } });
+                    }
+                  }}
+                >
+                  {task.title}
+                </button>
+                <p className="MTL-goalTiming">
+                  { task.start ? `${task.start.split("T")[1].slice(0, 2)}:00` : "" } - { task.deadline ? `${task.deadline.split("T")[1].slice(0, 2)}:00` : "" }
+                </p>
+              </div>
             </div>
+            { displayOptionsIndex === task.goalid ? (
+              <div className="MTL-options">
+                <button type="button"> Forget</button><div />
+                <button type="button"> Reschedule</button><div />
+                <button type="button"> Done</button><div />
+              </div>
+            ) : null}
           </div>
-          { displayOptionsIndex === task.goalid ? (
-            <div className="MTL-options">
-              <button type="button"> Forget</button><div />
-              <button type="button"> Reschedule</button><div />
-              <button type="button"> Done</button><div />
-            </div>
-          ) : null}
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 };
