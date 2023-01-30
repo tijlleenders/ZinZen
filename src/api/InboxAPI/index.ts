1f2197c446c7a6f18b11e4d21db1930736a92a88
Tushar <tusharinani93@gmail.com>
2023-01-30 22:58:56 +0530

    created: api(s) of inbox collection

---
+++ src/api/InboxAPI/index.ts
@@ -0,0 +1,42 @@
+import { db } from "@models";
+import { IChangesInGoal, InboxItem, typeOfChange } from "@src/models/InboxItem";
+import { getDefaultValueOfGoalChanges } from "@src/utils";
+
+export const createEmptyInboxItem = async (id: string) => {
+  db.transaction("rw", db.inboxCollection, async () => {
+    await db.inboxCollection.add({ id, goalChanges: getDefaultValueOfGoalChanges() });
+  }).catch((e) => {
+    console.log(e);
+  });
+};
+
+export const addGoalChangesInID = async (id: string, newChanges: IChangesInGoal) => {
+  db.transaction("rw", db.inboxCollection, async () => {
+    await db.inboxCollection.where("id").equals(id)
+      .modify((obj: InboxItem) => {
+        Object.keys(newChanges).forEach((changeType: typeOfChange) => {
+          obj.goalChanges[changeType] = [...obj.goalChanges[changeType], ...newChanges[changeType]];
+        });
+      });
+  }).catch((e) => {
+    console.log(e.stack || e);
+  });
+};
+
+export const deleteGoalChangesInID = async (id:string, changes: IChangesInGoal) => {
+  db.transaction("rw", db.inboxCollection, async () => {
+    await db.inboxCollection.where("id").equals(id)
+      .modify((obj: InboxItem) => {
+        Object.keys(changes).forEach((changeType: typeOfChange) => {
+          const arr = [...obj.goalChanges[changeType]];
+          obj.goalChanges[changeType] = arr.filter((ele) =>
+            (["archived", "deleted"].includes(changeType)
+              ? !changes[changeType].includes(ele.id)
+              : !changes[changeType].includes(ele.goal.id))
+          );
+        });
+      });
+  }).catch((e) => {
+    console.log(e.stack || e);
+  });
+};
