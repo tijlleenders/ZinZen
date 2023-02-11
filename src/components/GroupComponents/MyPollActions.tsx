b9f13ab59fe19be7b02a8d237423b16c1ad72ead
Tushar <tusharinani93@gmail.com>
2023-02-11 16:52:31 +0530

    developed: Poll actions skeleton

---
+++ src/components/GroupComponents/MyPollActions.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+
+import peopleIcon from "@assets/images/peopleIcon.svg";
+import correct from "@assets/images/correct.svg";
+import vote from "@assets/images/vote.svg";
+
+import { darkModeState, lastAction } from "@src/store";
+import { IPoll } from "@src/models/PublicGroupItem";
+import { displayGroup } from "@src/store/GroupsState";
+import { sendReactionOnPoll } from "@src/services/group.service";
+import { updateMyMetric } from "@src/api/PublicGroupsAPI";
+
+interface MyGoalActionsProps {
+  poll: IPoll,
+}
+type actionType = "upvotes" | "downvotes" | "inMygoals" | "completed"
+
+const MyPollActions: React.FC<MyGoalActionsProps> = ({ poll }) => {
+  const darkModeStatus = useRecoilValue(darkModeState);
+  const selectedGroup = useRecoilValue(displayGroup);
+  const setLastAction = useSetRecoilState(lastAction);
+  const handleClick = async (typeOfAction: string) => {
+    if (selectedGroup) {
+      const newMetricsState = poll.myMetrics;
+      if (typeOfAction === "inMygoals") {
+        newMetricsState.inMyGoals = true;
+      } else if (typeOfAction === "completed") {
+        newMetricsState.completed = true;
+      } else {
+        newMetricsState.voteScore = typeOfAction === "upvotes" ? 1 : -1;
+      }
+      await updateMyMetric(selectedGroup.id, poll.id, newMetricsState);
+      await sendReactionOnPoll(selectedGroup.id, poll.id, newMetricsState);
+      setLastAction("groupAction");
+    }
+  };
+  const getActionImg = (typeOfAction: actionType, imgSrc: string, definition: string, exceptional = false, customStyle = {}) => (
+    <button type="button" onClick={async () => { await handleClick(typeOfAction); }} style={{ background: "transparent", border: "none" }}>
+      <img
+        alt={definition}
+        src={imgSrc}
+        className={exceptional ? `exceptional${darkModeStatus ? "-dark" : ""}-img` : ""}
+        style={{ cursor: "pointer", ...customStyle }}
+      />&nbsp;&nbsp;&nbsp;{poll.metrics[typeOfAction]}
+    </button>
+  );
+  return (
+    <div className={`interactables${darkModeStatus ? "-dark" : ""}`}>
+      {getActionImg("upvotes", vote, "upvote", darkModeStatus, { rotate: "180deg" })}
+      {getActionImg("downvotes", vote, "downvote", darkModeStatus)}
+      {getActionImg("inMygoals", peopleIcon, "People added to my goals", darkModeStatus)}
+      {getActionImg("completed", correct, "People Completed Goal", true)}
+    </div>
+  );
+};
+
+export default MyPollActions;
