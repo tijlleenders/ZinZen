82d551f6e318625ab9823e19d8c643bc61f80072
Tushar <tusharinani93@gmail.com>
2023-02-16 20:32:22 +0530

    Re-dev: Header contains a default plus button

--- src/components/HeaderDashboard/MainHeaderDashboard.tsx
+++ src/components/HeaderDashboard/MainHeaderDashboard.tsx
@@ -1,25 +1,24 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { useEffect } from "react";
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { useNavigate } from "react-router-dom";
 
-import { darkModeState, displayLoader, displayToast, lastAction } from "@store";
+import { darkModeState, displayToast, lastAction } from "@store";
 
 import mainAvatarLight from "@assets/images/mainAvatarLight.svg";
 import mainAvatarDark from "@assets/images/mainAvatarDark.svg";
 // import ZinZenTextLight from "@assets/images/LogoTextLight.svg";
 // import ZinZenTextDark from "@assets/images/LogoTextDark.svg";
 
-import myGroupsIcon from "@assets/images/myGroupsIconLight.svg";
 import myTimeIcon from "@assets/images/myTimeIconLight.svg";
 import myGoalsIcon from "@assets/images/myGoalsIconLight.svg";
 import myFeelingsIcon from "@assets/images/myFeelingsIconLight.svg";
 
-import myGroupsIconFilledLight from "@assets/images/myGroupsIconFilledLight.svg";
 import myTimeIconFilledLight from "@assets/images/myTimeIconFilledLight.svg";
 import myGoalsIconFilledLight from "@assets/images/myGoalsIconFilledLight.svg";
 import myFeelingsIconFilledLight from "@assets/images/myFeelingsIconFilledLight.svg";
 
-import myGroupsIconFilledDark from "@assets/images/myGroupsIconFilledDark.svg";
 import myTimeIconFilledDark from "@assets/images/myTimeIconFilledDark.svg";
 import myGoalsIconFilledDark from "@assets/images/myGoalsIconFilledDark.svg";
 import myFeelingsIconFilledDark from "@assets/images/myFeelingsIconFilledDark.svg";
@@ -29,7 +28,6 @@ import addDark from "@assets/images/addDark.svg";
 import correctLight from "@assets/images/correctLight.svg";
 import correctDark from "@assets/images/correctDark.svg";
 
-import Loader from "@src/common/Loader";
 import Sidebar from "@components/Sidebar";
 import { colorPalleteList } from "@src/utils";
 import { displaySidebar } from "@src/store/SidebarState";
@@ -37,16 +35,16 @@ import { createUserGroup } from "@src/helpers/GroupsProcessor";
 import { createGoal, modifyGoal } from "@src/helpers/GoalController";
 import SuggestionModal from "@components/GoalsComponents/SuggestionModal/SuggestionModal";
 import { displayAddPublicGroup, newGroupTitle } from "@src/store/GroupsState";
-import { inputGoalTags, extractedTitle, displayAddGoal, displayGoalId, displayUpdateGoal, selectedColorIndex, goalsHistory, displayAddGoalOptions } from "@src/store/GoalsState";
+import { inputGoalTags, extractedTitle, displayAddGoal, displayGoalId, displayUpdateGoal, selectedColorIndex, goalsHistory } from "@src/store/GoalsState";
 
 import "@translations/i18n";
 import "./HeaderDashboard.scss";
 
 export const MainHeaderDashboard = () => {
   const navigate = useNavigate();
+
   const currentPage = window.location.pathname.split("/")[1];
   const goalID = useRecoilValue(displayGoalId);
-  const showLoader = useRecoilValue(displayLoader);
   const colorIndex = useRecoilValue(selectedColorIndex);
   const selectedGoalId = useRecoilValue(displayGoalId);
   const subGoalsHistory = useRecoilValue(goalsHistory);
@@ -61,14 +59,12 @@ export const MainHeaderDashboard = () => {
   const [darkModeStatus, setDarkModeStatus] = useRecoilState(darkModeState);
   const [showUpdateGoal, setShowUpdateGoal] = useRecoilState(displayUpdateGoal);
 
-  const setShowAddGoalOptions = useSetRecoilState(displayAddGoalOptions);
   const setShowToast = useSetRecoilState(displayToast);
 
   const isTitleEmpty = () => {
     if (goalTitle.length === 0) { setShowToast({ open: true, message: `Goal cannot be ${showAddGoal ? "added" : "updated"} without title`, extra: "" }); }
     return goalTitle.length === 0;
   };
-
   const resetCurrentStates = () => {
     if (showAddGoal) { setShowAddGoal(null); } else if (showUpdateGoal) { setShowUpdateGoal(null); }
     // @ts-ignore
@@ -107,7 +103,7 @@ export const MainHeaderDashboard = () => {
         await createUserGroup(newGroupName);
         setNewGroupName("");
       } else if (!showAddGoal && !showUpdateGoal) {
-        setShowAddGoalOptions(true);
+        setShowAddGoal({ open: true, goalId: selectedGoalId });
       } else if (showAddGoal) {
         setLastAction("addGoal");
       } else if (showUpdateGoal) {
@@ -119,7 +115,6 @@ export const MainHeaderDashboard = () => {
   const getNavIcon = (imageVariable: string, to = "", customStyle = {}) => {
     const pageName = to === "My Time" && currentPage === "" ? "" : to.split(" ").join("");
     const imgCustomClass = `nav-icon${darkModeStatus ? "-dark" : ""}${pageName === currentPage ? "-filled" : ""}`;
-    console.log(to === "Sidebar", imgCustomClass);
     return (
       <button
         type="button"
@@ -130,6 +125,7 @@ export const MainHeaderDashboard = () => {
       </button>
     );
   };
+
   useEffect(() => {
     if (action === "addGoal") {
       addThisGoal();
@@ -140,30 +136,29 @@ export const MainHeaderDashboard = () => {
     }
   }, [action]);
   return (
-    <div className={`positioning${!darkModeStatus ? "-light" : "-dark"}`}>
-      { showLoader && <Loader /> }
-      {getNavIcon(
-        (currentPage !== "" ? myTimeIcon :
-          darkModeStatus ? myTimeIconFilledDark : myTimeIconFilledLight),
-        "My Time",
-        { paddingTop: "4px" })}
-      {getNavIcon(
-        (currentPage !== "MyGroups" ? myGroupsIcon :
-          darkModeStatus ? myGroupsIconFilledDark : myGroupsIconFilledLight),
-        "My Groups",
-        { paddingTop: "4px" })}
-      { getNavIcon(darkModeStatus ? mainAvatarDark : mainAvatarLight, "Sidebar") }
-      {getNavIcon(
-        (currentPage !== "MyFeelings" ? myFeelingsIcon :
-          darkModeStatus ? myFeelingsIconFilledDark : myFeelingsIconFilledLight),
-        "My Feelings")}
-      {currentPage !== "/MyGoals" ?
-        getNavIcon((currentPage !== "MyGoals" ? myGoalsIcon :
-          darkModeStatus ? myGoalsIconFilledDark : myGoalsIconFilledLight), "My Goals", { paddingTop: "4px" }) :
-        getNavIcon(!showAddGoal && !showUpdateGoal ? (darkModeStatus ? addDark : addLight) : (darkModeStatus ? correctDark : correctLight), "save action", { width: "30px" })}
-      <Sidebar />
-      <SuggestionModal goalID={goalID} />
-    </div>
+    <>
+      <div className={showSidebar ? "overlay" : ""} onClick={() => setShowSidebar(false)}>
+        {showSidebar && (<Sidebar />)}
+      </div>
+      <div className={`positioning${!darkModeStatus ? "-light" : "-dark"}`} style={{ height: `${showSidebar ? 6 : 5}em` }}>
+        <div className="nav-layer">
+          {getNavIcon(
+            (currentPage !== "" ? myTimeIcon :
+              darkModeStatus ? myTimeIconFilledDark : myTimeIconFilledLight),
+            "My Time",
+            { paddingTop: "4px", width: "32px" })}
+          {getNavIcon((currentPage !== "MyGoals" ? myGoalsIcon :
+            darkModeStatus ? myGoalsIconFilledDark : myGoalsIconFilledLight), "My Goals", { paddingTop: "4px" })}
+          {getNavIcon(darkModeStatus ? mainAvatarDark : mainAvatarLight, "Sidebar")}
+          {getNavIcon(
+            (currentPage !== "MyFeelings" ? myFeelingsIcon :
+              darkModeStatus ? myFeelingsIconFilledDark : myFeelingsIconFilledLight),
+            "My Feelings")}
+          {getNavIcon(!showAddGoal && !showUpdateGoal ? (darkModeStatus ? addDark : addLight) : (darkModeStatus ? correctDark : correctLight), "save action", { width: "30px" })}
+        </div>
+        <SuggestionModal goalID={goalID} />
+      </div>
+    </>
 
   );
 };
