fb049e4ef964c634a122f8794665a749b18accea
Tushar <tusharinani93@gmail.com>
2023-02-16 21:54:27 +0530

    update: new assets integrated in my groups

--- src/components/GroupComponents/MyGroup.tsx
+++ src/components/GroupComponents/MyGroup.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 
-import { darkModeState, lastAction } from "@src/store";
+import joinGroup from "@assets/images/joinGroup.svg";
+import leaveGroup from "@assets/images/leaveGroup.svg";
+
+import { darkModeState, displayToast, lastAction } from "@src/store";
 import { PublicGroupItem } from "@src/models/PublicGroupItem";
 import { displayExploreGroups, displayGroup } from "@src/store/GroupsState";
 import { addPublicGroup, deleteGroup, getPublicGroup } from "@src/api/PublicGroupsAPI";
@@ -14,8 +17,8 @@ const MyGroup = ({ group }: MyGroupProps) => {
   const openExploreGroups = useRecoilValue(displayExploreGroups);
 
   const setLastAction = useSetRecoilState(lastAction);
+  const setShowToast = useSetRecoilState(displayToast);
   const setSelectedGroup = useSetRecoilState(displayGroup);
-
   return (
     <div
       key={String(`goal-${group.id}`)}
@@ -40,7 +43,9 @@ const MyGroup = ({ group }: MyGroupProps) => {
         type="button"
         className="user-goal-main"
         onClick={async () => {
-          setSelectedGroup({ ...(openExploreGroups ? group : await getPublicGroup(group.id)) });
+          if (group.polls.length === 0) { setShowToast({ open: true, extra: "Tip: Share a goal in this group", message: "Group has no activities to display" }); } else {
+            setSelectedGroup({ ...(openExploreGroups ? group : await getPublicGroup(group.id)) });
+          }
         }}
       >
         <div
@@ -56,8 +61,7 @@ const MyGroup = ({ group }: MyGroupProps) => {
           style={{
             background: "transparent",
             border: "none",
-            top: "15px",
-            ...(darkModeStatus ? { filter: "invert(1)" } : { opacity: "0.6" })
+            top: "18px"
           }}
           onClickCapture={async (e) => {
             e.stopPropagation();
@@ -66,8 +70,8 @@ const MyGroup = ({ group }: MyGroupProps) => {
           }}
         >
           { openExploreGroups ?
-            <img alt="join public group" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACMAAAAjCAYAAAAe2bNZAAAACXBIWXMAAAsTAAALEwEAmpwYAAACj0lEQVR4nO2X32tOcRzHX/Zg1ma2FPlRFLsQy8WUFhd+E+UPIK5kK2TRkiuNuaBdLS6GUty4Ejd6Shq1tjElPaLkwiL2SCaK8tgefet96uz0Pb/PYyWf+lw83+/7nM/r+Z7P53M+B/5bfFsLXAOGgDxwBJht0S0GLgIDwCOgC5hPhrYP+AWUPT4I1Ll0LcBni+4d0JQFyDLgpyWA45ekmwW8CdANZwFzOiCA8W/ATGBbiM74mrQwfRGCLAIORdDtTQtzJiSAeYTVwJ4IMCanUtkq4HdAgJvS1QAfAnQvgSoysJPApCXAa2CBS7fdJ9m/AuvI0HYB94GPwCvgAtBg0TUDt1TOb4HrwAr+NdsA9KiibN4u3UqgO0DnuCmGJUlATBlOhFTHZoF8j1BJjheBhXFh8iE3LQFzgHMxQBw/FhfmWcgNx6W7kgCmezphikAvsEMvzJrpgClphKhTw1sPHAZOAW1Aa9RGmBZmHNiqYOa9Neqje6+qzFUKpqS3eC1w17L/2FKp94D6SsB06UTuWPYKum7Yspf3O6GkMEXliN9I8UTX9fvsH7XBPAyBKenfn/Ws92p9NCHMmIa1KXY5QskuB/Z71naqasoJYYxv8sJsjADTBswFvrjWmlS+zu8XwFOX39D9+zzrBdeYctz2qG6HwBT0SDpdaw2uudlUTRzrD+rQ84CREKATwAzgqn4v1YmVVb6DmoMcd74mejzrA65y7/CjNa3bDFI/LCAjepwI6IDm3NaUObMl7Ahrgd3Kh4PA6gBtlTprEphP+gbL1NoTwliTN63l1OK9wZ5rf8iy98DWY7KyesuQNqlTmbCANFJhy6nFjwXkSEclTwSLmWBmZjY5cV4AZsT4qxCJ7Q8siwq3/II50gAAAABJRU5ErkJggg==" />
-            : <img alt="leave public group" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACMAAAAjCAYAAAAe2bNZAAAACXBIWXMAAAsTAAALEwEAmpwYAAACK0lEQVR4nO2Yz0tVQRTHP1QapaBStMpy0SLBKAldu3DhrgQX/Q2iixYvNw/ShZG7IkSklZuSFCHIXJSCIEQQFKKLlAeS/RRX5cIsKwbOg+Ew45v37twuRAe+m3nzZj5v7pzvOffB/4gfLcA6UEioyRgwdcBN4HYCLQEfiBzHgMMVfG8wJkwTMA/8BHaAO8DRLGCOAG+A30q3soBpdYAYvfPMvyw/IBWYNg/MJ8fc48AuMAVUpQFTLamtYe555l8BviugqBf4ArBigRjPqLUe44DSgjWvbJh2YA54pfQCmAH6xG9MVjWo7/Y5DO6zwHyUkw2GaQC+eO6FrW2gJ2C9E5J9W+LeZZ3MaABIUb+A/gPWMqfwWoEEw7SKkdkZ8sjjKyFABua+AgmGWVQbXZXxxwEnZO5KaJSEMca0b20wK+OngD0HwJ4DqHhh3wK5JDAmJmThTeCsjF33nMaw6KATa0wCY+KcPOtiuO7LD2sjH9BXoB53VGR6lzwbTat514AxYFx0V7yKmDB5D0xHuQvFgMk5QJYTglQEcwY46XDjglTjvwbTKP2JSetOZYRGI2nBmGrbq6rsnGz6XHrcvCObLqYBM13CK4aAQ8BTNf5SxqPCrJaAuWFV3w31WX9smG6pqi67f6AMsE3aSNvYTseEKTd6FfSTLGHsGqareyYwNaoHfp/VS1wxzgPfLKBm6YtzjoZc61ka79pdUh5GJc1NR7cW+C/Ew9gw/HPxByp4QRNrIFTOAAAAAElFTkSuQmCC" /> }
+            <img alt="join public group" src={joinGroup} className={darkModeStatus ? "dark-svg" : ""} />
+            : <img alt="leave public group" src={leaveGroup} className={darkModeStatus ? "dark-svg" : ""} /> }
         </button>
 
       </button>
