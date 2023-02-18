b476d775e5e69251735b20b7fa46c9b76f5d1ed6
Tijl Leenders <tijl.leenders@gmail.com>
2023-02-18 15:50:20 +0100

    remove unused model UpdatePublicGroupGoalItem

--- src/models/UpdatePublicGroupGoalItem.ts
+++
@@ -1,9 +0,0 @@
-export interface UpdatePublicGroupGoalItem {
-    id : string,
-    parentId: string,
-    title: string,
-    likeScore: -1 | 0 | 1,
-    feelingScore: -1 | 0 | 1,
-    inMyGoals: boolean,
-    completed: boolean
-  }
