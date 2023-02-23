dd7b85ae71fe370643081f89b67a6bd4859ce7b9
Tushar <tusharinani93@gmail.com>
2023-02-23 15:11:37 +0530

    written: confirmation modal content

---
+++ src/constants/confirmationHeaders.ts
@@ -0,0 +1,35 @@
+export const confirmationHeaders = {
+  goal: {
+    archive: {
+      header: "Do you want to mark this goal completed?",
+      note: "This goal will be moved to archive along with the subgoals."
+    },
+    delete: {
+      header: "Do you want to delete this goal?",
+      note: "You won't be able to recover it again."
+    },
+    shareAnonymously: {
+      header: "Do you want to share this goal anonymously?",
+      note: "Only this goal and its time related properties will be shared publicly :)"
+    },
+    shareWithOne: {
+      header: "Do you want to share this goal with one of your contacts?",
+      note: "Every changes you make on/under this goal will be shared with that person."
+    }
+  },
+  collaboration: {
+    colabRequest: {
+      header: "Do you wish to collaborate?",
+      note: "During collaboration, this goal will be added to your MyGoals, and any changes you make will be communicated to the other person."
+    },
+    delete: {
+      header: "Do you want to delete this goal?",
+      note: "This will end the collaboration and you will not recieve any updates."
+    },
+    archive: {
+      header: "Do you want to archive this goal?",
+      note: "This will end the collaboration and you will not recieve any updates."
+    },
+  },
+
+};
