123629f1116ab988c09f334ac2a00ff69b6a4f62
Tushar <tusharinani93@gmail.com>
2023-02-23 15:10:13 +0530

    update: created and clubbed  interfaces of all pages

---
+++ src/Interfaces/IPages.ts
@@ -0,0 +1,37 @@
+import { feelingListType } from "@src/global";
+import { IFeelingItem } from "@src/models";
+
+/* Add Feelings Page */
+export interface AddFeelingsPageProps {
+  feelingDate: Date | null;
+  setShowAddFeelingsModal: React.Dispatch<React.SetStateAction<Date | null>>;
+}
+
+export interface AddFeelingTemplate {
+    feelingCategory: string;
+    feelingsList: string[];
+    feelingDate: Date;
+  }
+
+/* Show Feelings Page */
+
+export interface ISetFeelingsListObject {
+    feelingsList: feelingListType[];
+    setFeelingsList: React.Dispatch<React.SetStateAction<feelingListType[]>>;
+  }
+export interface ISetSelectedFeeling {
+    selectedFeeling: number;
+    setSelectedFeeling: React.Dispatch<React.SetStateAction<number>>;
+  }
+export interface ShowFeelingTemplateProps {
+    feelingsListObject: IFeelingItem[];
+    setFeelingsListObject: ISetFeelingsListObject;
+    currentFeelingsList: feelingListType[];
+    handleFocus: ISetSelectedFeeling;
+  }
+
+/* MyGoals Page */
+export interface ILocationProps {
+    openGoalOfId: string,
+    isRootGoal: boolean
+}
