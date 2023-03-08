faaa8c241825aaf0f7bc0e7eb91d2440e8605769
Tushar <tusharinani93@gmail.com>
2023-03-08 23:42:18 +0530

    update: n-level scheduling

--- src/helpers/MiniScheduler.ts
+++ src/helpers/MiniScheduler.ts
@@ -1,5 +1,4 @@
 /* eslint-disable no-continue */
-// @ts-nocheck
 import { IMSInputGoal } from "@src/Interfaces/ISchedulerInputGoal";
 import { addFreeSlots, BookSlots, convertDateToDay, get24Slots, getDurations, getHrInDate, getNecessaryParams, sortTheGoals } from "@src/utils/SchedulerUtils";
 
@@ -68,10 +67,82 @@ const tryThisWeeklyGoal = (goal: IMSInputGoal, StartDate: Date, endDate: Date, o
     totalDurationLeft = remainingHr;
     if (totalDurationLeft === 0) {
       breakout = true;
+      break;
     }
     thisDate.setDate(thisDate.getDate() + 1);
   }
+  console.log(goal.title, totalDurationLeft);
   return { status: breakout, slotsOnDays, outputSlotsTemp };
+};
+const alreadyDone: string[] = [];
+export const handleNLevel = (goals, startDate, endDate, OP, level = "root") => {
+  const hierarchicalGoals = goals.filter((goal) => (goal.children && goal.children.length > 0 && level === goal.parentGoalId));
+  const inputGoalsObj = goals.reduce((acc, curr) => ({ ...acc, [curr.id]: curr }), {});
+  for (let p = 0; p < hierarchicalGoals.length; p += 1) {
+    const goal = hierarchicalGoals[p];
+    alreadyDone.push(goal.id);
+    const { goalDuration, diff } = getDurations(goal);
+    let duration = goalDuration;
+    const hierGoals = [];
+    goal.children.forEach((id) => {
+      const child = inputGoalsObj[id];
+      duration -= Number(child.duration);
+      hierGoals.push(child);
+      alreadyDone.push(child.id);
+    });
+    const fillerDuration = duration > diff ? duration - diff : 0;
+    if (duration >= 0) {
+      let nextLevelDive = false;
+      if (duration > 0) { hierGoals.push({ ...goal, title: `${goal.title} filler`, duration: fillerDuration }); }
+      let success = true;
+      const assignedSlotsOfGoal = {};
+      let outputCopy = { ...OP };
+      for (let i = 0; i < hierGoals.length; i += 1) {
+        const hGoal = hierGoals[i];
+        // console.log(goal.title, hGoal.id, hGoal.title);
+        if (hGoal.children && hGoal.children.length > 0 && hGoal.id !== goal.id) {
+          nextLevelDive = true;
+          // console.log(outputCopy);
+          const res = handleNLevel(goals, new Date(startDate), new Date(endDate), outputCopy, hGoal.parentGoalId);
+          const { levelSuccess, levelOutput } = res;
+          outputCopy = { ...levelOutput };
+          success = levelSuccess;
+          // console.log("vie", hGoal.title, success, outputCopy);
+        } else {
+          // console.log("no dive", hGoal.title, hGoal.duration);
+          const hStartDate = goal.start ? new Date(goal.start) : new Date(startDate);
+          const hEndDate = goal.deadline ? new Date(goal.deadline) : new Date(endDate);
+          const { status, outputSlotsTemp, slotsOnDays } = tryThisWeeklyGoal({ ...hGoal, repeat: "weekly" }, hStartDate, hEndDate, outputCopy);
+          // console.log(outputSlotsTemp);
+          outputCopy = { ...outputSlotsTemp };
+          assignedSlotsOfGoal[hGoal.id] = { ...slotsOnDays };
+          nextLevelDive = false;
+          // console.log(hGoal.title, status);
+          if (success) { success = status; } else break;
+        }
+      }
+      // console.log("outofloop", success, nextLevelDive);
+      if (!nextLevelDive) {
+        Object.keys(assignedSlotsOfGoal).forEach((goalid) => {
+          Object.keys(assignedSlotsOfGoal[goalid]).forEach((slotsOfDay) => {
+            if (success) {
+              schedulerSlots[slotsOfDay] = [...(schedulerSlots[slotsOfDay] || []), ...assignedSlotsOfGoal[goalid][slotsOfDay]];
+            } else {
+              impossibleTasks[slotsOfDay] = [...(impossibleTasks[slotsOfDay] || []), ...assignedSlotsOfGoal[goalid][slotsOfDay]];
+            }
+          });
+        });
+      }
+      // console.log(level, success);
+      if (level === "root" && success) {
+        // console.log(outputCopy);
+        output = { ...outputCopy };
+      }
+      // console.log(goal.title, success);
+      return { levelSuccess: success, levelOutput: success ? outputCopy : output };
+    }
+  }
+  // console.log("out", level, hierarchicalGoals);
 };
 
 export const callMiniScheduler = (inputObj: {
@@ -85,51 +156,7 @@ export const callMiniScheduler = (inputObj: {
   });
 
   debug = true;
-  const hierarchicalGoals = goals.filter((goal) => goal.children && goal.children.length > 0);
-  const inputGoalsObj = goals.reduce((acc, curr) => ({ ...acc, [curr.id]: curr }), {});
-  const alreadyDone: string[] = [];
-  hierarchicalGoals.forEach((goal) => {
-    alreadyDone.push(goal.id);
-    const { goalDuration, diff } = getDurations(goal);
-    let duration = goalDuration;
-    const hierGoals = [];
-    // if (goal.repeat.toLowerCase().includes("week")) {
-    goal.children.forEach((id) => {
-      const child = inputGoalsObj[id];
-      duration -= Number(child.duration);
-      hierGoals.push(child);
-      alreadyDone.push(child.id);
-    });
-    const fillerDuration = duration > diff ? duration - diff : 0;
-    if (duration >= 0) {
-      if (duration > 0) { hierGoals.push({ ...goal, title: `${goal.title} filler`, duration: fillerDuration }); }
-      let success = true;
-      const assignedSlotsOfGoal = {};
-      let outputCopy = { ...output };
-      for (let i = 0; i < hierGoals.length; i += 1) {
-        const hGoal = hierGoals[i];
-        const hStartDate = goal.start ? new Date(goal.start) : new Date(startDate);
-        const hEndDate = goal.deadline ? new Date(goal.deadline) : new Date(endDate);
-        const { status, outputSlotsTemp, slotsOnDays } = tryThisWeeklyGoal({ ...hGoal, repeat: "weekly" }, hStartDate, hEndDate, outputCopy);
-        outputCopy = { ...outputSlotsTemp };
-        assignedSlotsOfGoal[hGoal.id] = { ...slotsOnDays };
-        if (success) { success = status; } else break;
-      }
-      Object.keys(assignedSlotsOfGoal).forEach((goalid) => {
-        Object.keys(assignedSlotsOfGoal[goalid]).forEach((slotsOfDay) => {
-          if (success) {
-            schedulerSlots[slotsOfDay] = [...(schedulerSlots[slotsOfDay] || []), ...assignedSlotsOfGoal[goalid][slotsOfDay]];
-          } else {
-            impossibleTasks[slotsOfDay] = [...(impossibleTasks[slotsOfDay] || []), ...assignedSlotsOfGoal[goalid][slotsOfDay]];
-          }
-        });
-      });
-      if (success) {
-        output = { ...outputCopy };
-      }
-    }
-    // }
-  });
+  handleNLevel(goals, new Date(startDate), new Date(endDate), output);
   goals.forEach((goal: IMSInputGoal) => {
     const wStartDate = goal.start ? new Date(goal.start) : new Date(startDate);
     const wEndDate = goal.deadline ? new Date(goal.deadline) : new Date(endDate);
