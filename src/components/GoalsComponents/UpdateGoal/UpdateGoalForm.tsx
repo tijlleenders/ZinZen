5026e70e19742c6ff9b338b0717bbe99be574235
Tushar <tusharinani93@gmail.com>
2023-03-17 22:05:31 +0530

    added: settings icon to open config goal tags modal

--- src/components/GoalsComponents/UpdateGoal/UpdateGoalForm.tsx
+++ src/components/GoalsComponents/UpdateGoal/UpdateGoalForm.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 
+import settings from "@assets/images/settings.svg";
+
 import { getGoal } from "@src/api/GoalsAPI";
 import { colorPalleteList } from "@src/utils";
 import { displayInbox, lastAction } from "@store";
@@ -12,12 +14,16 @@ import InputGoal from "../InputGoal";
 
 import "@translations/i18n";
 import "./UpdateGoalForm.scss";
+import GoalConfigModal from "../GoalConfigModal/GoalConfigModal";
+import { GoalItem } from "@src/models/GoalItem";
 
 export const UpdateGoalForm = () => {
   const showUpdateGoal = useRecoilValue(displayUpdateGoal);
   const openInbox = useRecoilValue(displayInbox);
+  const [thisGoal, setThisGoal] = useState<GoalItem | null>(null);
   const [goalInput, setGoalInput] = useState("");
   const [goalLang, setGoalLang] = useState("english");
+  const [openGoalConfig, setOpenGoalConfig] = useState(false);
   const [colorIndex, setColorIndex] = useRecoilState(selectedColorIndex);
   const setLastAction = useSetRecoilState(lastAction);
 
@@ -29,6 +35,7 @@ export const UpdateGoalForm = () => {
   useEffect(() => {
     if (showUpdateGoal) {
       (openInbox ? getSharedWMGoal(showUpdateGoal.goalId) : getGoal(showUpdateGoal.goalId)).then((goal) => {
+        setThisGoal(goal);
         const res = formatTagsToText(goal);
         if (goal.language) setGoalLang(goal.language);
         setColorIndex(colorPalleteList.indexOf(goal.goalColor));
@@ -48,7 +55,11 @@ export const UpdateGoalForm = () => {
           />
         )
       }
-      <ColorPalette colorIndex={colorIndex} setColorIndex={setColorIndex} />
+      <div>
+        <ColorPalette colorIndex={colorIndex} setColorIndex={setColorIndex} />
+        <button type="button" onClick={() => { setOpenGoalConfig(!openGoalConfig); }} className="ordinary-btn" style={{ marginLeft: "20px" }}> <img src={settings} alt="update goal" /></button>
+      </div>
+      { openGoalConfig && thisGoal && <GoalConfigModal goal={thisGoal} />}
     </form>
   );
 };
