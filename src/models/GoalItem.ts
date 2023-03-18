7d76be81c80a70b8e98a9d7796ba49a14a67b8a3
Tushar <tusharinani93@gmail.com>
2023-03-19 01:06:21 +0530

    update: Goal Item schema updated

--- src/models/GoalItem.ts
+++ src/models/GoalItem.ts
@@ -2,23 +2,25 @@ import { ICollaboration } from "@src/Interfaces/ICollaboration";
 import { IShared } from "@src/Interfaces/IShared";
 
 export interface GoalItem {
-  id: string;
-  title: string;
-  duration?: string | null;
-  sublist: string[];
-  repeat?: string | null;
-  due: Date | null;
-  start: Date | null;
-  beforeTime: number | null;
-  afterTime: number | null;
-  createdAt?: Date;
-  archived: "false" | "true";
-  parentGoalId: string;
-  goalColor: string;
-  language: string;
-  link: string | null;
-  collaboration: ICollaboration;
+  id: string,
+  title: string,
+  duration: string | null,
+  sublist: string[],
+  habit: string | null,
+  on: "weekends" | "weekdays" | null,
+  due: Date | null,
+  start: Date | null,
+  beforeTime: number | null,
+  afterTime: number | null,
+  createdAt: Date,
+  archived: "false" | "true",
+  parentGoalId: string,
+  goalColor: string,
+  language: string,
+  link: string | null,
+  collaboration: ICollaboration,
   shared: IShared,
   rootGoalId: string,
+  timeBudget: { duration: string, period: "week" | "day" } | null,
   typeOfGoal: "myGoal" | "shared" | "collaboration",
 }
