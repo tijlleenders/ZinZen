71f7ada9357ef7fc360b8517c7c069d9a92b67d3
Tushar <tusharinani93@gmail.com>
2023-04-20 20:08:13 +0530

    update: navigations via bottom navbar allowed

--- src/components/BottomNavbar/BottomNavbar.tsx
+++ src/components/BottomNavbar/BottomNavbar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 import goalsIcon from "@assets/images/goalsIcon.svg";
 import calendarIcon from "@assets/images/calendarIcon.svg";
@@ -9,24 +10,34 @@ import { getOrdinalSuffix } from "@src/utils";
 import "./BottomNavbar.scss";
 
 const BottomNavbar = () => {
+  const navigate = useNavigate();
   const currentPage = window.location.pathname.split("/")[1];
   const [month, date] = new Date().toDateString().split(" ").slice(1, 3);
 
-  console.log(currentPage);
+  const handleClick = (to: string) => {
+    if (to === "MyTime" && currentPage !== "/") {
+      navigate("/");
+    } else if (to === "MyGoals" && currentPage !== "MyGoals") {
+      navigate("/MyGoals");
+    } else if (currentPage !== "MyJournal") {
+      navigate("/MyJournal");
+    }
+  };
+
   return (
     <div className="bottom-navbar">
-      <div className={`bottom-nav-item ${currentPage === "MyTime" ? "active" : ""}`}>
+      <button type="button" onClick={() => { handleClick("MyTime"); }} className={`bottom-nav-item ${currentPage === "MyTime" ? "active" : ""}`}>
         <img className="secondary-icon" src={calendarIcon} alt="My Time" />
         <p>{date}<sup>{getOrdinalSuffix(Number(date))}</sup>{` ${month}`}</p>
-      </div>
-      <div className={`bottom-nav-item ${currentPage === "MyGoals" ? "active" : ""}`}>
+      </button>
+      <button type="button" onClick={() => { handleClick("MyGoals"); }} className={`bottom-nav-item ${currentPage === "MyGoals" ? "active" : ""}`}>
         <img className="secondary-icon" src={goalsIcon} alt="My Goals" />
         <p>Goals</p>
-      </div>
-      <div style={{ gap: 10 }} className={`bottom-nav-item ${currentPage === "MyJournal" ? "active" : ""}`}>
+      </button>
+      <button type="button" onClick={() => { handleClick("MyJournal"); }} style={{ gap: 10 }} className={`bottom-nav-item ${currentPage === "MyJournal" ? "active" : ""}`}>
         <img style={{ paddingTop: 3 }} className="secondary-icon" src={journalIcon} alt="My Journal" />
         <p>Journal</p>
-      </div>
+      </button>
     </div>
   );
 };
