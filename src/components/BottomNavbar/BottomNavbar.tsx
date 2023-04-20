b8d7e3bb5ffc5162385c54c16ab0113ca337357d
Tushar <tusharinani93@gmail.com>
2023-04-20 14:55:32 +0530

    added: Subtext of bottom nav icons

--- src/components/BottomNavbar/BottomNavbar.tsx
+++ src/components/BottomNavbar/BottomNavbar.tsx
@@ -4,21 +4,28 @@ import goalsIcon from "@assets/images/goalsIcon.svg";
 import calendarIcon from "@assets/images/calendarIcon.svg";
 import journalIcon from "@assets/images/journalIcon.svg";
 
+import { getOrdinalSuffix } from "@src/utils";
+
 import "./BottomNavbar.scss";
 
 const BottomNavbar = () => {
   const currentPage = window.location.pathname.split("/")[1];
-  console.log(currentPage)
+  const [month, date] = new Date().toDateString().split(" ").slice(1, 3);
+
+  console.log(currentPage);
   return (
     <div className="bottom-navbar">
       <div className={`bottom-nav-item ${currentPage === "MyTime" ? "active" : ""}`}>
-        <img src={calendarIcon} alt="My Time" />
+        <img className="secondary-icon" src={calendarIcon} alt="My Time" />
+        <p>{date}<sup>{getOrdinalSuffix(Number(date))}</sup>{` ${month}`}</p>
       </div>
       <div className={`bottom-nav-item ${currentPage === "MyGoals" ? "active" : ""}`}>
-        <img src={goalsIcon} alt="My Goals" />
+        <img className="secondary-icon" src={goalsIcon} alt="My Goals" />
+        <p>Goals</p>
       </div>
-      <div className={`bottom-nav-item ${currentPage === "MyJournal" ? "active" : ""}`}>
-        <img src={journalIcon} alt="My Journal" />
+      <div style={{ gap: 10 }} className={`bottom-nav-item ${currentPage === "MyJournal" ? "active" : ""}`}>
+        <img style={{ paddingTop: 3 }} className="secondary-icon" src={journalIcon} alt="My Journal" />
+        <p>Journal</p>
       </div>
     </div>
   );
