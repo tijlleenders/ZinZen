0a80f9302f3e1c9eeea1a6ae2e369ce8dd95a241
Tushar <tusharinani93@gmail.com>
2023-04-21 10:58:17 +0530

    fix: allow scrolling in app content only

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -6,7 +6,16 @@ import React from "react";
 const AppLayout: React.FC<AppLayoutProps> = ({ children, title }) => (
   <>
     <Header title={title} />
-    {children}
+    <div style={{
+      maxWidth: 600,
+      minWidth: 236,
+      overflow: "scroll",
+      width: "100%",
+      marginTop: 75,
+      height: "calc(100vh - 145px)"
+    }}
+    > {children}
+    </div>
     <BottomNavbar />
   </>
 );
