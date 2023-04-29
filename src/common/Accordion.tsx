4f5a6326f9006e2a29a30fac9050edf5e416c135
Tushar <tusharinani93@gmail.com>
2023-04-29 06:14:00 +0530

    patch: defaultActiveKey Param in Common accordion

--- src/common/Accordion.tsx
+++ src/common/Accordion.tsx
@@ -4,16 +4,17 @@ import { Collapse } from "antd";
 export interface ZAccordionProps {
   style: React.CSSProperties | undefined,
   panels: { header: string, body: ReactNode | ReactNode[] }[]
-  showCount: boolean
+  showCount: boolean,
+  defaultActiveKey: string[] | undefined
 }
-const ZAccordion : React.FC<ZAccordionProps> = ({ panels, style, showCount }) => (
-  <Collapse style={style}>
-    {panels.map((panel) => (
+const ZAccordion : React.FC<ZAccordionProps> = ({ panels, style, showCount, defaultActiveKey }) => (
+  <Collapse defaultActiveKey={defaultActiveKey} style={style}>
+    {panels.map((panel, index) => (
       <Collapse.Panel
+        key={`${index + 1}`}
         style={{ flexDirection: "row-reverse" }}
         // @ts-ignore
         header={`${panel.header} ${showCount ? `(${panel.body?.length})` : ""}`}
-        key={panel.header}
       > {panel.body}
       </Collapse.Panel>
     ))}
