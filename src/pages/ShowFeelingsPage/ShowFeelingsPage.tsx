146db6edb8449590aeb05c114cb2a1967d8aa999
Tushar <tusharinani93@gmail.com>
2023-04-30 12:05:59 +0530

    patch: using subheader comp in my journal

--- src/pages/ShowFeelingsPage/ShowFeelingsPage.tsx
+++ src/pages/ShowFeelingsPage/ShowFeelingsPage.tsx
@@ -2,16 +2,17 @@ import React, { useState, useEffect } from "react";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router";
-import { ChevronLeft, ChevronRight } from "react-bootstrap-icons";
 
-import { getAllFeelings } from "@api/FeelingsAPI";
 import { getDates } from "@utils";
 import { IFeelingItem } from "@models";
-import { darkModeState, displayToast } from "@store";
+import SubHeader from "@src/common/SubHeader";
 import { feelingListType } from "@src/global";
 import AppLayout from "@src/layouts/AppLayout";
-import { AddFeelingsPage } from "@pages/AddFeelingsPage/AddFeelingsPage";
+import { getAllFeelings } from "@api/FeelingsAPI";
+import { darkModeState, displayToast } from "@store";
 import { displayAddFeeling } from "@src/store/FeelingsState";
+import { AddFeelingsPage } from "@pages/AddFeelingsPage/AddFeelingsPage";
+
 import { ShowFeelingTemplate } from "./ShowFeelingTemplate";
 
 import "./ShowFeelings.scss";
@@ -68,31 +69,13 @@ export const ShowFeelingsPage = () => {
       <div>
         {feelingsList && date && (
           <div key={date}>
-            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", paddingTop: 10 }}>
-              <ChevronLeft style={{ flex: 1 }} onClick={() => { handleNavigation("left"); }} />
-              <p style={{ padding: "15px 0", margin: "0px 15px", flex: 2 }} className={`feelings-date${darkModeStatus ? "-dark" : ""}`}>
-                <span
-                  role="button"
-                  tabIndex={0}
-                  onClick={() => {
-                    navigate("/AddFeelings", {
-                      state: { feelingDate: new Date(date) },
-                    });
-                  }}
-                  onKeyDown={() => {
-                    navigate("/AddFeelings", {
-                      state: { feelingDate: new Date(date) },
-                    });
-                  }}
-                  style={{ cursor: "pointer" }}
-                >
-                  {new Date(date).toDateString() === new Date().toDateString()
-                    ? "Today"
-                    : new Date(date).toDateString()}
-                </span>
-              </p>
-              <ChevronRight style={{ flex: 1 }} onClick={() => { handleNavigation("right"); }} />
-            </div>
+            <SubHeader
+              title={new Date(date).toDateString() === new Date().toDateString()
+                ? "Today"
+                : new Date(date).toDateString()}
+              leftNav={() => handleNavigation("left")}
+              rightNav={() => handleNavigation("right")}
+            />
             {feelingsList[date] && feelingsList[date].length > 0 && (
             <ShowFeelingTemplate
               key={date}
