71ebc73b6e00d6d49d689173dfd0afdd7e6f35b9
Tushar <tusharinani93@gmail.com>
2023-04-30 12:03:54 +0530

    finalized: dropdown and search integration in header file

--- src/common/Header/Header.tsx
+++ src/common/Header/Header.tsx
@@ -1,32 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Dropdown, Switch } from "antd";
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
-import { darkModeState, displayToast } from "@src/store";
+import { darkModeState, displayInbox, displayToast, searchActive } from "@src/store";
 import type { MenuProps } from "antd/es/menu/menu";
 
-import bulbIcon from "@assets/images/bulbIcon.svg";
 import searchIcon from "@assets/images/searchIcon.svg";
 import verticalDots from "@assets/images/verticalDots.svg";
+
+import { IHeader } from "@src/Interfaces/ICommon";
+import { themeState } from "@src/store/ThemeState";
+
+import { getActiveSharedWMGoals } from "@src/api/SharedWMAPI";
+import Search from "../Search";
+import { inboxIcon } from "../../assets";
 
 import "./Header.scss";
 
 const HeaderBtn = ({ path, alt } : {path: string, alt: string}) => {
-  const [darkMode, setDarkMode] = useRecoilState(darkModeState);
+  const theme = useRecoilValue(themeState);
 
   const setShowToast = useSetRecoilState(displayToast);
+  const setDisplaySearch = useSetRecoilState(searchActive);
+
+  const [openInbox, setOpenInbox] = useRecoilState(displayInbox);
+  const [darkModeStatus, setDarkModeStatus] = useRecoilState(darkModeState);
+
   const items: MenuProps["items"] = [
+    ...["Inbox", "Donate", "Feedback"].map((ele, index) => ({
+      label: ele,
+      key: `${index}`
+    })),
     {
       label: (
-        <div style={{ display: "flex" }}>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
           <p>Dark Mode</p>
           <Switch
-            checked={darkMode}
-            onChange={() => { localStorage.setItem("darkMode", darkMode ? "off" : "on"); setDarkMode(!darkMode); }}
+            checked={darkModeStatus}
+            onChange={() => { localStorage.setItem("darkMode", darkModeStatus ? "off" : "on"); setDarkModeStatus(!darkModeStatus); }}
           />
         </div>
       ),
-      key: "1",
-    }
+      key: "7",
+    },
   ];
 
   const handleClick = async () => {
@@ -40,14 +55,18 @@ const HeaderBtn = ({ path, alt } : {path: string, alt: string}) => {
       // } else {
       //   setShowToast({ open: true, message: "Awww... no hints today. We'll keep looking!", extra: "" });
       // }
+    } else if (alt === "zinzen search") {
+      setDisplaySearch(true);
+    } else if (alt === "zinzen inbox") {
+      setOpenInbox(!openInbox);
     }
     // setLoading(false);
   };
   return (
     <div style={{ alignSelf: "center" }}>
       { alt === "zinzen settings" ? (
-        <Dropdown menu={{ items }} trigger={["click"]}>
-          <img className="theme-icon header-icon" src={path} alt={alt} />
+        <Dropdown rootClassName={`header-dropdown${darkModeStatus ? "-dark" : ""} ${darkModeStatus ? "dark" : "light"}-theme${theme[darkModeStatus ? "dark" : "light"]}`} overlayStyle={{ width: 175 }} menu={{ items }} trigger={["click"]}>
+          <img className="theme-icon header-icon settings-icon" src={path} alt={alt} />
         </Dropdown>
       ) : (
         <img
@@ -60,16 +79,43 @@ const HeaderBtn = ({ path, alt } : {path: string, alt: string}) => {
     </div>
   );
 };
-const Header = ({ title } : { title: string }) => {
+const Header: React.FC<IHeader> = ({ title, debounceSearch }) => {
+  const displaySearch = useRecoilValue(searchActive);
   const darkModeStatus = useRecoilValue(darkModeState);
+  const [openInbox, setOpenInbox] = useRecoilState(displayInbox);
+  const [showInboxOption, setShowInboxOption] = useState(false);
+
+  useEffect(() => {
+    getActiveSharedWMGoals().then((res) => {
+      if (res && res.length > 0) {
+        setShowInboxOption(true);
+      }
+    });
+  });
   return (
     <div className="header" style={{ background: darkModeStatus ? "var(--selection-color)" : "transparent" }}>
-      <h6>{title || "My Goals"}</h6>
-      <div className="header-items">
-        <HeaderBtn path={searchIcon} alt="zinzen search" />
-        <HeaderBtn path={bulbIcon} alt="zinzen hints" />
-        <HeaderBtn path={verticalDots} alt="zinzen settings" />
-      </div>
+      { displaySearch && debounceSearch ?
+        <Search debounceSearch={debounceSearch} />
+        : (
+          <>
+            <h6>{title}</h6>
+            <div className="header-items">
+              { ["My Goals", "Inbox"].includes(title) && <HeaderBtn path={searchIcon} alt="zinzen search" /> }
+              { showInboxOption && <HeaderBtn path={inboxIcon} alt="zinzen inbox" /> }
+              {/* { title === "My Time" && (
+                <p style={{
+                  fontSize: 26,
+                  position: "absolute",
+                  top: 10,
+                  right: 78,
+                  fontWeight: 500
+                }}>D
+                </p>
+              ) } */}
+              <HeaderBtn path={verticalDots} alt="zinzen settings" />
+            </div>
+          </>
+        )}
     </div>
   );
 };
