ec997f1eb977144700442b90bf5c2f38cf09a966
Tushar <tusharinani93@gmail.com>
2023-04-30 18:27:17 +0530

    patch: dark & light mode logo of zinzen in onboarding layout

--- src/layouts/OnboardingLayout.tsx
+++ src/layouts/OnboardingLayout.tsx
@@ -1,29 +1,36 @@
 import React from "react";
 
 import ZinZenTextLight from "@assets/images/LogoTextLight.svg";
+import ZinZenTextDark from "@assets/images/LogoTextDark.svg";
 
-const OnboardingLayout = ({ children }: {children: React.ReactNode}) => (
-  <div style={{
-    textAlign: "center",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    background: "transparent",
-    paddingBottom: 16,
-    borderRadius: "1.275em",
-    maxWidth: 320,
-    paddingTop: 16,
-    width: "100%",
-  }}
-  >
-    <img
-      role="presentation"
-      src={ZinZenTextLight}
-      alt="ZinZen Text Logo"
-      className="zinzen-textLogo"
-    />
-    {children}
-  </div>
-);
+import { useRecoilValue } from "recoil";
+import { darkModeState } from "@src/store";
+
+const OnboardingLayout = ({ children }: {children: React.ReactNode}) => {
+  const darkModeStatus = useRecoilValue(darkModeState);
+  return (
+    <div style={{
+      textAlign: "center",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      background: "transparent",
+      paddingBottom: 16,
+      borderRadius: "1.275em",
+      maxWidth: 320,
+      paddingTop: 16,
+      width: "100%",
+    }}
+    >
+      <img
+        role="presentation"
+        src={darkModeStatus ? ZinZenTextDark : ZinZenTextLight}
+        alt="ZinZen Text Logo"
+        className="zinzen-textLogo"
+      />
+      {children}
+    </div>
+  );
+};
 
 export default OnboardingLayout;
