bb4bda19c454b96214178442690f24008c8fb0f6
Tushar <tusharinani93@gmail.com>
2023-04-30 12:03:18 +0530

    developed: subheader as a common comp. for Time and feelings page

---
+++ src/common/SubHeader.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
+import { useRecoilValue } from "recoil";
+import { darkModeState } from "@src/store";
+
+import chevronLeftIcon from "@assets/images/chevronLeft.svg";
+
+import { ISubHeaderProps } from "@src/Interfaces/ICommon";
+
+const NavBtn = ({ className, handleClick } : { className: string, handleClick: () => void }) => (
+  <button type="button" onClick={handleClick} style={{ background: "transparent", border: "none", flex: 1 }}>
+    <img src={chevronLeftIcon} className={className} />
+  </button>
+);
+
+const SubHeader: React.FC<ISubHeaderProps> = ({ leftNav, rightNav, title }) => {
+  const darkModeStatus = useRecoilValue(darkModeState);
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", paddingTop: 10 }}>
+      <NavBtn className="chevronLeft" handleClick={leftNav} />
+      <p style={{ padding: "15px 0", margin: "0px 15px", flex: 2 }} className={`feelings-date${darkModeStatus ? "-dark" : ""}`}>
+        <span
+          role="button"
+          tabIndex={0}
+          style={{ cursor: "pointer" }}
+        >
+          {title}
+        </span>
+      </p>
+      <NavBtn className="chevronRight" handleClick={rightNav} />
+    </div>
+
+  );
+};
+
+export default SubHeader;
