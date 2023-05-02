ec22699a6d53e0e3d8bcd649524097f705f79edd
Tushar <tusharinani93@gmail.com>
2023-05-02 07:38:53 +0530

    fix: check for valid value of theme in local storage

--- src/store/ThemeState.ts
+++ src/store/ThemeState.ts
@@ -2,7 +2,9 @@ import { atom } from "recoil";
 
 export const getTheme = () => {
   const x = localStorage.getItem("theme");
-  return x ? JSON.parse(x) : { light: 1, dark: 1 };
+  if (x && x.search(/^\{"light":\d,"dark":\d\}$/i) === 0) {
+    return JSON.parse(x);
+  } return { light: 1, dark: 1 };
 };
 export const themeState = atom({
   key: "themeState",
