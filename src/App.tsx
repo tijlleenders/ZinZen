797be6e7f8b982fcc6b6e17d7694ead8001b3518
Tushar <tusharinani93@gmail.com>
2023-05-24 21:58:40 +0530

    Update: using Antd toast for notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { notification } from "antd";
 import { v4 as uuidv4 } from "uuid";
 import React, { useEffect } from "react";
-import Toast from "react-bootstrap/Toast";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { darkModeState, languageSelectionState, displayToast, lastAction, showConfirmation, backupRestoreModal, openDevMode } from "@store";
@@ -14,7 +14,6 @@ import { MyTimePage } from "@pages/MyTimePage/MyTimePage";
 import MyGroupsPage from "@pages/MyGroupsPage/MyGroupsPage";
 import { MyGoalsPage } from "@pages/MyGoalsPage/MyGoalsPage";
 import { LandingPage } from "@pages/LandingPage/LandingPage";
-import { NotFoundPage } from "@pages/NotFoundPage/NotFoundPage";
 import { FeedbackPage } from "@pages/FeedbackPage/FeedbackPage";
 import { ShowFeelingsPage } from "@pages/ShowFeelingsPage/ShowFeelingsPage";
 
@@ -33,8 +32,8 @@ import { getContactByRelId, updateAllUnacceptedContacts } from "./api/ContactsAP
 import "./global.scss";
 import "./customize.scss";
 import "./override.scss";
-import "@fontsource/montserrat";
-import "bootstrap/dist/css/bootstrap.min.css";
+
+const Context = React.createContext({ name: "Default" });
 
 const App = () => {
   const theme = useRecoilValue(themeState);
@@ -47,6 +46,16 @@ const App = () => {
   const [devMode, setDevMode] = useRecoilState(openDevMode);
   const [showToast, setShowToast] = useRecoilState(displayToast);
   const setLastAction = useSetRecoilState(lastAction);
+  const [api, contextHolder] = notification.useNotification();
+
+  const openNotification = () => {
+    api.info({
+      message: `${showToast.message}`,
+      description: <Context.Consumer>{() => `${showToast.extra}`}</Context.Consumer>,
+      placement: "top",
+      duration: 4
+    });
+  };
 
   useEffect(() => {
     const init = async () => {
@@ -111,11 +120,20 @@ const App = () => {
     };
     checkDevMode();
   }, []);
+
+  useEffect(() => {
+    if (showToast) {
+      openNotification();
+      setShowToast({ ...showToast, open: false });
+    }
+  }, [showToast]);
+
   return (
     <div className={`${darkModeEnabled ? "dark" : "light"}-theme${theme[darkModeEnabled ? "dark" : "light"]}`}>
       <div className={`App-${darkModeEnabled ? "dark" : "light"}`}>
         <BrowserRouter>
           {isLanguageChosen}
+          {contextHolder}
           <Routes>
             {!isLanguageChosen ? (
               <Route path="/" element={<LandingPage />} />
@@ -127,18 +145,12 @@ const App = () => {
             <Route path="/MyGoals" element={<MyGoalsPage />} />
             <Route path="/MyGroups" element={<MyGroupsPage />} />
             <Route path="/MyJournal" element={<ShowFeelingsPage />} />
-            <Route path="*" element={<NotFoundPage />} />
+            <Route path="*" element={<MyGoalsPage />} />
             <Route path="/ZinZenFAQ" element={<FAQPage />} />
             <Route path="/invite/:id" element={<InvitePage />} />
             <Route path="/Invest" element={<InvestPage />} />
           </Routes>
         </BrowserRouter>
-        <Toast autohide delay={5000} show={showToast.open} onClose={() => setShowToast({ ...showToast, open: false })} id="zinzen-toast">
-          <Toast.Body>
-            <p id="toast-message" style={showToast.extra === "" ? { margin: 0 } : {}}>{showToast.message}</p>
-            { showToast.extra !== "" && <p id="extra-message">{showToast.extra}</p> }
-          </Toast.Body>
-        </Toast>
         { displayBackupRestoreModal && <BackupRestoreModal /> }
       </div>
     </div>
