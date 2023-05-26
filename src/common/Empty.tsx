a28e4762db9a9bad4b7a52de6e8a45849c0ac331
Tushar <tusharinani93@gmail.com>
2023-05-26 18:44:03 +0530

    Patch: New Messge when update available

--- src/common/Empty.tsx
+++ src/common/Empty.tsx
@@ -2,10 +2,18 @@ import React from "react";
 
 import emptyIllustration from "@assets/images/emptySvg.svg";
 
+const isUpdgradeAvailable = localStorage.getItem("updateAvailable") === "true";
+
 const Empty = () => (
   <div style={{ textAlign: "center" }}>
     <img alt="Zinzen Empty" src={emptyIllustration} style={{ width: 300, height: 300 }} />
-    <p>😇 It&apos;s empty today 😇</p>
+    <p>😇 It&apos;s empty today 😇
+      { isUpdgradeAvailable && (
+      <>
+        <br />But ZinZen brought new updates for you
+      </>
+      )}
+    </p>
   </div>
 
 );
