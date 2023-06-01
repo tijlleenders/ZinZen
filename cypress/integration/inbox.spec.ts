8d75de16818b09968a6914ea01b486a05c8243c3
Tushar <tusharinani93@gmail.com>
2023-06-01 15:44:51 +0530

    created: tests for upgrade notification in inbox

---
+++ cypress/integration/inbox.spec.ts
@@ -0,0 +1,37 @@
+import { completeOnboarding } from "./landing-page.spec";
+
+describe("Inbox component", () => {
+  before(() => {
+    // Set the upgrade available flag in localStorage
+    localStorage.setItem("updateAvailable", "true");
+    completeOnboarding();
+    cy.visit("/MyGoals");
+    cy.get(".header-items")
+      .find(".header-icon[alt='zinzen inbox']")
+      .click();
+  });
+  it("should render the upgrade message when an upgrade is available", () => {
+    cy.visit("/MyGoals");
+
+    cy.get(".header-items")
+      .find(".header-icon[alt='zinzen inbox']")
+      .click();
+
+    // Assert that the upgrade message is displayed
+    cy.contains("Notifications").click();
+
+    cy.get(".notification-item") // Check if the notification item is rendered
+      .should("exist")
+      .contains("Update Available !!"); // Check if the notification text is correct
+
+    cy.get(".default-btn") // Check if the update button is rendered
+      .should("exist")
+      .contains("Update Now"); // Check if the button text is correct
+  });
+
+  // it('triggers update when the "Update Now" button is clicked', () => {
+  //   cy.contains("Update Now").click(); // Click the "Update Now" button
+  //   cy.get(".notification-item") // Check if the notification item is rendered
+  //     .should("not.exist");
+  // });
+});
