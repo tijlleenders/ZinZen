43127b8afbd20884890ee01c291845ed54e455a7
Tushar <tusharinani93@gmail.com>
2023-06-01 11:07:30 +0530

    created: handler for passing theme switching state in browser history

--- src/hooks/useGlobalStore.tsx
+++ src/hooks/useGlobalStore.tsx
@@ -4,11 +4,13 @@ import { useLocation, useNavigate } from "react-router-dom";
 
 import { ILocationState } from "@src/Interfaces";
 import { backupRestoreModal } from "@src/store";
+import { themeSelectionMode } from "@src/store/ThemeState";
 
 function useGlobalStore() {
   const location = useLocation();
   const navigate = useNavigate();
   const [openBackupModal, setBackupRestoreModal] = useRecoilState(backupRestoreModal);
+  const [themeSelection, setThemeSelection] = useRecoilState(themeSelectionMode);
 
   const handleLocationChange = () => {
     const locationState : ILocationState = location.state || {};
@@ -17,16 +19,25 @@ function useGlobalStore() {
     } else if (locationState.displayBackResModal) {
       setBackupRestoreModal(locationState.displayBackResModal);
     }
+
+    if (themeSelection) {
+      setThemeSelection(false);
+    } else if (locationState.changeTheme) {
+      setThemeSelection(locationState.changeTheme);
+    }
   };
 
   const handleBackResModal = () => { navigate(window.location.pathname, { state: { displayBackResModal: true } }); };
+
+  const handleChangeTheme = () => { navigate("/MyGoals", { state: { ...location.state, changeTheme: true } }); };
 
   useEffect(() => {
     handleLocationChange();
   }, [location]);
 
   return {
-    handleBackResModal
+    handleBackResModal,
+    handleChangeTheme
   };
 }
 
