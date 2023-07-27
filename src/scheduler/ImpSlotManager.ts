a1b2d1757ddc721dbec1ec2ffabfba7fe0fd8c7f
Tushar Inani <tusharinani93@gmail.com>
2023-07-27 22:19:26 +0530

    update: fillUpImpSlotsForGoalId supports start and end day

--- src/scheduler/ImpSlotManager.ts
+++ src/scheduler/ImpSlotManager.ts
@@ -18,11 +18,11 @@ export const createImpossibleSlot = (task: ISchedulerOutputSlot, tmpStartDate: D
   };
 };
 
-export const fillUpImpSlotsForGoalId = (goalId: string, lastDay: number) => {
+export const fillUpImpSlotsForGoalId = (goalId: string, startDay: number, lastDay: number) => {
   let dueHrs = getDueHrs(goalId);
-  console.log("🚀 ~ file: ImpSlotManager.ts:23 ~ fillUpImpSlotsForGoalId ~ dueHrs:", dueHrs)
+  // console.log("🚀 ~ file: ImpSlotManager.ts:23 ~ fillUpImpSlotsForGoalId ~ dueHrs:", dueHrs);
   if (dueHrs) {
-    for (let dayItr = 1; dayItr < lastDay && dueHrs !== 0; dayItr += 1) {
+    for (let dayItr = startDay; dayItr < lastDay && dueHrs !== 0; dayItr += 1) {
       const pickSlots = getBlockingSlotsOfTheDayForGoalId(goalId, dayItr);
       if (pickSlots.length > 0) {
         for (let psIndex = 0; psIndex < pickSlots.length && dueHrs !== 0; psIndex += 1) {
@@ -30,14 +30,12 @@ export const fillUpImpSlotsForGoalId = (goalId: string, lastDay: number) => {
           const currentBlockers = getUsedBlockers(thisSlot.goalid);
           if (currentBlockers) {
             if (currentBlockers[dayItr]?.includes(psIndex)) {
-              // if (thisSlot.goalid === "6debddbd-6558-4a1f-9884-1b3467b887cb") { console.log("exists", thisSlot); }
               continue;
             }
             updateBlockers(thisSlot.goalid, dayItr, [...(currentBlockers[dayItr] || []), psIndex]);
           } else {
             initBlockers(thisSlot.goalid, dayItr, psIndex);
           }
-          // if (thisSlot.goalid === "53a8e2e5-b659-4f1f-8b44-b394158dc49d") { console.log("🚀 ~ file: MiniScheduler.ts:262 ~ thisSlot:", thisSlot, dueHrs, getHrFromDateString(thisSlot.start), (thisSlot.duration - dueHrs)); }
           if (thisSlot.duration > dueHrs) {
             pushToImpossible(dayItr, {
               ...thisSlot,
