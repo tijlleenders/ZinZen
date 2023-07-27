311a3ae4dd918598208b541cec725ee674b1f23c
Tushar Inani <tusharinani93@gmail.com>
2023-07-27 10:25:28 +0530

    fix: valid days not iterable because of bad assignment

--- src/scheduler/index.ts
+++ src/scheduler/index.ts
@@ -35,7 +35,7 @@ export const pushTaskToMyDays = (selectedDay: number, slot: ISchedulerOutputSlot
 export const pushTaskToFlexibleArr = (validDays: string[], slot: ISchedulerOutputSlot) => {
   flexibleWeeklyGoals.push({
     slot: { ...slot },
-    validDays: { ...validDays }
+    validDays: [...validDays]
   });
 };
 
@@ -60,6 +60,7 @@ export const getBufferValue = (goalId: string) => buffer[goalId];
 export const getBlockingSlotsOfTheDayForGoalId = (goalId: string, day: number) => blockingSlots[goalId][day];
 
 export const getAllDueTasks = () => ({ ...dueTaskHrs });
+
 export const getDueHrs = (goalId: string) => (dueTaskHrs[goalId]);
 
 export const getUsedBlockers = (goalId: string) => (usedBlockers[goalId]);
@@ -159,4 +160,4 @@ export const resetAll = () => {
   scheduled = [];
   impossible = [];
   flexibleWeeklyGoals = [];
-}
+};
