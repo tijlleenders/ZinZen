4dbd13f80eaa7fe230c145ee0e284a5cb3b53f1f
Tushar Inani <tusharinani93@gmail.com>
2023-08-05 22:10:47 +0530

    patch: start day initialize conditionally for goals while generating tasks

--- src/scheduler/TaskGenerator.ts
+++ src/scheduler/TaskGenerator.ts
@@ -21,7 +21,7 @@ const processBudgetGoal = (
   iGoalStart: Date,
   startDayItr: number
 ) => {
-  let goalStart = new Date(iGoalStart);
+  let goalStart = iGoalStart < new Date() ? new Date() : new Date(iGoalStart);
   const { after_time = 0 } = goal.filters || {};
   const slot = { ...currSlot };
   let totalDuration = inputDuration - (goal.hoursSpent || 0);
@@ -101,6 +101,7 @@ const goalProcessor = (goal: ISchedulerInputGoal, weekStart: Date, pushToNext: b
   }
 
   validDays = validDays.filter((ele) => !not_on.includes(ele));
+  if (slot.title === "Work") console.log("🚀 ~ file: TaskGenerator.ts:92 ~ goalProcessor ~ validDays:", validDays)
 
   if (goal.repeat || goal.filters?.on_days) {
     if (goal.repeat === "daily") {
