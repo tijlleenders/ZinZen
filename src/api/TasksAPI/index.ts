a7b63e0395a1139d1147834bde1d3a8bb2ec9e3f
Tushar Inani <tusharinani93@gmail.com>
2023-08-09 21:21:46 +0530

    created: api to refresh task collection once in a day daily

--- src/api/TasksAPI/index.ts
+++ src/api/TasksAPI/index.ts
@@ -23,6 +23,59 @@ export const getTaskByGoalId = async (goalId: string) => {
   }
 };
 
+export const getForgetHrsCount = (task: TaskItem) => {
+  let yesterdaysCount = 0;
+  task.forgotToday.forEach((slot) => {
+    const [start, end] = slot.split("-");
+    yesterdaysCount += Number(end) - Number(start);
+  });
+  return yesterdaysCount;
+};
+
+export const refreshTaskCollection = async () => {
+  const tasks = await db.taskCollection.toArray();
+  const goals: { [key: string]: GoalItem } = (await Promise.all(tasks.map((ele) => getGoal(ele.goalId)))).reduce(
+    (acc, curr) => {
+      return curr ? { ...acc, [curr.id]: { ...curr } } : acc;
+    },
+    {}
+  );
+  try {
+    await db.transaction("rw", db.taskCollection, async () => {
+      const updatedRows = tasks.map((_task) => {
+        const task = { ..._task };
+        const goal: GoalItem = goals[task.goalId];
+        const startDate = new Date(goal.start || goal.createdAt);
+        if (goal.habit === "daily") {
+          task.hoursSpent = 0;
+        } else if (goal.habit === "weekly") {
+          const dayIndex = calDays.indexOf(convertDateToDay(startDate));
+          const lastReset = getLastDayDate(dayIndex);
+          const lastAction = new Date(
+            new Date(task.lastForget) < new Date(task.lastCompleted) ? task.lastCompleted : task.lastForget
+          );
+          if (lastAction < lastReset) {
+            task.hoursSpent = 0;
+          } else {
+            task.hoursSpent += task.completedToday + getForgetHrsCount(task);
+          }
+        } else {
+          task.hoursSpent += task.completedToday + getForgetHrsCount(task);
+        }
+        task.completedToday = 0;
+        task.forgotToday = [];
+        task.lastCompleted = new Date().toLocaleDateString();
+        task.lastForget = new Date().toLocaleDateString();
+        return task;
+      });
+
+      // Bulk update the rows
+      await db.taskCollection.bulkPut(updatedRows);
+    });
+  } catch (error) {
+    console.error("Error updating field:", error);
+  }
+};
 export const completeTask = async (id: string, duration: number) => {
   db.transaction("rw", db.taskCollection, async () => {
     await db.taskCollection.where("id").equals(id)
