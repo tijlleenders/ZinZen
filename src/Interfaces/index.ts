62dd56728731f5a80e1d7ad91d43a772eaba1293
Tushar Inani <tusharinani93@gmail.com>
2023-08-19 16:38:13 +0530

    created: new state to control actions popup

--- src/Interfaces/index.ts
+++ src/Interfaces/index.ts
@@ -1,20 +1,22 @@
 import { ISubGoalHistory } from "@src/store/GoalsState";
+import { GoalItem } from "@src/models/GoalItem";
 import { ILanguage, ILanguageListProps } from "./ILanguage";
 import { confirmActionState } from "./IPopupModals";
 
 export { ILanguage, ILanguageListProps };
 
 export interface ILocationState {
-    goalsHistory?: ISubGoalHistory[], // heirarchy history of a goal
-    displayUpdateGoal?: string, // id of goal to be updated
-    activeGoalId?: string, // selected goal id
-    displayAddGoal?: string // parent id of the goal to be added
-    displayShareModal?: string // show share modal
-    displayConfirmation?: confirmActionState // show confirmation modal
-    displayBackResModal?: boolean // show backup restore modal
-    displayAddFeeling?: boolean // show add feeling modal
-    displayNoteModal?: number // show saved note of feeling id
-    displayInputNoteModal?: number // show input modal for adding note for id
-    changeTheme?: boolean // theme changer mode
-    expandedGoalId?: string // id of goal to be expanded
+  goalsHistory?: ISubGoalHistory[]; // heirarchy history of a goal
+  displayUpdateGoal?: string; // id of goal to be updated
+  activeGoalId?: string; // selected goal id
+  displayAddGoal?: string; // parent id of the goal to be added
+  displayShareModal?: string; // show share modal
+  displayConfirmation?: confirmActionState; // show confirmation modal
+  displayBackResModal?: boolean; // show backup restore modal
+  displayAddFeeling?: boolean; // show add feeling modal
+  displayNoteModal?: number; // show saved note of feeling id
+  displayInputNoteModal?: number; // show input modal for adding note for id
+  changeTheme?: boolean; // theme changer mode
+  expandedGoalId?: string; // id of goal to be expanded
+  displayGoalActions?: GoalItem; // id of goal whose actions have to be opened
 }
