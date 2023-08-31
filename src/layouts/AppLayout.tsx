9259aa7725b4e98f00eef3fe8d1fe49b8ddbe1f0
Tushar Inani <tusharinani93@gmail.com>
2023-09-01 01:41:57 +0530

    prettifying code of app layout

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -8,16 +8,18 @@ import Header from "../components/Header/Header";
 const AppLayout: React.FC<AppLayoutProps> = ({ children, title, debounceSearch }) => (
   <>
     <Header title={title} debounceSearch={debounceSearch} />
-    <div style={{
-      maxWidth: 600,
-      minWidth: 236,
-      overflow: "scroll",
-      width: "100%",
-      marginTop: 57,
-      height: "calc(100vh - 112px)",
-      position: "sticky",
-    }}
-    > {children}
+    <div
+      style={{
+        maxWidth: 600,
+        minWidth: 236,
+        overflow: "scroll",
+        width: "100%",
+        marginTop: 57,
+        height: "calc(100vh - 112px)",
+        position: "sticky",
+      }}
+    >
+      {children}
     </div>
     <BottomNavbar title={title} />
   </>
