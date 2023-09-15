d49cfcaf7f90311e0429821274724117f5cc3d86
Tijl Leenders <gh@zinzen.com>
2023-09-15 06:38:16 +0200

    Update artifacts.

--- pkg/scheduler.d.ts
+++ pkg/scheduler.d.ts
@@ -22,12 +22,9 @@ export interface InitOutput {
   readonly schedule: (a: number, b: number) => void;
   readonly __wbindgen_malloc: (a: number, b: number) => number;
   readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
-  readonly __wbindgen_export_2: WebAssembly.Table;
   readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
-  readonly __externref_table_dealloc: (a: number) => void;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
   readonly __wbindgen_exn_store: (a: number) => void;
-  readonly __externref_table_alloc: () => number;
-  readonly __wbindgen_start: () => void;
 }
 
 export type SyncInitInput = BufferSource | WebAssembly.Module;
