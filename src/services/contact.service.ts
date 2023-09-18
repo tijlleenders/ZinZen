16577ddbd78a9f6a9ee759fd13ac05f3db8fddaf
Tushar Inani <tusharinani93@gmail.com>
2023-09-18 20:57:49 +0530

    code formatting: contact service

--- src/services/contact.service.ts
+++ src/services/contact.service.ts
@@ -23,34 +23,54 @@ export const acceptRelationship = async () => {
 
 export const shareGoalWithContact = async (relId: string, goalWithChildrens: GoalItem[]) => {
   const url = "https://od66oidjc64tghsplm2s4seuau0dbkgy.lambda-url.eu-west-1.on.aws/";
-  const res = await createContactRequest(url, { method: "shareMessage", installId: getInstallId(), relId, event: { type: "shareMessage", goalWithChildrens } });
+  const res = await createContactRequest(url, {
+    method: "shareMessage",
+    installId: getInstallId(),
+    relId,
+    event: { type: "shareMessage", goalWithChildrens },
+  });
   return res;
 };
 
 export const collaborateWithContact = async (relId: string, goal: GoalItem) => {
   const url = "https://od66oidjc64tghsplm2s4seuau0dbkgy.lambda-url.eu-west-1.on.aws/";
-  const res = await createContactRequest(url, { method: "shareMessage", installId: getInstallId(), relId, event: { type: "collaborationInvite", goal } });
+  const res = await createContactRequest(url, {
+    method: "shareMessage",
+    installId: getInstallId(),
+    relId,
+    event: { type: "collaborationInvite", goal },
+  });
   return res;
 };
 
 export const getContactSharedGoals = async () => {
   const lastProcessedTimestamp = new Date(Number(localStorage.getItem("lastProcessedTimestamp"))).toISOString();
   const url = "https://od66oidjc64tghsplm2s4seuau0dbkgy.lambda-url.eu-west-1.on.aws/";
-  const res = await createContactRequest(url, { method: "getMessages", installId: getInstallId(), ...(lastProcessedTimestamp ? { lastProcessedTimestamp } : {}) });
+  const res = await createContactRequest(url, {
+    method: "getMessages",
+    installId: getInstallId(),
+    ...(lastProcessedTimestamp ? { lastProcessedTimestamp } : {}),
+  });
   localStorage.setItem("lastProcessedTimestamp", `${Date.now()}`);
   return res;
 };
 
 export const getRelationshipStatus = async (relationshipId: string) => {
   const url = "https://n65hkx5nehmmkzy5wp6ijyarka0qujrj.lambda-url.eu-west-1.on.aws/";
-  const res = await createContactRequest(url, { method: "getRelationshipStatus", installId: getInstallId(), relationshipId });
+  const res = await createContactRequest(url, {
+    method: "getRelationshipStatus",
+    installId: getInstallId(),
+    relationshipId,
+  });
   return res;
 };
 
 export const sendUpdatesToSubscriber = async (
-  sub: ISubscriber, rootGoalId: string,
+  sub: ISubscriber,
+  rootGoalId: string,
   changeType: typeOfChange,
-  changes: { level: number, goal: GoalItem }[] | { level: number, id: string }[]) => {
+  changes: { level: number; goal: GoalItem }[] | { level: number; id: string }[],
+) => {
   const url = "https://od66oidjc64tghsplm2s4seuau0dbkgy.lambda-url.eu-west-1.on.aws/";
   const { subId, type } = sub;
   const requestBody = {
@@ -61,8 +81,8 @@ export const sendUpdatesToSubscriber = async (
       type,
       changeType,
       rootGoalId,
-      changes
-    }
+      changes,
+    },
   };
   const res = await createContactRequest(url, requestBody);
   return res;
