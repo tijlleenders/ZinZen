3724ff722ee91189c0271531f89b7faf9da0b40c
Tushar Inani <tusharinani93@gmail.com>
2023-09-18 21:07:52 +0530

    removed: outbox collection

--- src/models/db.ts
+++ src/models/db.ts
@@ -3,7 +3,6 @@ import Dexie, { Table } from "dexie";
 import { IFeelingItem } from "./FeelingItem";
 import { GoalItem } from "./GoalItem";
 import ContactItem from "./ContactItem";
-import { OutboxItem } from "./OutboxItem";
 import { PubSubItem } from "./PubSubItem";
 import { InboxItem } from "./InboxItem";
 import { PublicGroupItem } from "./PublicGroupItem";
@@ -12,7 +11,7 @@ import { GCustomItem } from "./GCustomItem";
 import { DumpboxItem } from "./DumpboxItem";
 import { PartnerItem } from "./PartnerItem";
 
-export const dexieVersion = 8;
+export const dexieVersion = 9;
 
 localStorage.setItem("dexieVersion", `${dexieVersion}`);
 
@@ -24,8 +23,6 @@ export class ZinZenDB extends Dexie {
   contactsCollection!: Table<ContactItem, number>;
 
   sharedWMCollection!: Table<GoalItem, string>;
-
-  outboxCollection!: Table<OutboxItem, number>;
 
   pubSubCollection!: Table<PubSubItem, string>;
 
@@ -51,7 +48,7 @@ export class ZinZenDB extends Dexie {
         sharedWMCollection:
           "id, title, duration, sublist, repeat, start, due, afterTime, beforeTime, createdAt, parentGoalId, archived, goalColor, language, link, collaboration, shared, rootGoalId, timeBudget, typeOfGoal",
         contactsCollection: "id, name, collaborativeGoals, sharedGoals, relId, accepted, createdAt",
-        outboxCollection: "++id, relId, goalId, subgoals, updates, deleted, completed, anyUpdates",
+        outboxCollection: null,
         inboxCollection: "id, goalChanges",
         pubSubCollection: "id, subscribers",
         publicGroupsCollection: "id, title, polls, language, groupColor, createdAt",
