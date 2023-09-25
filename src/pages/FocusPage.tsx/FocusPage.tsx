0762275f10573734b93f3a718a7e74b22b9e3a0a
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2023-09-25 19:33:56 +0530

    add: edit time functionality

--- src/pages/FocusPage.tsx/FocusPage.tsx
+++ src/pages/FocusPage.tsx/FocusPage.tsx
@@ -12,14 +12,18 @@ import "./focusPage.scss";
 export const FocusPage = () => {
   const darkModeStatus = useRecoilValue(darkModeState);
   const location = useLocation();
-  const taskTitle = location.state?.taskTitle || "Task Name";
+  const taskTitle = location.state?.taskTitle || "Task";
 
-  const [initialTime] = useState(0.5 * 60); // Initial time in seconds
+  const [initialTime] = useState(0.5 * 60); // Default initial time in seconds
   const [time, setTime] = useState(initialTime);
   const [isActive, setIsActive] = useState(false);
+  const [editMode, setEditMode] = useState(false);
+  const [newTime, setNewTime] = useState("");
+  const [userEnteredTime, setUserEnteredTime] = useState(null);
 
   useEffect(() => {
-    let interval: NodeJS.Timeout | null = null;
+    let interval;
+
     if (isActive && time > 0) {
       interval = setInterval(() => {
         setTime((prevTime) => prevTime - 1);
@@ -33,36 +37,72 @@ export const FocusPage = () => {
   const toggle = () => {
     setIsActive(!isActive);
   };
-
   const reset = () => {
-    setTime(initialTime);
+    if (userEnteredTime !== null) {
+      setTime(userEnteredTime);
+    } else {
+      setTime(initialTime);
+    }
     setIsActive(false);
   };
+
   const percentage = ((initialTime - time) / initialTime) * 100;
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
 
+  const handleEditClick = () => {
+    setEditMode(true);
+  };
+
+  const handleSaveClick = () => {
+    const newTimeInSeconds = parseInt(newTime, 10) * 60;
+    if (!Number.isNaN(newTimeInSeconds)) {
+      setUserEnteredTime(newTimeInSeconds);
+      setTime(newTimeInSeconds);
+      setEditMode(false);
+    } else setEditMode(false);
+  };
+
   return (
     <AppLayout title="Focus">
       <SubHeader title={taskTitle} leftNav={() => {}} rightNav={() => {}} showLeftNav={false} showRightNav={false} />
-      <div className="FocusPage" style={{ color: darkModeStatus ? "white" : "black" }}>
+      <div className="FocusPage">
         <Progress
-          strokeColor={darkModeStatus ? "white" : "var(--primary-background)"}
+          className={`progress-${darkModeStatus ? "dark" : ""}`}
+          size={200}
+          strokeColor={darkModeStatus ? "white" : "var(--header-color)"}
           trailColor="var(--selection-color)"
           type="circle"
           percent={percentage}
           success={{ percent: 0, strokeColor: darkModeStatus ? "white" : "black" }}
           format={() => `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`}
         />
-        <div style={{ display: "flex", gap: "10px" }}>
-          <button className="action-btn" type="button" onClick={toggle}>
-            {isActive ? "Pause" : "Start"}
-          </button>
-          <button className="action-btn" type="button" onClick={reset}>
-            {" "}
-            Reset{" "}
-          </button>
-        </div>
+        {editMode ? (
+          <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <input
+              className="default-input"
+              style={{ textAlign: "center", maxWidth: 30 }}
+              placeholder="min"
+              value={newTime}
+              onChange={(e) => setNewTime(e.target.value)}
+            />
+            <button className="action-btn" type="button" onClick={handleSaveClick}>
+              Save
+            </button>
+          </div>
+        ) : (
+          <div style={{ display: "flex", gap: "10px" }}>
+            <button className="action-btn" type="button" onClick={toggle}>
+              {isActive ? "Pause" : "Start"}
+            </button>
+            <button className="action-btn" type="button" onClick={reset}>
+              Reset
+            </button>
+            <button className="action-btn" type="button" onClick={handleEditClick}>
+              Edit Time
+            </button>
+          </div>
+        )}
       </div>
     </AppLayout>
   );
