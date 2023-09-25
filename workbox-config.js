ab850edc55ccef9abe155741384bccea9ac8d54b
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2023-09-25 12:15:09 +0530

    revert: workbox package versions

--- workbox-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	globDirectory: '.',
-	globPatterns: [
-		'**/*.{ts,json,png,svg,mp3,ico,js,css,wasm,html,webmanifest,tsx,scss,txt,sh}'
-	],
-	swDest: 'sw.js',
-	swSrc: '/home/vinaybadgujar/Github/ZinZen/src/sw.ts'
-};
