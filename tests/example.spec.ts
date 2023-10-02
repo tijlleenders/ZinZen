e90447fa44b228de10cd6cea6a696ebac8c87582
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2023-10-02 15:59:42 +0530

    add: initial test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,71 @@
 import { test, expect } from "@playwright/test";
 
-test("has title", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+test("test", async ({ browser }) => {
+  test.setTimeout(40000);
+  const userOneContext = await browser.newContext({
+    storageState: "./auth.json",
+  });
+  const userTwoContext = await browser.newContext();
+  const userTwoPage = await userTwoContext.newPage();
+  const userOnePage = await userOneContext.newPage();
+  await userOnePage.goto("http://localhost:3000/");
+  await userOnePage.getByRole("button", { name: "My Goals Goals" }).click();
+  await userOnePage.locator(".goal-tile").first().click();
+  await userOnePage
+    .locator("div")
+    .filter({ hasText: /^ActionsEdit$/ })
+    .locator("div")
+    .nth(2)
+    .click();
+  await userOnePage
+    .locator("div")
+    .filter({ hasText: /^Share$/ })
+    .first()
+    .click();
+  await userOnePage.getByRole("button", { name: "share with friend Share privately" }).click();
+  await userOnePage.getByRole("button", { name: "Choose contact" }).click();
+  await userOnePage.getByPlaceholder("Name").click();
+  await userOnePage.getByPlaceholder("Name").fill("User 2");
+  await userOnePage.getByRole("button", { name: "add contact Share invitation" }).click();
+  await userOnePage.waitForTimeout(5000);
+  await userOnePage.goBack();
+  await userOnePage.getByRole("button", { name: "U", exact: true }).click();
+  await userOnePage.waitForSelector(".ant-notification-notice");
+  const link = await userOnePage.evaluate("navigator.clipboard.readText()");
+  await userOnePage.goto("http://localhost:3000/");
+  await userOnePage.getByRole("button", { name: "My Goals Goals" }).click();
+  await userOnePage.locator(".goal-tile").first().click();
+  await userOnePage
+    .locator("div")
+    .filter({ hasText: /^ActionsEdit$/ })
+    .locator("div")
+    .nth(2)
+    .click();
+  await userOnePage
+    .locator("div")
+    .filter({ hasText: /^Share$/ })
+    .first()
+    .click();
+  await userOnePage.getByRole("button", { name: "share with friend Share privately" }).click();
+  await userOnePage.getByRole("button", { name: "Choose contact" }).click();
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  await userTwoPage.waitForTimeout(1000);
+  await userTwoPage.goto(`${link}`);
+  await userTwoPage.getByRole("button", { name: "English" }).click();
+  await userTwoPage.getByRole("button", { name: "Continue zinzen faq" }).click();
+  await userTwoPage.getByPlaceholder("Contact name").click();
+  await userTwoPage.getByPlaceholder("Contact name").fill("User 1");
+  await userTwoPage.getByRole("button", { name: "Add to my contacts" }).click();
+  await userTwoPage.waitForTimeout(2000);
+  await userOnePage.getByRole("button", { name: "U", exact: true }).click();
+  await userOnePage.waitForSelector(".ant-notification-notice");
 
-test("get started link", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-
-  // Click the get started link.
-  await page.getByRole("link", { name: "Get started" }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole("heading", { name: "Installation" })).toBeVisible();
+  await userTwoPage.reload(); // Refresh the window
+  await userTwoPage.getByRole("button", { name: "My Time" }).click(); // Switch to My Time
+  await userTwoPage.getByRole("button", { name: "My Goals" }).click(); // Switch back to My Goals
+  await userTwoPage.reload(); // Refresh the window
+  await userTwoPage.waitForSelector(".header-icon[alt='zinzen inbox']");
+  await userTwoPage.waitForSelector(".header-icon[alt='zinzen inbox']"); // Check if inbox icon is visible
+  await userTwoPage.getByRole("button", { name: "zinzen inbox" }).click(); // Click on inbox
+  await userTwoPage.waitForSelector(".goal-tile");
 });
