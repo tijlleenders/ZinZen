4049af83fa58f74e7d2520c344ebe4a5a4cf039e
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2023-10-03 16:05:00 +0530

    edit: format code

--- playwright/goal-sharing-feature.spec.ts
+++ playwright/goal-sharing-feature.spec.ts
@@ -1,60 +1,62 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { test, expect, Page } from "@playwright/test";
-
 import { shareGoalPrivately } from "./lib/testUtils";
 
+test.describe.configure({ mode: "serial" });
+
 test.describe("Goal Sharing Feature", () => {
-  test("test", async ({ browser }) => {
-    test.setTimeout(40000);
-    const userOneContext = await browser.newContext({
+  test.setTimeout(40000);
+  let userOneContext;
+  let userTwoContext;
+  let userOnePage: Page;
+  let userTwoPage: Page;
+  let link: string;
+
+  test.beforeAll(async ({ browser }) => {
+    userOneContext = await browser.newContext({
       storageState: "playwright/userOnboarding.json",
     });
-    const userTwoContext = await browser.newContext({
+    userTwoContext = await browser.newContext({
       storageState: "playwright/userOnboarding.json",
     });
 
-    const userTwoPage = await userTwoContext.newPage();
-    const userOnePage = await userOneContext.newPage();
+    userTwoPage = await userTwoContext.newPage();
+    userOnePage = await userOneContext.newPage();
+  });
 
+  test("add contact in user 1", async () => {
     await userOnePage.goto("http://localhost:3000/");
     await userOnePage.getByRole("button", { name: "Goals" }).click();
     await shareGoalPrivately(userOnePage);
     await userOnePage.getByPlaceholder("Name").click();
     await userOnePage.getByPlaceholder("Name").fill("User 2");
     await userOnePage.getByRole("button", { name: "add contact Share invitation" }).click();
-    await userOnePage.waitForTimeout(3000);
+    await userOnePage.waitForTimeout(5000);
     await userOnePage.goBack();
     await userOnePage.getByRole("button", { name: "U", exact: true }).click();
     await userOnePage.waitForSelector(".ant-notification-notice");
-    const link = await userOnePage.evaluate("navigator.clipboard.readText()");
+    link = await userOnePage.evaluate("navigator.clipboard.readText()");
     await userOnePage.goto("http://localhost:3000/");
     await userOnePage.getByRole("button", { name: "Goals" }).click();
     await shareGoalPrivately(userOnePage);
+  });
 
+  test("add contact in user 2", async () => {
     await userTwoPage.goto(`${link}`);
     await userTwoPage.getByPlaceholder("Contact name").click();
     await userTwoPage.getByPlaceholder("Contact name").fill("User 1");
     await userTwoPage.getByRole("button", { name: "Add to my contacts" }).click();
+  });
 
+  test("share goal in user 1", async () => {
     await userOnePage.getByRole("button", { name: "U", exact: true }).click();
     await userOnePage.waitForSelector(".ant-notification-notice");
-    await userOnePage.goto("http://localhost:3000/");
-    await userOnePage.getByRole("button", { name: "Goals" }).click();
-    await userOnePage.locator("div:nth-child(2) > .user-goal > .user-goal-main > .goal-tile").click();
-    await userOnePage
-      .locator("div")
-      .filter({ hasText: /^ActionsEdit$/ })
-      .locator("div")
-      .nth(2)
-      .click();
-    await userOnePage
-      .locator("div")
-      .filter({ hasText: /^Share$/ })
-      .first()
-      .click();
-    await userOnePage.getByRole("button", { name: "Share privately" }).click();
-    await userOnePage.getByRole("button", { name: "Choose contact" }).click();
-    await userOnePage.locator("#modal-contact-list").getByRole("button", { name: "U" }).click();
+    await userOnePage.waitForTimeout(1000);
+    await userOnePage.getByRole("button", { name: "U", exact: true }).click();
     await userOnePage.waitForSelector(".ant-notification-notice");
+  });
+
+  test("check inbox in user 2", async () => {
     await userTwoPage.getByRole("button", { name: "Schedule" }).click();
     await userTwoPage.reload(); // Refresh the window
     await userTwoPage.waitForTimeout(3000);
