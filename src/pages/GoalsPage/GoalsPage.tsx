73a31f6d8d61eb4d5ca827b2ae4fd2c1376cfa6b
Tushar Inani <tusharinani93@gmail.com>
2023-10-07 01:55:05 +0530

    integrated: partner's mode UI

--- src/pages/GoalsPage/GoalsPage.tsx
+++ src/pages/GoalsPage/GoalsPage.tsx
@@ -1,9 +1,15 @@
-import React, { useEffect } from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilValue } from "recoil";
+import React, { useEffect, useState } from "react";
 
-import { getMyPartner } from "@src/api/PartnerAPI";
-import { displayShareModal } from "@src/store/GoalsState";
-import { displayPartner, partnerDetails } from "@src/store";
+import { PartnerItem } from "@src/models/PartnerItem";
+import { getAllContacts } from "@src/api/ContactsAPI";
+import { getPartnerByRelId } from "@src/api/PartnerAPI";
+import { displayPartnerMode } from "@src/store";
+import { displayParticipants } from "@src/store/GoalsState";
+
+import ContactItem from "@src/models/ContactItem";
+import Participants from "@components/GoalsComponents/Participants";
+import ParticipantsNavbar from "@components/ParticipantsNavbar";
 import GoalLocStateHandler from "@src/helpers/GoalLocStateHandler";
 
 import { MyGoals } from "./MyGoals";
@@ -11,24 +17,42 @@ import PartnerGoals from "./PartnerGoals";
 import "./GoalsPage.scss";
 
 const GoalsPage = () => {
-  const [partner, setPartner] = useRecoilState(partnerDetails);
-  const showPartner = useRecoilValue(displayPartner);
-  const showShareModal = useRecoilValue(displayShareModal);
+  const showPartnerMode = useRecoilValue(displayPartnerMode);
+  const showParticipants = useRecoilValue(displayParticipants);
 
-  const getPartnerGoals = async () => {
-    const myPartner = await getMyPartner();
-    if (myPartner) {
-      setPartner(JSON.parse(JSON.stringify(myPartner)));
+  const [activePartner, setActivePartner] = useState<string>("");
+  const [partner, setPartner] = useState<PartnerItem | null>(null);
+  const [partnersList, setPartnersList] = useState<ContactItem[]>([]);
+
+  const getPartnerDetails = async () => {
+    let activeRelId;
+    if (partnersList.length > 0) {
+      activeRelId = partnersList[0].relId;
+    } else {
+      const list = await getAllContacts();
+      activeRelId = list[0].relId;
+      setPartnersList([...list]);
     }
+    setActivePartner(activeRelId);
+    setPartner(await getPartnerByRelId(activeRelId));
   };
+
   useEffect(() => {
-    getPartnerGoals();
-  }, [showPartner, showShareModal]);
+    getPartnerDetails();
+  }, [showPartnerMode, activePartner]);
 
   return (
     <>
       <GoalLocStateHandler />
-      {showPartner && partner ? <PartnerGoals partner={partner} refreshGoals={getPartnerGoals} /> : <MyGoals />}
+      {showParticipants && <Participants list={showParticipants} />}
+      {showPartnerMode && partner ? (
+        <>
+          <PartnerGoals partner={partner} refreshGoals={getPartnerDetails} />
+          <ParticipantsNavbar list={partnersList} activePartner={activePartner} setActivePartner={setActivePartner} />
+        </>
+      ) : (
+        <MyGoals />
+      )}
     </>
   );
 };
