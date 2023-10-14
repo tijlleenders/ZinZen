37c021b8c1fbdfec3c7411ff9c62324521fd448d
Tushar Inani <tusharinani93@gmail.com>
2023-10-14 18:16:52 +0530

    restored: accept and ignore btn of display changes

--- src/components/GoalsComponents/DisplayChangesModal/AcceptBtn.tsx
+++ src/components/GoalsComponents/DisplayChangesModal/AcceptBtn.tsx
@@ -10,68 +10,49 @@ import { GoalItem } from "@src/models/GoalItem";
 import { getTypeAtPriority } from "@src/helpers/GoalProcessor";
 import { deleteGoalChangesInID, getInboxItem } from "@src/api/InboxAPI";
 import { IDisplayChangesModal } from "@src/Interfaces/IDisplayChangesModal";
-import { changeNewUpdatesStatus, convertSharedGoalToColab } from "@src/api/GoalsAPI";
+import { typeOfChange } from "@src/models/InboxItem";
 
 interface AcceptBtnProps {
   goal: GoalItem;
   acceptChanges: () => Promise<void>;
-  showChangesModal: IDisplayChangesModal;
-  setShowChangesModal: React.Dispatch<React.SetStateAction<IDisplayChangesModal | null>>;
+  typeAtPriority: typeOfChange | "none";
 }
 
-const AcceptBtn = ({ showChangesModal, goal, acceptChanges, setShowChangesModal }: AcceptBtnProps) => {
-  const { typeAtPriority } = showChangesModal;
-  const isConversionRequest = typeAtPriority === "conversionRequest";
+const AcceptBtn = ({ typeAtPriority, goal, acceptChanges }: AcceptBtnProps) => {
   const darkModeStatus = useRecoilValue(darkModeState);
-  const setLastAction = useSetRecoilState(lastAction);
 
   const handleClick = async () => {
-    if (isConversionRequest) {
-      // await convertTypeOfSub(goal.rootGoalId, goal.shared.contacts[0].relId, "collaboration");
-      await convertSharedGoalToColab(goal.id);
-      setShowChangesModal(null);
-    } else {
-      await acceptChanges();
-      const removeChanges =
-        typeAtPriority === "subgoals" ? showChangesModal.goals.map((ele) => ele.id) : [showChangesModal.goals[0].id];
-      if (typeAtPriority !== "none") {
-        await deleteGoalChangesInID(goal.rootGoalId, typeAtPriority, removeChanges);
-      }
-    }
+    await acceptChanges();
+
     const inbox = await getInboxItem(goal.rootGoalId);
-    if (getTypeAtPriority(inbox.goalChanges).typeAtPriority === "none") {
-      changeNewUpdatesStatus(false, goal.rootGoalId)
-        .then(() => {
-          setLastAction("goalUpdates");
-        })
-        .catch((err) => console.log(err));
-    }
-    setShowChangesModal(null);
+    // if (getTypeAtPriority(inbox.goalChanges).typeAtPriority === "none") {
+    // changeNewUpdatesStatus(false, goal.rootGoalId)
+    //   .then(() => {
+    //     setLastAction("goalUpdates");
+    //   })
+    //   .catch((err) => console.log(err));
+    // }
   };
   return (
     <button
       type="button"
-      style={{ width: "100%", padding: "8px 15px", justifyContent: "flex-start" }}
+      style={{ padding: "8px 15px", justifyContent: "flex-start" }}
       className={`default-btn${darkModeStatus ? "-dark" : ""}`}
-      onClick={handleClick}
+      onClick={async () => {
+        await acceptChanges();
+      }}
     >
       <img
         alt="add changes"
-        src={isConversionRequest ? collaborateSvg : typeAtPriority === "deleted" ? deleteIcon : plus}
+        src={typeAtPriority === "deleted" ? deleteIcon : plus}
         width={25}
         style={!darkModeStatus ? { filter: "brightness(0)" } : {}}
       />
       &nbsp;
-      {isConversionRequest ? (
-        `Collaborate with ${goal.shared.contacts[0].name}`
-      ) : (
-        <>
-          {typeAtPriority === "archived" && "Complete for me too"}
-          {typeAtPriority === "deleted" && "Delete for me too"}
-          {typeAtPriority === "subgoals" && "Add all checked"}
-          {typeAtPriority === "modifiedGoals" && "Make all checked changes"}
-        </>
-      )}
+      {typeAtPriority === "archived" && "Complete for me too"}
+      {typeAtPriority === "deleted" && "Delete for me too"}
+      {typeAtPriority === "subgoals" && "Add all checked"}
+      {typeAtPriority === "modifiedGoals" && "Make all checked changes"}
     </button>
   );
 };
--- src/components/GoalsComponents/DisplayChangesModal/IgnoreBtn.tsx
+++ src/components/GoalsComponents/DisplayChangesModal/IgnoreBtn.tsx
@@ -4,54 +4,25 @@ import { useRecoilValue } from "recoil";
 import ignore from "@assets/images/ignore.svg";
 
 import { darkModeState } from "@src/store";
-import { GoalItem } from "@src/models/GoalItem";
-import { getTypeAtPriority } from "@src/helpers/GoalProcessor";
-import { deleteGoalChangesInID, getInboxItem, removeGoalInbox } from "@src/api/InboxAPI";
-import { IDisplayChangesModal } from "@src/Interfaces/IDisplayChangesModal";
-import { changeNewUpdatesStatus, convertSharedGoalToColab } from "@src/api/GoalsAPI";
 
 interface IgnoreBtnProps {
-  goal: GoalItem;
-  showChangesModal: IDisplayChangesModal;
-  setShowChangesModal: React.Dispatch<React.SetStateAction<IDisplayChangesModal | null>>;
+  deleteChanges: () => Promise<void>;
 }
-const IgnoreBtn = ({ showChangesModal, goal, setShowChangesModal }: IgnoreBtnProps) => {
-  const { typeAtPriority } = showChangesModal;
-  const isConversionRequest = typeAtPriority === "conversionRequest";
+const IgnoreBtn = ({ deleteChanges }: IgnoreBtnProps) => {
   const darkModeStatus = useRecoilValue(darkModeState);
-
-  const handleClick = async () => {
-    let deleteInbox = false;
-    if (isConversionRequest) {
-      deleteInbox = true;
-      convertSharedGoalToColab(goal.id, false);
-    } else {
-      const removeChanges = showChangesModal.goals.map((colabGoal: GoalItem) => colabGoal.id);
-      if (typeAtPriority !== "none") {
-        await deleteGoalChangesInID(goal.rootGoalId, typeAtPriority, removeChanges);
-      }
-    }
-    const inbox = await getInboxItem(goal.rootGoalId);
-    if (deleteInbox || getTypeAtPriority(inbox.goalChanges).typeAtPriority === "none") {
-      await changeNewUpdatesStatus(false, goal.rootGoalId).catch((err) => console.log(err));
-      removeGoalInbox(goal.rootGoalId);
-    }
-    setShowChangesModal(null);
-  };
   return (
     <button
       type="button"
       style={{
         padding: "8px 15px",
-        backgroundColor: "rgba(115, 115, 115, 0.6)",
-        width: "100%",
+        background: "var(--secondary-background)",
         justifyContent: "flex-start",
       }}
       className={`default-btn${darkModeStatus ? "-dark" : ""}`}
-      onClick={handleClick}
+      onClick={deleteChanges}
     >
       <img alt="add changes" src={ignore} width={25} />
-      &nbsp;{isConversionRequest ? "Keep separate" : "Ignore all"}
+      &nbsp;Ignore all
     </button>
   );
 };
