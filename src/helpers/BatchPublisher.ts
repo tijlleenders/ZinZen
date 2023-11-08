ef276702c1ebfa3b2ff9fc35672656485d8ee556
Tushar Inani <tusharinani93@gmail.com>
2023-11-08 19:48:04 +0000

    creatd: helper to send goals in a batch

---
+++ src/helpers/BatchPublisher.ts
@@ -0,0 +1,29 @@
+import { getParticipantsOfGoals } from "@src/api/GoalsAPI";
+import { GoalItem } from "@src/models/GoalItem";
+import { sendUpdatesToSubscriber } from "@src/services/contact.service";
+import { getHistoryUptoGoal } from "./GoalProcessor";
+
+export const sendNewGoals = async (
+  newGoals: GoalItem[],
+  ancestors: string[] = [],
+  redefineAncestors = true,
+  excludeSubs: string[] = [],
+) => {
+  const ancestorGoalIds = redefineAncestors
+    ? (await getHistoryUptoGoal(newGoals[0].id)).map((ele) => ele.goalID)
+    : ancestors;
+  const subscribers = await getParticipantsOfGoals(ancestorGoalIds);
+  subscribers
+    .filter((ele) => !excludeSubs.includes(ele.sub.relId))
+    .forEach(async ({ sub, rootGoalId }) => {
+      sendUpdatesToSubscriber(
+        sub,
+        rootGoalId,
+        "subgoals",
+        newGoals.map((goal) => {
+          const { participants, ...changes } = goal;
+          return { level: ancestorGoalIds.length, goal: { ...changes, rootGoalId } };
+        }),
+      ).then(() => console.log("update sent"));
+    });
+};
