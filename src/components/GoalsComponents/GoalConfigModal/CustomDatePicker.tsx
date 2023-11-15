89f1a62b174af1df9322a8757ccb6b51a10ba1b3
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2023-11-15 11:09:23 +0530

    edit: optionally hide time fields

--- src/components/GoalsComponents/GoalConfigModal/CustomDatePicker.tsx
+++ src/components/GoalsComponents/GoalConfigModal/CustomDatePicker.tsx
@@ -1,5 +1,5 @@
-import { Select } from "antd";
 import React from "react";
+import { Select } from "antd";
 import { useTranslation } from "react-i18next";
 
 interface ICustomDatePicker {
@@ -8,15 +8,19 @@ interface ICustomDatePicker {
   timeValue: number;
   handleDateChange: (value: string) => void;
   handleTimeChange: (value: number) => void;
+  showTime: boolean;
 }
+
 const CustomDatePicker: React.FC<ICustomDatePicker> = ({
   label,
   dateValue,
   timeValue,
   handleDateChange,
   handleTimeChange,
+  showTime = true,
 }) => {
   const { t } = useTranslation();
+
   return (
     <div
       style={{
@@ -36,25 +40,22 @@ const CustomDatePicker: React.FC<ICustomDatePicker> = ({
       >
         {label}
       </span>
-      <input
-        type="date"
-        value={dateValue}
-        onChange={(e) => {
-          handleDateChange(e.target.value);
-        }}
-        className="datepicker"
-      />
-      <span>{t("at")}</span>
-      <Select
-        className="timepicker"
-        value={timeValue || 0}
-        placeholder="Select Time"
-        onChange={handleTimeChange}
-        options={[...Array(24).keys()].map((hr) => ({
-          value: hr,
-          label: `${hr > 9 ? "" : "0"}${hr}:00`,
-        }))}
-      />
+      <input type="date" value={dateValue} onChange={(e) => handleDateChange(e.target.value)} className="datepicker" />
+      {showTime && (
+        <>
+          <span>{t("at")}</span>
+          <Select
+            className="timepicker"
+            value={timeValue || 0}
+            placeholder="Select Time"
+            onChange={handleTimeChange}
+            options={[...Array(24).keys()].map((hr) => ({
+              value: hr,
+              label: `${hr > 9 ? "" : "0"}${hr}:00`,
+            }))}
+          />
+        </>
+      )}
     </div>
   );
 };
