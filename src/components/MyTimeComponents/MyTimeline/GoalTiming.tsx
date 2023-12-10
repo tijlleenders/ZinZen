f17717094a2e843de0f3709d08b38eaff61c9d15
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2023-12-10 14:34:51 +0530

    refactor: simplify GoalTiming's renderTime function

--- src/components/MyTimeComponents/MyTimeline/GoalTiming.tsx
+++ src/components/MyTimeComponents/MyTimeline/GoalTiming.tsx
@@ -6,19 +6,18 @@ interface GoalTimingProps {
 }
 
 export const GoalTiming: React.FC<GoalTimingProps> = ({ startTime, endTime }) => {
-  const renderTime = (time: string | null) =>
-    time ? (
-      <>
-        <span>{parseInt(time, 10)}</span>
-        <sup>00</sup>
-      </>
-    ) : null;
+  const renderTime = (time: string) => (
+    <>
+      <span>{parseInt(time, 10)}</span>
+      <sup>00</sup>
+    </>
+  );
 
   return (
     <div className="MTL-goalTiming">
-      {renderTime(startTime)}
+      {startTime && renderTime(startTime)}
       {startTime && endTime && <span> - </span>}
-      {renderTime(endTime)}
+      {endTime && renderTime(endTime)}
     </div>
   );
 };
