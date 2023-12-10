7f3d854e277a2024c9a92a76269da170a90f588c
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2023-12-10 15:25:54 +0530

    refactor: move goalsSummary component to seperate file

--- src/components/GoalsComponents/MyGoal/MyGoal.tsx
+++ src/components/GoalsComponents/MyGoal/MyGoal.tsx
@@ -15,7 +15,8 @@ import { displayGoalId, displayUpdateGoal, goalsHistory, displayChangesModal } f
 import NotificationSymbol from "@src/common/NotificationSymbol";
 
 import { jumpToLowestChanges, getHistoryUptoGoal } from "@src/helpers/GoalProcessor";
-import GoalAvatar from "./GoalAvatar";
+import GoalAvatar from "../GoalAvatar";
+import GoalSummary from "./GoalSummary";
 
 interface MyGoalProps {
   goal: GoalItem;
@@ -31,63 +32,6 @@ interface MyGoalProps {
     }>
   >;
 }
-
-const GoalSent = ({ goal }: { goal: GoalItem }) => {
-  const { t } = useTranslation();
-  const onLength = goal.on.length;
-  const onWeekdays = onLength === 5 && !goal.on.includes("Sat") && !goal.on.includes("Sun");
-  const onWeekends = onLength === 2 && goal.on.includes("Sat") && goal.on.includes("Sun");
-
-  const hasStarted = !!goal.start && new Date(goal.start).getTime() < new Date().getTime();
-  const showStart = !!goal.due || !hasStarted;
-
-  const hoursPerDayText = formatBudgetHrsToText(goal.timeBudget.perDay);
-  const hoursPerWeekText = formatBudgetHrsToText(goal.timeBudget.perWeek);
-  const dueDateText = goal.due ? calculateDaysLeft(goal.due) : null;
-
-  return (
-    <>
-      <div>
-        {goal.timeBudget.perDay === null &&
-          (goal.duration !== null ? (
-            <span>
-              {goal.duration} {t(`hour${Number(goal.duration) > 1 ? "s" : ""}`)}
-            </span>
-          ) : !goal.due ? (
-            <span>{t("noDuration")}</span>
-          ) : null)}
-        {goal.timeBudget.perDay && <span>{hoursPerDayText}</span>}
-        {goal.timeBudget.perDay && (
-          <span>
-            {onLength > 0 &&
-              !onWeekdays &&
-              !onWeekends &&
-              (onLength === 7 ? ` ${t("daily")}` : ` ${t("on")} ${goal.on.map((ele) => t(ele)).join(" ")}`)}
-            {onWeekdays && ` ${t("on")} ${t("weekdays")}`}
-            {onWeekends && " on weekends"}
-          </span>
-        )}
-      </div>
-      <div>
-        {goal.beforeTime && goal.afterTime
-          ? `${t("between")} ${goal.afterTime}-${goal.beforeTime}`
-          : goal.beforeTime
-          ? `${t("before")} ${goal.beforeTime}`
-          : goal.afterTime
-          ? `${t("after")} ${goal.afterTime}`
-          : ""}
-      </div>
-      <div>{goal.timeBudget.perWeek && <span>{`${hoursPerWeekText} per week`}</span>}</div>
-      {showStart && !!goal.start && (
-        <div>
-          {hasStarted ? t("started") : t("starts")} {new Date(goal.start).toDateString().slice(4)}
-        </div>
-      )}
-      <div>{goal.due && <div>{dueDateText}</div>}</div>
-      <div>{goal.habit === "weekly" && `${t("every")} week`}</div>
-    </>
-  );
-};
 
 const MyGoal: React.FC<MyGoalProps> = ({ goal, showActions, setShowActions }) => {
   const archived = goal.archived === "true";
@@ -250,7 +194,7 @@ const MyGoal: React.FC<MyGoalProps> = ({ goal, showActions, setShowActions }) =>
           </div>
           {showActions.open === goal.id && showActions.click > 0 && (
             <p className="goal-desc">
-              <GoalSent goal={goal} />
+              <GoalSummary goal={goal} />
             </p>
           )}
         </div>
