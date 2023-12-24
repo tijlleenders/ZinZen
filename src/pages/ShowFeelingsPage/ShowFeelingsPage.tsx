c18a2eda987f68b55cae1a62a684ddbf77578b2d
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2023-12-24 17:10:09 +0530

    add: 'yesterday' title to subheader for previous day's feeling

--- src/pages/ShowFeelingsPage/ShowFeelingsPage.tsx
+++ src/pages/ShowFeelingsPage/ShowFeelingsPage.tsx
@@ -18,6 +18,29 @@ export const ShowFeelingsPage = () => {
   const isToday = (date: string) => {
     return new Date(date).toDateString() === new Date().toDateString();
   };
+
+  const isYesterday = (date: string) => {
+    return new Date(date).toDateString() === new Date(new Date().setDate(new Date().getDate() - 1)).toDateString();
+  };
+
+  const formatSubheaderDate = (date: string) => {
+    return new Date(date).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    });
+  };
+
+  const getTitleForDate = (date: string) => {
+    if (isToday(date)) {
+      return "Today";
+    }
+    if (isYesterday(date)) {
+      return "Yesterday";
+    }
+    return formatSubheaderDate(date);
+  };
+
   return (
     <AppLayout title="myJournal">
       <div>
@@ -25,7 +48,7 @@ export const ShowFeelingsPage = () => {
           <div key={date}>
             {feelingsList[date] && (
               <SubHeader
-                title={isToday(date) ? "Today" : new Date(date).toDateString()}
+                title={getTitleForDate(date)}
                 leftNav={() => {
                   return null;
                 }}
