178350550367a0974daddcc61744989c14f537fb
Tijl Leenders <44887755+tijlleenders@users.noreply.github.com>
2024-01-23 15:01:02 +0100

    Revert "only add translation to word part in formatSingularPlural function"

--- src/components/GoalsComponents/MyGoal/GoalSummary/GoalDurationSummary.tsx
+++ src/components/GoalsComponents/MyGoal/GoalSummary/GoalDurationSummary.tsx
@@ -1,10 +1,13 @@
-import React from "react";
-import { formatSingularPlural } from "@src/utils";
 import { GoalItem } from "@src/models/GoalItem";
+import { formatSingularPlural } from "@src/utils";
+import React from "react";
+import { useTranslation } from "react-i18next";
 
 const GoalDurationSummary = ({ goal }: { goal: GoalItem }) => {
+  const { t } = useTranslation();
+
   if (goal.duration) {
-    return <span>{formatSingularPlural(Number(goal.duration), "hour")}</span>;
+    return <span>{formatSingularPlural(Number(goal.duration), t("hour"))}</span>;
   }
   return null;
 };
