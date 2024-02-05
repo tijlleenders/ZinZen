466ec30753f7e40cfdbd1a894aba66945877453d
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-02-05 18:28:52 +0530

    edit: mark parent goal as impossible if contains children goal as impossible

--- src/hooks/useIsGoalImpossible.tsx
+++ src/hooks/useIsGoalImpossible.tsx
@@ -1,20 +1,31 @@
+import { getChildrenGoals } from "@src/api/GoalsAPI";
 import { getImpossibleGoalById } from "@src/api/ImpossibleGoalsApi";
 import { useEffect, useState } from "react";
 
 const useIsGoalImpossible = ({ id }: { id: string }) => {
   const [isImpossible, setIsImpossible] = useState(false);
 
-  useEffect(() => {
-    const isGoalImpossible = async (goalId: string) => {
-      const res = await getImpossibleGoalById(goalId);
-      if (res) {
-        setIsImpossible(true);
-      } else {
-        setIsImpossible(false);
-      }
-    };
+  const isGoalOrChildrenImpossible = async (currentGoalId: string): Promise<boolean> => {
+    const goalIsImpossible = await getImpossibleGoalById(currentGoalId);
 
-    isGoalImpossible(id);
+    if (goalIsImpossible) {
+      setIsImpossible(true);
+      return true;
+    }
+
+    const children = await getChildrenGoals(currentGoalId);
+    const childResults = await Promise.all(children.map((child) => isGoalOrChildrenImpossible(child.id)));
+
+    if (childResults.some((result) => result)) {
+      setIsImpossible(true);
+      return true;
+    }
+
+    return false;
+  };
+
+  useEffect(() => {
+    isGoalOrChildrenImpossible(id);
   }, [id]);
 
   return isImpossible;
