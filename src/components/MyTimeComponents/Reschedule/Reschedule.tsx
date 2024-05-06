58526d86745fabe3b15e6e22c3d859b664548b3a
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-05-06 09:02:32 +0530

    refactoring and move reschedule options to constant folder

--- src/components/MyTimeComponents/Reschedule/Reschedule.tsx
+++ src/components/MyTimeComponents/Reschedule/Reschedule.tsx
@@ -1,7 +1,6 @@
 import { Radio } from "antd";
 import { useRecoilState, useRecoilValue } from "recoil";
 import React, { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
 
 import { darkModeState } from "@src/store";
 import "./Reschedule.scss";
@@ -9,6 +8,7 @@ import ZModal from "@src/common/ZModal";
 import { addBlockedSlot } from "@src/api/TasksAPI";
 import { displayReschedule } from "@src/store/TaskState";
 import moment from "moment";
+import { RESCHEDULE_OPTIONS } from "@src/constants/rescheduleOptions";
 
 const Reschedule = () => {
   const [task, setOpen] = useRecoilState(displayReschedule);
@@ -18,14 +18,6 @@ const Reschedule = () => {
   if (!task) {
     return null;
   }
-
-  const rescheduleOptions = [
-    { label: "1 hour", value: 1 },
-    { label: "3 hours", value: 3 },
-    { label: "1 day", value: 24 },
-    { label: "3 days", value: 72 },
-    { label: "7 days", value: 168 },
-  ];
 
   const handleReschedule = (value: number) => {
     setSelectedOption(value);
@@ -45,26 +37,28 @@ const Reschedule = () => {
   };
 
   return (
-    <ZModal type="rescheduleModal" open={!!task} onCancel={() => setOpen(null)}>
+    <ZModal
+      type={`reschedule-modal ${darkModeStatus ? "dark" : "light"}`}
+      open={!!task.title}
+      onCancel={() => setOpen(null)}
+    >
       <div className="header-title">
         <h4>{task.title}</h4>
       </div>
-      <div
-        className={`reschedule-options ${darkModeStatus ? "dark" : "light"}`}
-        style={{ display: "flex", flexWrap: "wrap" }}
-      >
+      <div className="reschedule-options">
         <Radio.Group
           size="large"
           buttonStyle="solid"
           defaultValue={selectedOption}
           onChange={(e) => setSelectedOption(e.target.value)}
         >
-          {rescheduleOptions.map((option) => (
+          {RESCHEDULE_OPTIONS.map((option) => (
             <Radio.Button
+              className={`radio-button ${darkModeStatus ? "dark" : "light"}`}
               style={{
                 backgroundColor: darkModeStatus ? "rgba(255, 255, 255, 0.08)" : "var(--secondary-background)",
               }}
-              key={uuidv4()}
+              key={option.label}
               value={option.value}
             >
               {option.label}
---
+++ src/constants/rescheduleOptions.ts
@@ -0,0 +1,7 @@
+export const RESCHEDULE_OPTIONS: { label: string; value: number }[] = [
+  { label: "1 hour", value: 1 },
+  { label: "3 hours", value: 3 },
+  { label: "1 day", value: 24 },
+  { label: "3 days", value: 72 },
+  { label: "7 days", value: 168 },
+];
