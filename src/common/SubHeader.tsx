c228ea4da8a78f70e1585153d827c51675cf51da
Tushar Inani <tusharinani93@gmail.com>
2024-06-24 11:27:33 +0000

    fix: subheader alignment

--- src/common/SubHeader.tsx
+++ src/common/SubHeader.tsx
@@ -6,25 +6,28 @@ import chevronLeftIcon from "@assets/images/chevronLeft.svg";
 
 import { ISubHeaderProps } from "@src/Interfaces/ICommon";
 
-const NavBtn = ({ className, handleClick }: { className: string; handleClick?: () => void }) => (
+const NavBtn = ({ show, className, handleClick }: { show?: boolean; className: string; handleClick?: () => void }) => (
   <Col span={6} style={{ textAlign: "center", alignSelf: "center" }}>
-    <button type="button" className="ordinary-element" onClick={handleClick}>
-      <img src={chevronLeftIcon} className={className} />
-    </button>
+    {show && (
+      <button type="button" className="ordinary-element" onClick={handleClick}>
+        <img src={chevronLeftIcon} className={className} />
+      </button>
+    )}
   </Col>
 );
 
 const SubHeader: React.FC<ISubHeaderProps> = ({ leftNav, rightNav, title, showLeftNav, showRightNav }) => (
   <Row className="subheader">
-    {showLeftNav && <NavBtn className="chevronLeft theme-icon" handleClick={leftNav} />}
+    <NavBtn show={showLeftNav} className="chevronLeft theme-icon" handleClick={leftNav} />
     <Col span={12}>
       <p style={{ padding: "15px 0", margin: "0px 15px" }} className="subheader-title">
         <span role="button" tabIndex={0} style={{ cursor: "pointer" }}>
+          {" "}
           {title}
         </span>
       </p>
     </Col>
-    {showRightNav && <NavBtn className="chevronRight theme-icon" handleClick={rightNav} />}
+    <NavBtn show={showRightNav} className="chevronRight theme-icon" handleClick={rightNav} />
   </Row>
 );
 
