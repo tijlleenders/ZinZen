81d92cc4cda2b357aa128b9ef1f342e6ce8b5e82
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-06-26 20:19:02 +0530

    test: migrate landing-page test from cypress to playwright

---
+++ playwright/tests/landing-page.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect, Page } from "@playwright/test";
+
+test.describe("Onboarding", () => {
+  let page: Page;
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+  });
+
+  test("should select a language and navigate to the FAQ page", async () => {
+    await page.goto("http://127.0.0.1:3000/");
+    await page.getByRole("button", { name: "English" }).click();
+    await expect(page).toHaveURL("http://127.0.0.1:3000/ZinZenFAQ");
+  });
+
+  test("should navigate to the homepage after clicking continue button on the FAQ page", async () => {
+    await page.getByRole("button", { name: "Continue" }).click();
+    await expect(page).toHaveURL("http://127.0.0.1:3000/");
+  });
+});
