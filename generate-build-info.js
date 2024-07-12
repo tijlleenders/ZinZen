09fafdc410022e33f55b9d5144ec752fef76163a
Nicholas Kipkoech <nickey968@gmail.com>
2024-07-12 15:37:24 +0300

    function cleanup and converting hexHash to decimal hash

--- generate-build-info.js
+++ generate-build-info.js
@@ -9,15 +9,15 @@ function getHash(filePath) {
   const fileBuffer = fs.readFileSync(filePath);
   const hashSum = crypto.createHash("sha256");
   hashSum.update(fileBuffer);
-  return hashSum.digest("hex");
+  const hexHash = hashSum.digest("hex");
+  const decimalHash = BigInt("0x" + hexHash).toString(); //convert hex to decimal
+  return decimalHash;
 }
 
 const buildInfo = {
   version: packageJson.version,
   releaseDate: new Date().toISOString(),
-  hashes: {
-    schedulerWasm: getHash(path.resolve(__dirname, "pkg/scheduler_bg.wasm")),
-  },
+  buildHash: getHash(path.resolve(__dirname, "pkg/scheduler_bg.wasm")),
 };
 
 fs.writeFileSync(path.resolve(__dirname, "dist", "build-info.json"), JSON.stringify(buildInfo, null, 2));
