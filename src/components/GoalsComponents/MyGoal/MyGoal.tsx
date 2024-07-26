86476e8c3b53c30197c1e9edd547c5c8fdc8e364
Nicholas Kipkoech <nickey968@gmail.com>
2024-07-26 10:48:32 +0300

    add: check if goal is code then copy

--- src/components/GoalsComponents/MyGoal/MyGoal.tsx
+++ src/components/GoalsComponents/MyGoal/MyGoal.tsx
@@ -12,7 +12,8 @@ import { useParentGoalContext } from "@src/contexts/parentGoal-context";
 import GoalAvatar from "../GoalAvatar";
 import GoalTitle from "./components/GoalTitle";
 import GoalDropdown from "./components/GoalDropdown";
-import { extractLinks } from "@src/utils/patterns";
+import { extractLinks, isGoalCode } from "@src/utils/patterns";
+import useGoalActions from "@src/hooks/useGoalActions";
 
 interface MyGoalProps {
   goal: ImpossibleGoal;
@@ -23,6 +24,9 @@ interface MyGoalProps {
 const MyGoal: React.FC<MyGoalProps> = ({ goal, dragAttributes, dragListeners }) => {
   const [expandGoalId, setExpandGoalId] = useState("root");
   const [isAnimating, setIsAnimating] = useState(true);
+  const { copyCode } = useGoalActions();
+  const { title } = goal;
+  const isCode = isGoalCode(title);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -50,11 +54,17 @@ const MyGoal: React.FC<MyGoalProps> = ({ goal, dragAttributes, dragListeners }) 
     }
   };
 
-  const handleGoalClick = () => {
-    const url = extractLinks(goal.title);
+  const handleGoalClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.stopPropagation();
+
+    const url = extractLinks(title);
     if (url) {
       const finalUrl = url.startsWith("http://") || url.startsWith("https://") ? url : "https://" + url;
       window.open(finalUrl, "_blank");
+    }
+    if (isCode) {
+      copyCode(title);
+      return;
     }
     const newState: ILocationState = {
       ...location.state,
@@ -105,7 +115,7 @@ const MyGoal: React.FC<MyGoalProps> = ({ goal, dragAttributes, dragListeners }) 
         <div style={{ touchAction: "none" }} onClickCapture={handleDropDown} {...dragAttributes} {...dragListeners}>
           <GoalDropdown goal={goal} />
         </div>
-        <div aria-hidden className="goal-tile" onClick={handleGoalClick}>
+        <div aria-hidden className="goal-tile" onClick={(e) => handleGoalClick(e)}>
           <GoalTitle goal={goal} isImpossible={goal.impossible} />
         </div>
       </div>
