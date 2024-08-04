8e5eb0ea15c835fc587ebb49272bb6eb005db4f6
Tushar Inani <tusharinani93@gmail.com>
2024-08-04 12:28:25 +0000

    cleaning: participants component

--- src/components/GoalsComponents/Participants.tsx
+++ src/components/GoalsComponents/Participants.tsx
@@ -1,44 +1,36 @@
 import React from "react";
-import { followContactOnGoal } from "@src/api/GoalsAPI";
-import { IParticipant } from "@src/models/GoalItem";
 import { Switch } from "antd";
 import CrossIcon from "@assets/images/deleteIcon.svg";
 import TickIcon from "@assets/images/correct.svg";
+
 import ZModal from "@src/common/ZModal";
+import { IParticipant } from "@src/models/GoalItem";
+import { followContactOnGoal } from "@src/api/GoalsAPI";
 import { useActiveGoalContext } from "@src/contexts/activeGoal-context";
 
-const Participants = ({ goalId }: { goalId: string }) => {
+const Participants = () => {
   const { goal } = useActiveGoalContext();
 
-  const handleFollow = async (following: boolean, participant: IParticipant) => {
-    await followContactOnGoal(goalId, { ...participant, following });
-  };
-
-  const toggleFollowStatus = async (participant: IParticipant, value: boolean) => {
-    await handleFollow(value, participant);
+  const toggleFollowStatus = async (participant: IParticipant, following: boolean) => {
+    if (goal) await followContactOnGoal(goal.id, { ...participant, following });
   };
 
   return (
     <ZModal type="configModal" open onCancel={() => window.history.back()}>
-      <div style={{ textAlign: "left", padding: 20, fontSize: 16, fontWeight: 600 }} className="header-title">
+      <div style={{ textAlign: "left", padding: 20 }} className="header-title fw-600">
         Following
       </div>
       <div
+        className="d-flex f-col gap-20"
         style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: 20,
           marginTop: 24,
           padding: "0 20px",
         }}
       >
         {goal?.participants.map((participant) => (
-          <div
-            style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}
-            key={`${participant.relId}-${participant.name}`}
-          >
+          <div className="place-middle justify-sb" key={`${participant.relId}-${participant.name}`}>
             <p style={{ fontSize: 16 }}>{participant.name}</p>
-            <div style={{ display: "flex", alignItems: "center" }}>
+            <div className="place-middle justify-fs">
               <Switch
                 defaultChecked={participant.following}
                 onChange={(value) => toggleFollowStatus(participant, value)}
