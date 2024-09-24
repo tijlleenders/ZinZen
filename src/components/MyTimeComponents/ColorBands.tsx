14baa29ea8e34c0a2046e270c1062db396281d25
Vinay Badgujar <vinaybadgujar8@gmail.com>
2024-09-24 18:05:51 +0530

    remove: unnecessary business logic from colorbands component

--- src/components/MyTimeComponents/ColorBands.tsx
+++ src/components/MyTimeComponents/ColorBands.tsx
@@ -1,42 +1,14 @@
-import { ITaskProgress } from "@src/Interfaces/IPages";
 import { ITaskOfDay } from "@src/Interfaces/Task";
-import { TaskItem } from "@src/models/TaskItem";
 import React from "react";
 import { v4 as uuidv4 } from "uuid";
 
 interface IColorBands {
-  tasksStatus: {
-    [goalId: string]: TaskItem;
-  };
-  active: boolean;
   list: ITaskOfDay;
 }
 
-const ColorBands: React.FC<IColorBands> = ({ list, tasksStatus, active }) => {
-  const taskProgress: ITaskProgress = {};
-  const completion: { [goalId: string]: number } = {};
-  list.scheduled.forEach((ele) => {
-    const { goalid } = ele;
-    if (!taskProgress[goalid]) {
-      const completedToday = tasksStatus[goalid]?.completedToday || 0;
-      completion[goalid] = completedToday + (completion[goalid] || 0);
-      taskProgress[goalid] = { total: 0, completed: completedToday, goalColor: ele.goalColor };
-    }
-    taskProgress[goalid].total += ele.duration;
-  });
-
-  const completed: React.CSSProperties[] = [];
-  let acc = 0;
-  [...(list.colorBands || [])].forEach((ele) => {
-    const customStyle = { ...ele.style };
-    if (completion[ele.goalId]) {
-      acc += ele.duration;
-      completion[ele.goalId] -= ele.duration;
-      completed.push({ ...customStyle, width: `${(acc / 24) * 100}%` });
-    }
-  });
+const ColorBands: React.FC<IColorBands> = ({ list }) => {
   return (
-    <div className={`MyTime_colorPalette ${active ? "active" : ""}`}>
+    <div className="MyTime_colorPalette">
       {(list.colorBands || []).map((ele, index) => (
         <div className="colorBand" key={uuidv4()} style={{ zIndex: 30 - index, height: 10, ...ele.style }} />
       ))}
