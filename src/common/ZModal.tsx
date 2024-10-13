39a712b232ee57a1c16eb7e05c8fe0e5f5209926
Aarif Khan <ajajchauhan@gmail.com>
2024-10-13 14:42:41 +0530

    Add esc key event listener to call cancel func

--- src/common/ZModal.tsx
+++ src/common/ZModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Modal } from "antd";
 import { useRecoilValue } from "recoil";
 import { darkModeState } from "@src/store";
@@ -8,6 +8,7 @@ import { ZModalProps } from "@src/Interfaces/ICommon";
 const ZModal: React.FC<ZModalProps> = ({ children, type, open, onCancel, width, style }) => {
   const theme = useRecoilValue(themeState);
   const darkModeStatus = useRecoilValue(darkModeState);
+  const handleCancel = onCancel || (() => window.history.back());
 
   const commonClassName = `popupModal${darkModeStatus ? "-dark" : ""} ${darkModeStatus ? "dark" : "light"}-theme${
     theme[darkModeStatus ? "dark" : "light"]
@@ -18,13 +19,28 @@ const ZModal: React.FC<ZModalProps> = ({ children, type, open, onCancel, width, 
     backgroundColor: darkModeStatus ? "rgba(0, 0, 0, 0.50)" : "rgba(87, 87, 87, 0.4)",
   };
 
+  useEffect(() => {
+    const handleKeyDown = (event: any) => {
+      event.stopPropagation();
+      if (event.key === "Escape") {
+        handleCancel();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleCancel]);
+
   return (
     <Modal
       open={open}
       closable={false}
       footer={null}
       centered
-      onCancel={onCancel || (() => window.history.back())}
+      onCancel={handleCancel}
       className={combinedClassName}
       styles={{ mask: maskStyle }}
       width={width}
