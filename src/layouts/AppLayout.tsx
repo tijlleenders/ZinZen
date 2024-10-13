36a76226e38045d0a0dd0030377fba87b128fdf4
Rupali Haldiya <78981177+rupali-codes@users.noreply.github.com>
2024-10-13 14:59:33 +0530

    fix: show scrollbar on overflow

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -8,20 +8,7 @@ import Header from "../components/Header/Header";
 const AppLayout: React.FC<AppLayoutProps> = ({ children, title, debounceSearch }) => (
   <>
     <Header title={title} debounceSearch={debounceSearch} />
-    <div
-      style={{
-        maxWidth: 600,
-        minWidth: 236,
-        overflow: "scroll",
-        width: "100%",
-        marginTop: 60,
-        height: "calc(100vh - 112px)",
-        position: "sticky",
-        paddingBottom: 150,
-      }}
-    >
-      {children}
-    </div>
+    <div className="mainDiv">{children}</div>
     <BottomNavbar title={title} />
   </>
 );
