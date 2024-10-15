ec9b80fedee745241672fad8944749a5223ea571
Rupali Haldiya <78981177+rupali-codes@users.noreply.github.com>
2024-10-15 15:23:24 +0530

    chore: put CSS back in the JSX file

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -8,7 +8,23 @@ import Header from "../components/Header/Header";
 const AppLayout: React.FC<AppLayoutProps> = ({ children, title, debounceSearch }) => (
   <>
     <Header title={title} debounceSearch={debounceSearch} />
-    <div className="mainDiv">{children}</div>
+    <div
+      style={{
+        maxWidth: 600,
+        minWidth: 236,
+        overflow: "scroll",
+        scrollBehavior: "smooth",
+        width: "100%",
+        marginTop: 60,
+        marginBottom: 7,
+        height: "calc(100vh - 112px)",
+        paddingBottom: 100,
+        zIndex: 0,
+      }}
+      className="appLayout"
+    >
+      {children}
+    </div>
     <BottomNavbar title={title} />
   </>
 );
