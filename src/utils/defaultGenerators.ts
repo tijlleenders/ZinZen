156ec4d981738f2d808a418ff0d60e3bb4f3bdb5
Vinay Badgujar <vinaybadgujar8@gmail.com>
2024-11-05 13:09:53 +0530

    remove: dead code

--- src/utils/defaultGenerators.ts
+++ src/utils/defaultGenerators.ts
@@ -1,26 +1,4 @@
-import { v5 as uuidv5 } from "uuid";
-
-import { GoalItem } from "@src/models/GoalItem";
 import { ChangesByType } from "@src/models/InboxItem";
-import { myNameSpace } from ".";
-
-export const createPollObject = (goal: GoalItem, params: object = {}) => ({
-  id: uuidv5(goal.title, myNameSpace),
-  goal,
-  metrics: {
-    upVotes: 0,
-    downVotes: 0,
-    inMyGoals: 0,
-    completed: 0,
-  },
-  myMetrics: {
-    voteScore: 0,
-    inMyGoals: false,
-    completed: false,
-  },
-  createdAt: new Date().toISOString(),
-  ...params,
-});
 
 export function getDefaultValueOfGoalChanges(): ChangesByType {
   return {
