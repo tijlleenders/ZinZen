beab83407165e006817bbef928eba57c9c47f52a
arashveysi3 <am7team@gmail.com>
2024-11-09 20:20:38 +0330

    Fixedv a bug which cause the share list to not get updates

--- src/pages/GoalsPage/PartnerGoals.tsx
+++ src/pages/GoalsPage/PartnerGoals.tsx
@@ -58,7 +58,7 @@ const PartnerGoals = () => {
     const rootGoals = await getRootGoalsOfPartner(relId);
     handleUserGoals(rootGoals);
   };
-
+  refreshActiveGoals();
   const search = async (text: string) => {
     const rootGoals = await getRootGoalsOfPartner(relId);
     handleUserGoals(rootGoals.filter((goal) => goal.title.toUpperCase().includes(text.toUpperCase())));
