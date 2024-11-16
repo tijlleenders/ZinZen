2baf26c8ab3b467dd0a99324b1d6505eb77f273b
Vinay Badgujar <vinaybadgujar8@gmail.com>
2024-11-16 20:36:51 +0530

    refactor: use styles from short css file

--- src/components/GoalsComponents/DisplayChangesModal/ShowChanges.tsx
+++ src/components/GoalsComponents/DisplayChangesModal/ShowChanges.tsx
@@ -11,28 +11,28 @@ export const getMovedSubgoalsList = (
   if (!goalUnderReview) return null;
 
   return (
-    <div className="move-info-container">
-      <div className="move-info-item">
-        <span className="move-info-label">Goal Being Moved</span>
-        <div className="move-info-value highlight-box">{goalUnderReview.title}</div>
+    <div className="move-info-container d-flex f-col gap-20 bordered br-12 bg-sec">
+      <div className="move-info-item d-flex f-col gap-8">
+        <span className="move-info-label text-sm fw-600">Goal Being Moved</span>
+        <div className="move-info-value bg-primary  br-8 bordered">{goalUnderReview.title}</div>
       </div>
 
-      <div className="move-direction-container">
+      <div className="move-direction-container br-12 gap-8 bordered br-12">
         <div className="move-info-item">
           <span className="move-info-label">From</span>
-          <div className="move-info-value">{oldParentTitle}</div>
+          <div className="move-info-value  br-8 bordered">{oldParentTitle}</div>
         </div>
 
-        <div className="arrow">→</div>
+        <div className="arrow place-middle text-sm bg-sec">→</div>
 
         <div className="move-info-item">
           <span className="move-info-label">To</span>
-          <div className="move-info-value">{newParentTitle}</div>
+          <div className="move-info-value br-8 bordered">{newParentTitle}</div>
         </div>
       </div>
 
       {newParentTitle === "Non-shared goal" && (
-        <div className="warning-message">
+        <div className="warning-message place-middle gap-8 text-sm br-8">
           <InfoCircleOutlined />
           <span>The new parent goal is not shared. The goal will be moved to the root.</span>
         </div>
