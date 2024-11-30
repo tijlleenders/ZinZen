d86c28f1c07d603e6025b10a292dee0c9cc4f900
Vinay Badgujar <vinaybadgujar8@gmail.com>
2024-11-30 18:54:56 +0530

    only download csv if respective data is present

--- src/utils/ExportStrategies/CsvExportStrategy.ts
+++ src/utils/ExportStrategies/CsvExportStrategy.ts
@@ -9,28 +9,26 @@ export class CsvExportStrategy implements ExportStrategy {
       const goalsArray = await db.goalsCollection.toArray();
       const feelingsArray = await db.feelingsCollection.toArray();
 
-      // Format dates only show YYYY-MM-DD
-      const formattedFeelingsArray = feelingsArray.map((feeling) => ({
-        ...feeling,
-        date: new Date(feeling.date).toISOString().slice(0, 10),
-      }));
+      if (goalsArray.length > 0) {
+        const goalsCollectionKeys = db.goalsCollection.schema.indexes;
+        const goalsKeys = goalsCollectionKeys.map((key) => key.name);
+        const goalsCsvContent = convertJSONToCSV(goalsArray, goalsKeys);
+        const goalsBlob = new Blob([goalsCsvContent], { type: "text/csv;charset=utf-8;" });
+        await downloadFile(goalsBlob, getFileName("ZinZenGoals", "csv"));
+      }
 
-      console.log(formattedFeelingsArray[0].date);
+      if (feelingsArray.length > 0) {
+        const formattedFeelingsArray = feelingsArray.map((feeling) => ({
+          ...feeling,
+          date: new Date(feeling.date).toISOString().slice(0, 10),
+        }));
 
-      const goalsCollectionKeys = db.goalsCollection.schema.indexes;
-      const goalsKeys = goalsCollectionKeys.map((key) => key.name);
-
-      const feelingsCollectionKeys = db.feelingsCollection.schema.indexes;
-      const feelingsKeys = feelingsCollectionKeys.map((key) => key.name);
-
-      const goalsCsvContent = convertJSONToCSV(goalsArray, goalsKeys);
-      const feelingsCsvContent = convertJSONToCSV(formattedFeelingsArray, feelingsKeys);
-
-      const goalsBlob = new Blob([goalsCsvContent], { type: "text/csv;charset=utf-8;" });
-      const feelingsBlob = new Blob([feelingsCsvContent], { type: "text/csv;charset=utf-8;" });
-
-      await downloadFile(goalsBlob, getFileName("ZinZenGoals", "csv"));
-      await downloadFile(feelingsBlob, getFileName("ZinZenFeelings", "csv"));
+        const feelingsCollectionKeys = db.feelingsCollection.schema.indexes;
+        const feelingsKeys = feelingsCollectionKeys.map((key) => key.name);
+        const feelingsCsvContent = convertJSONToCSV(formattedFeelingsArray, feelingsKeys);
+        const feelingsBlob = new Blob([feelingsCsvContent], { type: "text/csv;charset=utf-8;" });
+        await downloadFile(feelingsBlob, getFileName("ZinZenFeelings", "csv"));
+      }
     } catch (error) {
       console.error("CSV Export failed:", error);
       throw error;
