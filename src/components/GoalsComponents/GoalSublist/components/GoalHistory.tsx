04dead6ada9d727772f4fb2bb2d7f8222fe124e5
Vinay Badgujar <vinaybadgujar8@gmail.com>
2025-03-27 12:47:45 +0530

    fix: broken goal history navigation in partner mode

--- src/components/GoalsComponents/GoalSublist/components/GoalHistory.tsx
+++ src/components/GoalsComponents/GoalSublist/components/GoalHistory.tsx
@@ -5,35 +5,32 @@ import { darkModeState } from "@src/store";
 import goalsIcon from "@assets/images/goalsIcon.svg";
 import { ISubGoalHistory } from "@src/store/GoalsState";
 import { useTranslation } from "react-i18next";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { ILocationState } from "@src/Interfaces";
 import "./GoalHistory.scss";
+import { BreadcrumbItem } from "./BreadcrumbItem";
 
-const BreadcrumbItem = ({ goal }: { goal: ISubGoalHistory }) => {
-  const { t } = useTranslation();
-  return (
-    <span
-      className="breadcrumb-item fw-500"
-      style={{
-        border: `1px solid ${goal.goalColor}`,
-        background: `${goal.goalColor}33`,
-      }}
-    >
-      {t(goal.goalTitle)}
-    </span>
-  );
-};
+interface BreadcrumbItem {
+  title: React.ReactNode;
+  onClick: () => void;
+}
 
-const GoalHistory = () => {
+const GoalHistory: React.FC = () => {
   const location = useLocation();
   const navigate = useNavigate();
+  const { partnerId } = useParams();
+  const { t } = useTranslation();
+
   const locationState: ILocationState = location.state;
   const goalsHistory = locationState.goalsHistory ?? [];
   const darkModeStatus = useRecoilValue(darkModeState);
+  const isPartnerGoalActive = Boolean(partnerId);
 
   const handleBreadcrumbClick = (goalId: string, index: number) => {
     const newGoalsHistory = goalsHistory.slice(0, index + 1);
-    navigate(`/goals/${goalId}`, {
+    const path = isPartnerGoalActive ? `/partners/${partnerId}/goals/${goalId}` : `/goals/${goalId}`;
+
+    navigate(path, {
       state: {
         ...locationState,
         goalsHistory: newGoalsHistory,
@@ -41,31 +38,37 @@ const GoalHistory = () => {
     });
   };
 
+  const handleHomeClick = () => {
+    const path = isPartnerGoalActive ? `/partners/${partnerId}/goals` : "/goals";
+
+    navigate(path, {
+      state: { ...location.state, goalsHistory: [] },
+    });
+  };
+
+  const breadcrumbItems: BreadcrumbItem[] = [
+    {
+      title: (
+        <img
+          src={goalsIcon}
+          className={`goals-icon ${darkModeStatus ? "goals-icon-history" : ""}`}
+          alt={t("my goals")}
+        />
+      ),
+      onClick: handleHomeClick,
+    },
+    ...goalsHistory.map((goal: ISubGoalHistory, index: number) => ({
+      title: <BreadcrumbItem goal={goal} />,
+      onClick: () => handleBreadcrumbClick(goal.goalID, index),
+    })),
+  ];
+
   return (
     <div className="goal-history">
       <Breadcrumb
         className="breadcrumb-container"
         separator={<span className={`separator ${darkModeStatus ? "dark-mode" : ""}`}>/</span>}
-        items={[
-          {
-            title: (
-              <img
-                src={goalsIcon}
-                className={`goals-icon ${darkModeStatus ? "goals-icon-history" : ""}`}
-                alt="my goals"
-              />
-            ),
-            onClick: () => {
-              navigate("/goals", {
-                state: { ...location.state, goalsHistory: [] },
-              });
-            },
-          },
-          ...goalsHistory.map((goal: ISubGoalHistory, index: number) => ({
-            title: <BreadcrumbItem goal={goal} />,
-            onClick: () => handleBreadcrumbClick(goal.goalID, index),
-          })),
-        ]}
+        items={breadcrumbItems}
       />
     </div>
   );
