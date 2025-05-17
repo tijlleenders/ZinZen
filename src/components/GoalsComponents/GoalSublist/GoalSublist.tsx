c6c73f130ace81eece2936879c7ded94a5365f65
Vinay Badgujar <vinaybadgujar8@gmail.com>
2025-05-17 22:39:08 +0530

    refactor: remove unused state and effects in GoalSublist component

--- src/components/GoalsComponents/GoalSublist/GoalSublist.tsx
+++ src/components/GoalsComponents/GoalSublist/GoalSublist.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from "react";
+import React, { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
 import { useTranslation } from "react-i18next";
 import { displayChangesModal, displayGoalId, displaySuggestionsModal } from "@src/store/GoalsState";
@@ -6,14 +6,12 @@ import { GoalItem } from "@src/models/GoalItem";
 import { createGoalObjectFromTags } from "@src/helpers/GoalProcessor";
 import { lastAction } from "@src/store";
 import { getGoalHintItem } from "@src/api/HintsAPI";
-import { priotizeImpossibleGoals } from "@src/utils/priotizeImpossibleGoals";
 import { AvailableGoalHintProvider } from "@src/contexts/availableGoalHint-context";
 import { DeletedGoalProvider } from "@src/contexts/deletedGoal-context";
 import DeletedGoals from "@pages/GoalsPage/components/DeletedGoals";
 import ArchivedGoals from "@pages/GoalsPage/components/ArchivedGoals";
 import GoalItemSummary from "@src/common/GoalItemSummary/GoalItemSummary";
 import AvailableGoalHints from "@pages/GoalsPage/components/AvailableGoalHints";
-import { moveGoalState } from "@src/store/moveGoalState";
 import { useParams } from "react-router-dom";
 import { useGetActiveGoals } from "@src/hooks/api/Goals/useGetActiveGoals";
 import { useGetGoalById } from "@src/hooks/api/Goals/useGetGoalById";
@@ -25,19 +23,12 @@ export const GoalSublist = () => {
   const { parentId } = useParams();
   const { data: parentGoal } = useGetGoalById(parentId || "");
   const { activeGoals } = useGetActiveGoals(parentId || "root");
-  const [sortedGoals, setSortedGoals] = useState<GoalItem[]>([]);
   const [goalHints, setGoalHints] = useState<GoalItem[]>([]);
   const { t } = useTranslation();
   const action = useRecoilValue(lastAction);
   const goalID = useRecoilValue(displayGoalId);
   const showChangesModal = useRecoilValue(displayChangesModal);
   const showSuggestionModal = useRecoilValue(displaySuggestionsModal);
-  const goalToMove = useRecoilValue(moveGoalState);
-
-  useEffect(() => {
-    if (!activeGoals) return;
-    priotizeImpossibleGoals(activeGoals).then(setSortedGoals);
-  }, [activeGoals]);
 
   useEffect(() => {
     if (!parentId) return;
@@ -52,13 +43,7 @@ export const GoalSublist = () => {
     });
   }, [action, parentId, showSuggestionModal, showChangesModal, activeGoals, goalID]);
 
-  useEffect(() => {
-    async function init() {
-      if (!parentId) return;
-      const sortedGoals = await priotizeImpossibleGoals(activeGoals || []);
-    }
-    init();
-  }, [action, parentId, showSuggestionModal, showChangesModal, activeGoals, goalID, goalToMove]);
+  // TODO: re-implement sorting of goals
 
   return (
     <div className="sublist-container">
