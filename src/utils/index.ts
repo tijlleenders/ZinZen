fac0ead9a01f4564da85f04b22c6f5fd3430ef5b
Vinay Badgujar <vinaybadgujar8@gmail.com>
2025-06-01 11:38:38 +0530

    chore: remove unused ColorPicker, BudgetSummary, GoalItemSummary, and GoalSummary components along with their associated styles and hooks

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,5 @@
-import { useTranslation } from "react-i18next";
 import { GoalItem } from "@src/models/GoalItem";
 import { languagesFullForms } from "@src/translations/i18n";
-import i18next from "i18next";
 import sha256 from "crypto-js/sha256";
 import { LocalStorageKeys } from "@src/constants/localStorageKeys";
 
@@ -76,23 +74,6 @@ export const getDiffInHours = (date1: Date, date2: Date) => {
   let diff = date1.getTime() - date2.getTime();
   diff = Math.round(Math.abs(diff / 36e5));
   return diff;
-};
-
-export const getDiffInDates = (date1: Date, date2: Date, resetTime = true) => {
-  const a = new Date(date1);
-  const b = new Date(date2);
-  if (resetTime) {
-    a.setHours(0, 0, 0, 0);
-    b.setHours(0, 0, 0, 0);
-  }
-  // Convert both dates to milliseconds
-  const date1MS = a.getTime();
-  const date2MS = b.getTime();
-  // Calculate the difference in milliseconds
-  const differenceMS = date2MS - date1MS;
-  // Convert the difference to days
-  const daysDifference = Math.ceil(differenceMS / (1000 * 60 * 60 * 24));
-  return daysDifference;
 };
 
 export const colorPalleteList = [
@@ -214,41 +195,6 @@ export const getSvgForGoalPps = (count: number) => {
     default:
       return "ThreeAvatars";
   }
-};
-
-export const formatBudgetHrsToText = (hours: string | null) => {
-  if (hours === null) {
-    return "";
-  }
-  const parts = hours.split("-").map(Number);
-  if (parts.length === 2 && parts[0] === parts[1]) {
-    return `${i18next.t(`hourWithCount_${parts[0] <= 1 ? "one" : "other"}`, { count: parts[0] })}`;
-  }
-
-  return `${parts[0]}-${i18next.t(`hourWithCount_${parts[1] <= 1 ? "one" : "other"}`, { count: parts[1] })}`;
-};
-
-export const calculateDaysLeft = (dueDate: string) => {
-  const { t } = useTranslation();
-  if (!dueDate) return null;
-  const currentDate = new Date();
-  const due = new Date(dueDate);
-  const daysLeft = getDiffInDates(currentDate, due);
-
-  if (daysLeft === 0) {
-    return t("dueToday");
-  }
-  if (daysLeft >= 0) {
-    return daysLeft === 1 ? t("daysLeftSingular", { days: daysLeft }) : t("daysLeft", { days: daysLeft });
-  }
-  return daysLeft >= -1
-    ? t("dueDatePassedSingular", { days: Math.abs(daysLeft) })
-    : t("dueDatePassed", { days: Math.abs(daysLeft) });
-};
-
-// Utility function for singular/plural formatting
-export const formatSingularPlural = (count: number, singularWord: string) => {
-  return `${count} ${singularWord}${count !== 1 ? "s" : ""}`;
 };
 
 export const hashObject = (obj: object) => {
