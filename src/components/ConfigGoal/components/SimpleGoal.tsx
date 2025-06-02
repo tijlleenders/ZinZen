78556bad635407763fe1da71a7041117556ed7df
Vinay Badgujar <vinaybadgujar8@gmail.com>
2025-06-02 17:41:31 +0530

    feat: implement SimpleGoal component for goal configuration with duration and due date management

---
+++ src/components/ConfigGoal/components/SimpleGoal.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { ScheduleStatus } from "@src/Interfaces";
+import { useSearchParams } from "react-router-dom";
+import HintToggle from "./HintToggle";
+import { FormState } from "../ConfigGoal.helper";
+import Duration from "../Duration";
+import DueDate from "../DueDate";
+
+interface SimpleGoalProps {
+  formState: FormState;
+  setFormState: React.Dispatch<React.SetStateAction<FormState>>;
+  scheduleStatus: ScheduleStatus | null;
+  getScheduleStatusText: (status: ScheduleStatus) => string;
+}
+
+const SimpleGoal: React.FC<SimpleGoalProps> = ({ formState, setFormState, scheduleStatus, getScheduleStatusText }) => {
+  const [searchParams] = useSearchParams();
+  const action = searchParams.get("mode") === "edit" ? "Update" : "Create";
+
+  const { t } = useTranslation();
+
+  return (
+    <div className="d-flex f-col gap-16">
+      <div className="action-btn-container">
+        <HintToggle
+          setHints={(value: boolean) => setFormState((prev) => ({ ...prev, hintOption: value }))}
+          defaultValue={formState.hintOption}
+        />
+        <button type="submit" className="action-btn place-middle gap-16">
+          {t(`${action} Goal`)}
+        </button>
+      </div>
+      <div className="place-middle justify-fs gap-16">
+        <Duration
+          value={formState.simpleGoal?.duration ?? ""}
+          onChange={(value) => {
+            if (formState.simpleGoal) {
+              setFormState((prev) => ({
+                ...prev,
+                simpleGoal: {
+                  due: prev.simpleGoal!.due,
+                  duration: value,
+                },
+              }));
+            }
+          }}
+        />
+
+        <DueDate
+          dateValue={formState.simpleGoal?.due ?? ""}
+          handleDateChange={(newDate) => {
+            setFormState((prev) => ({
+              ...prev,
+              simpleGoal: { due: newDate, duration: prev.simpleGoal!.duration },
+            }));
+          }}
+        />
+      </div>
+      {scheduleStatus && formState.simpleGoal?.duration && (
+        <div className={`schedule-status ${scheduleStatus}`}>{getScheduleStatusText(scheduleStatus)}</div>
+      )}
+    </div>
+  );
+};
+
+export default SimpleGoal;
