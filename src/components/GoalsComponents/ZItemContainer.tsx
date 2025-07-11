27dec7b0796dbf572fbdfa9c467f9b5a021d76c3
Vinay Badgujar <vinaybadgujar8@gmail.com>
2025-07-11 15:02:52 +0530

    refactor: update ZItemContainer and MyGoal components to use glowGoalIdState for animation handling

--- src/components/GoalsComponents/ZItemContainer.tsx
+++ src/components/GoalsComponents/ZItemContainer.tsx
@@ -6,28 +6,19 @@ import "./ZItemContainer.scss";
 
 export const ZItemContainer: React.FC<{
   id: string;
-  expandGoalId?: string;
-  isAnimating?: boolean;
+  shouldAnimate?: boolean;
   isGoalToBeMoved?: boolean;
   children: ReactNode;
   extraContent?: ReactNode;
   dataTestId?: string;
-}> = ({
-  id,
-  expandGoalId = "root",
-  isAnimating = false,
-  isGoalToBeMoved = false,
-  children,
-  extraContent,
-  dataTestId,
-}) => {
+}> = ({ id, shouldAnimate = false, isGoalToBeMoved = false, children, extraContent, dataTestId }) => {
   const darkModeStatus = useRecoilValue(darkModeState);
   return (
     <div
       key={id}
       id={id}
       data-testid={dataTestId}
-      className={`user-goal${darkModeStatus ? "-dark" : ""} ${expandGoalId === id && isAnimating ? "goal-glow" : ""} ${isGoalToBeMoved ? "goal-to-move-selected" : ""}`}
+      className={`user-goal${darkModeStatus ? "-dark" : ""} ${shouldAnimate ? "goal-glow" : ""} ${isGoalToBeMoved ? "goal-to-move-selected" : ""}`}
     >
       <div className="user-goal-main">{children}</div>
       {extraContent}
