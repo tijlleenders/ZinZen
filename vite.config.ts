70ad476cee89091861ebeb536be4cd1991eee37d
Vinay Badgujar <vinaybadgujar8@gmail.com>
2025-09-16 14:51:23 +0530

    chore: update dependencies in package.json and package-lock.json, add Vite React plugin and Babel plugin for React compiler

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from "path";
 import { Alias, defineConfig } from "vite";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
 
 import * as tsconfig from "./tsconfig.paths.json";
 import generateBuildInfo from "./plugins/generateVersion";
@@ -27,6 +28,11 @@ export default defineConfig({
     port: 3000,
   },
   plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", { target: "18" }]],
+      },
+    }),
     VitePWA({
       registerType: "autoUpdate",
       strategies: "injectManifest",
