552d5abb49d033f07c7f247ea933f30f1b00ed51
Vinay Badgujar <vinaybadgujar8@gmail.com>
2025-11-04 10:42:29 +0530

    feat: add routing components and structure for basic routes including donate, feedback, invest, and zinzenfaq

---
+++ src/routes/__root.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import type { QueryClient } from "react-query";
+
+interface RouterContext {
+  queryClient: QueryClient;
+}
+
+const RootComponent = () => (
+  <>
+    <Outlet />
+    {process.env.NODE_ENV === "development" && <TanStackRouterDevtools />}
+  </>
+);
+
+export const Route = createRootRouteWithContext<RouterContext>()({
+  component: RootComponent,
+  // notFoundComponent: () => <MyGoalsPage />,
+});
