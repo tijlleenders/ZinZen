4937519619c0c6c64c86a11dd5601b7ceb0a2407
Vinay Badgujar <vinaybadgujar8@gmail.com>
2025-12-01 12:11:44 +0530

    fix: hide partner goal fabs until invitation accepted

--- src/routes/(partnerGoalRoutes)/partners.$partnerId.goals.$parentId.tsx
+++ src/routes/(partnerGoalRoutes)/partners.$partnerId.goals.$parentId.tsx
@@ -4,10 +4,8 @@ import { createFileRoute } from "@tanstack/react-router";
 import PartnerGoals from "@pages/GoalsPage/PartnerGoals";
 import { useGetSharedWMActiveGoals } from "@src/hooks/api/SharedWMGoals/useGetSharedWMActiveGoals";
 import { useGetContactByPartnerId } from "@src/hooks/api/Contacts/queries/useGetContactByPartnerId";
-import { themeSelectionMode } from "@src/store/ThemeState";
 import { moveGoalState } from "@src/store/moveGoalState";
 import PartnerGoalMoveFab from "@components/fab/PartnerGoalMoveFab";
-import ThemeConfirmFab from "@components/fab/ThemeConfirmFab";
 import GoalsFab from "@components/fab/GoalsFab";
 
 export const Route = createFileRoute("/(partnerGoalRoutes)/partners/$partnerId/goals/$parentId")({
@@ -15,13 +13,12 @@ export const Route = createFileRoute("/(partnerGoalRoutes)/partners/$partnerId/g
     const { parentId = "root", partnerId = "" } = Route.useParams();
     const { data: contact } = useGetContactByPartnerId(partnerId);
     const { data: activeSharedWMGoals = [] } = useGetSharedWMActiveGoals(parentId, contact?.relId);
-    const themeSelection = useRecoilValue(themeSelectionMode);
     const goalToMove = useRecoilValue(moveGoalState);
 
     return (
       <>
         <PartnerGoals activeSharedWMGoals={activeSharedWMGoals} parentId={parentId} partnerId={partnerId} />
-        {themeSelection ? <ThemeConfirmFab /> : goalToMove ? <PartnerGoalMoveFab /> : <GoalsFab />}
+        {contact?.accepted && (goalToMove ? <PartnerGoalMoveFab /> : <GoalsFab />)}
       </>
     );
   },
